2531acbb35bd755a4b1770ddb993151d8feeb8ef
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-01-24 14:45:58 +0530

    Time: 1 ms (99.35%), Space: 38.8 MB (53.47%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean detectCapitalUse(String word) 
+    {
+        int cap=0, small=0, c3=0;
+        
+        for(int i=0;i<word.length();i++)
+        {
+            char ch=word.charAt(i);
+            
+            if(ch>=65 && ch<=90)
+            {
+                if(cap>=0 && small==0)
+                    cap++;
+                
+                else
+                    return false;
+            }
+            
+            else
+            {
+                if(cap<=1 && small>=0)
+                {
+                    small++;
+                }
+                else
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
