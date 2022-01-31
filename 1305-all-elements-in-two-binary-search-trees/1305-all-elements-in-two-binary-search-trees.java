576f27f2832bf27b6becdda8d4dbdf77a92249cb
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-01-31 14:55:38 +0530

    Time: 59 ms (7.98%), Space: 68.3 MB (74.24%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -13,155 +13,52 @@
  *     }
  * }
  */
-class Solution 
-{
-    class Pair
-    {
-        int state=0;
-        TreeNode node;
-        
-        Pair(TreeNode node, int state)
-        {
-            this.node=node;
-            this.state=state;
-        }
-    }
-    
-    public Stack<Pair>st;
-    public Stack<Pair>rst;
-    
+class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) 
     {
-//         ArrayList<Integer>list1=new ArrayList<>();
-//         ArrayList<Integer>list2=new ArrayList<>();
+        ArrayList<Integer>list1=new ArrayList<>();
+        ArrayList<Integer>list2=new ArrayList<>();
         
-//         traverse(root1,list1);
-//         traverse(root2,list2);
+        traverse(root1,list1);
+        traverse(root2,list2);
         
-        // int i=0, j=0;
+        int i=0, j=0;
         ArrayList<Integer>ans=new ArrayList<>();
         
-        st=new Stack<>();
-        rst=new Stack<>();
         
-        if(root1!=null)
-        st.push(new Pair(root1,0));
-        
-        if(root2!=null)
-        rst.push(new Pair(root2,0));
-        
-        Pair left=getNextLeft();
-        Pair right=getNextRight();
-        
-        
-        while(st.size()!=0 && rst.size()!=0)
+        while(list1.size()!=0 && list2.size()!=0)
         {
-            if(left.node.val<=right.node.val)
+            if(list1.get(0)<=list2.get(0))
             {
-                ans.add(left.node.val);
-                left=getNextLeft();
+                ans.add(list1.remove(0));
             }
-            else if(left.node.val>right.node.val)
+            
+            else if(list1.get(0)>list2.get(0))
             {
-                ans.add(right.node.val);
-                right=getNextRight();
+                ans.add(list2.remove(0));
             }
         }
         
-        while(st.size()!=0)
+        while(list1.size()!=0)
         {
-            ans.add(left.node.val);
-            left=getNextLeft();
+            ans.add(list1.remove(0));
         }
         
-        while(rst.size()!=0)
+        while(list2.size()!=0)
         {
-            ans.add(right.node.val);
-            right=getNextRight();
+            ans.add(list2.remove(0));
         }
         
         return ans;
     }
     
-    public Pair getNextLeft()
+    public void traverse(TreeNode node, ArrayList<Integer>list)
     {
-        while(st.size()>0)
-        {
-            Pair top=st.peek();
-            
-            if(top.state==0)
-            {
-                if(top.node.left!=null)
-                {
-                    Pair p=new Pair(top.node.left,0);
-                    st.push(p);
-                }
-                top.state++;
-            }
-            
-            else if(top.state==1)
-            {
-                if(top.node.right!=null)
-                {
-                    Pair p=new Pair(top.node.right,0);
-                    st.push(p);
-                }
-                top.state++;
-                return top;
-            }
-            
-            else
-            {
-                st.pop();
-            }
-            
-        }
-        return null;
-    }
-    
-    public Pair getNextRight()
-    {
-        while(rst.size()>0)
-        {
-            Pair top=rst.peek();
-            
-            if(top.state==0)
-            {
-                if(top.node.left!=null)
-                {
-                    Pair p=new Pair(top.node.left,0);
-                    rst.push(p);
-                }
-                top.state++;
-            }
-            
-            else if(top.state==1)
-            {
-                if(top.node.right!=null)
-                {
-                    Pair p=new Pair(top.node.right,0);
-                    rst.push(p);
-                }
-                top.state++;
-                return top;
-            }
-            
-            else
-            {
-                rst.pop();
-            }
-            
-        }
-        return null;
-    }
-    
-//     public void traverse(TreeNode node, ArrayList<Integer>list)
-//     {
-//         if(node==null)
-//             return;
+        if(node==null)
+            return;
         
-//         traverse(node.left,list);
-//         list.add(node.val);
-//         traverse(node.right,list);
-//     }
+        traverse(node.left,list);
+        list.add(node.val);
+        traverse(node.right,list);
+    }
 }
