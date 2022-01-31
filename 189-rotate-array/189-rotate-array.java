9a19907d96b43d97282cb5c52f411742361b1d92
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-01-31 13:04:35 +0530

    Time: 1 ms (76.91%), Space: 64.3 MB (10.13%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[] nums, int k) 
+    {
+        int n=nums.length;
+        
+        if(k>n)
+        {
+            k=k%n;
+        }
+        
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,nums.length-1);
+        reverse(nums,0,n-1);
+        
+    }
+    
+    public void reverse(int a[], int start, int end)
+    {
+        while(start<end)
+        {
+            int temp=a[start];
+            a[start]=a[end];
+            a[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
