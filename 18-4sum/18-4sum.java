082ced148188ae1da5ab43674036b9ce23112de6
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-02-04 00:26:51 +0530

    Time: 12 ms (86.84%), Space: 43 MB (29.18%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -4,38 +4,67 @@ class Solution {
         Arrays.sort(a);
         List<List<Integer>>res=new ArrayList<>();
         
-        int n=a.length;
-        
-        for(int i=0;i<n;i++)
+        for(int i=0;i<a.length;i++)
         {
-            if(i!=0 && a[i]==a[i-1]) continue;
+            if(i!=0 && a[i-1]==a[i])
+                continue;
             
-            for(int j=i+1;j<n;j++)
+            List<List<Integer>>smallSum=threeSum(a,i+1,a.length-1,target-a[i]);
+            
+            for(List<Integer>list:smallSum)
             {
-                if(j!=i+1 && a[j]==a[j-1]) continue;
-                
-                int si=j+1, ei=n-1;
-                
-                while(si<ei)
-                {
-                    int sum=a[i]+a[j]+a[si]+a[ei];
-                    
-                    if(sum>target) ei--;
-                    else if(sum<target) si++;
-                    else
-                    {
-                        res.add(new ArrayList(Arrays.asList(a[i],a[j],a[si],a[ei])));
-                        si++;
-                        ei--;
-                        
-                        while(si<ei && a[si]==a[si-1])si++;
-                        while(si<ei && a[ei]==a[ei+1])ei--;
-                    }
-                }
+                list.add(a[i]);
+                res.add(list);
             }
         }
         
         return res;
         
+    }
+    
+    public List<List<Integer>> twoSum(int a[], int si, int ei, int target)
+    {
+        List<List<Integer>>res=new ArrayList<>();
+        
+        while(si<ei)
+        {
+            int sum=a[si]+a[ei];
+            
+            if(sum>target)ei--;
+            else if(sum<target)si++;
+            
+            else
+            {
+                res.add(new ArrayList(Arrays.asList(a[si],a[ei])));    
+                si++;
+                ei--;
+                
+                while(si<ei && a[si]==a[si-1])si++;
+                while(si<ei && a[ei]==a[ei+1])ei--;
+            }
+        }
+        
+        return res;
+    }
+    
+    public List<List<Integer>> threeSum(int a[], int si, int ei, int target)
+    {
+        List<List<Integer>>res=new ArrayList<>();
+        
+        for(int i=si;i<=ei;i++)
+        {
+            if(i!=si && a[i-1]==a[i])
+                continue;
+            
+            List<List<Integer>>smallSum=twoSum(a,i+1,ei,target-a[i]);
+            
+            for(List<Integer>list:smallSum)
+            {
+                list.add(a[i]);
+                res.add(list);
+            }
+        }
+        
+        return res;
     }
 }
