5b419ca786db355e913cf10ec3e05a576ed9860e
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-02-13 11:32:10 +0530

    Time: 2 ms (44.88%), Space: 42.9 MB (19.26%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] a) 
+    {
+        List<List<Integer>>res=new ArrayList<>();
+        choose(a,a.length,0,res,new ArrayList<>());
+        return res;
+    }
+    
+    public void choose(int a[], int n, int ind, List<List<Integer>>res, List<Integer>list)
+    {
+        if(ind==n)
+        {
+            List<Integer>temp=new ArrayList<>(list);
+            res.add(temp);
+            return;
+        }
+        
+        list.add(a[ind]);
+        choose(a,n,ind+1,res,list);
+        list.remove(list.size()-1);
+        choose(a,n,ind+1,res,list);
+    }
+}
