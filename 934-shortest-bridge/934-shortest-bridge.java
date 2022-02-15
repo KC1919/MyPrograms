5c4d12ad68b4ddd0bcadda41924f6a43cd2a7403
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-02-15 16:21:55 +0530

    Time: 9 ms (73.64%), Space: 54 MB (26.45%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.java
+++ 934-shortest-bridge/934-shortest-bridge.java
@@ -1,5 +1,5 @@
-class Solution {
-    
+class Solution 
+{    
     class Pair
     {
         int row;
@@ -18,59 +18,65 @@ class Solution {
         
         boolean visited[][]=new boolean[n][n];
         
-        List<List<Pair>>res=new ArrayList<>();
+        Queue<Pair>que=new LinkedList<>();
         
-        for(int i=0;i<n;i++)
+        outer:for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(visited[i][j]==false && a[i][j]==1)
                 {
-                    List<Pair>comp=new ArrayList<>();
-                    component(a,i,j,n,visited,comp);
-                    res.add(comp);
+                    component(a,i,j,n,visited,que);
+                    break outer;
                 }
             }
         }
         
-        int min=getDistance(res.get(0), res.get(1));
+        int min=0;
+        
+        int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
+        
+        
+        while(que.size()>0)
+        {
+            int size=que.size();
+            
+            while(size-->0)
+            {
+                Pair rem=que.remove();
+                
+                for(int i=0;i<dir.length;i++)
+                {
+                    int row=rem.row+dir[i][0];
+                    int col=rem.col+dir[i][1];
+                    
+                    if(row>=0 && row<n && col>=0 && col<n && visited[row][col]==false)
+                    {
+                        if(a[row][col]==1)
+                            return min;
+                        
+                        que.add(new Pair(row,col));
+                        visited[row][col]=true;
+                    }
+                }
+            }
+            min++;
+        }
         return min;
     }
     
-    public int getDistance(List<Pair>l1, List<Pair>l2)
-    {
-        int min=Integer.MAX_VALUE;
-        
-        for(int i=0;i<l1.size();i++)
-        {
-            Pair p=l1.get(i);
-            for(int j=0;j<l2.size();j++)
-            {
-                Pair p1=l2.get(j);
-                int rd=Math.abs(p.row-p1.row);
-                int cd=Math.abs(p.col-p1.col);
-                int td=rd+cd;
-                
-                if(td<min)
-                {
-                    min=td;
-                }
-            }
-        }
-        return min-1;
-    }
     
-    public void component(int a[][], int i, int j, int n, boolean visited[][], List<Pair>comp)
+    public void component(int a[][], int i, int j, int n, boolean visited[][], Queue<Pair>que)
     {
         if(i<0 || j<0 || i>=n || j>=n || visited[i][j]==true || a[i][j]==0)
             return;
         
         visited[i][j]=true;
-        comp.add(new Pair(i,j));
+        que.add(new Pair(i,j));
         
-        component(a,i-1,j,n,visited,comp);
-        component(a,i+1,j,n,visited,comp);
-        component(a,i,j-1,n,visited,comp);
-        component(a,i,j+1,n,visited,comp);
+        component(a,i-1,j,n,visited,que);
+        component(a,i+1,j,n,visited,que);
+        component(a,i,j-1,n,visited,que);
+        component(a,i,j+1,n,visited,que);
     }
 }
