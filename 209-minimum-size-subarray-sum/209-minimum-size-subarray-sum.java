9056756c0864a9e5f08558144982ee3285d5a86c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-02-16 17:32:12 +0530

    Time: 1 ms (99.97%), Space: 41.8 MB (62.30%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -2,19 +2,22 @@ class Solution
 {
     public int minSubArrayLen(int target, int[] nums) 
     {
-        int n = nums.length;
-        int ans = Integer.MAX_VALUE;
+        int res = Integer.MAX_VALUE;
+        
         int left = 0;
+        
+        
         int sum = 0;
-        for (int i = 0; i < n; i++) 
-        {
-            sum += nums[i];
-            while (sum >= target) 
-            {
-                ans = Math.min(ans, i + 1 - left);
+        for(int right = 0; right < nums.length; right++){
+            sum += nums[right];
+            while(sum >= target){
+                
+                res = Math.min(res, right - left + 1);
                 sum -= nums[left++];
             }
+            
         }
-        return (ans != Integer.MAX_VALUE) ? ans : 0;
+        
+        return res == Integer.MAX_VALUE ? 0 : res;
     }
 }
