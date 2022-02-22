dc49588f7044610b7ab59552fda9314242d50c19
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-02-22 20:19:57 +0530

    Time: 1 ms (89.34%), Space: 42.4 MB (29.17%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -8,15 +8,15 @@ class Solution
         String keys[]={"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         List<String>ans=new ArrayList<>();
         
-        generateSeq(digits,keys,ans,"",0);
+        generateSeq(digits,keys,ans,new StringBuilder(),0);
         return ans;
     }
     
-    public void generateSeq(String digits, String keys[], List<String>list, String ans, int ind)
+    public void generateSeq(String digits, String keys[], List<String>list, StringBuilder ans, int ind)
     {
         if(ind==digits.length())
         {
-            list.add(ans);
+            list.add(ans.toString());
             return;
         }
         
@@ -25,7 +25,9 @@ class Solution
         for(int i=0;i<key.length();i++)
         {
             char ch=key.charAt(i);
-            generateSeq(digits,keys,list,ans+ch,ind+1);
+            ans.append(ch);
+            generateSeq(digits,keys,list,ans,ind+1);
+            ans.deleteCharAt(ans.length()-1);
         }
     }
 }
