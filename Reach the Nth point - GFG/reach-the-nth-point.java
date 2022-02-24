fe140152213a8b19846b32059c6612e907443951
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-02-24 22:49:29 +0530

    Added solution - LeetHub

---
+++ Reach the Nth point - GFG/reach-the-nth-point.java
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.nthPoint(n);
+            System.out.println(ans);            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int nthPoint(int n)
+    {
+        int dp[]=new int[n+1];
+        
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+        }
+        return dp[n]%1000000007;
+    }
+}
