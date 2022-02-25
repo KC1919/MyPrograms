9d8fdd1b8b898a637d4e3a6dfca8045f14c3cfa3
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-02-25 22:57:41 +0530

    Time: 173 ms (7.43%), Space: 41.2 MB (46.49%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public int maxi=0;
+    public int getMaximumGold(int[][] a) 
+    {
+        int n=a.length;
+        int m=a[0].length;
+        
+        maxi=0;
+        
+        boolean visited[][]=new boolean[n][m];
+        int dir[][]={{-1,0},{1,0},{0,1},{0,-1}};
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(a[i][j]!=0)
+                solve(a,i,j,n,m,visited,dir);
+            }
+        }
+        return maxi;
+    }
+    
+    public int solve(int a[][], int i, int j, int n, int m, boolean visited[][], int dir[][])
+    {
+        if(i<0 || i>=n || j<0 || j>=m || a[i][j]==0 || visited[i][j]==true)
+            return 0;
+        
+        int sum=0;
+        int max=0;
+        
+        visited[i][j]=true;
+        
+        for(int k=0;k<dir.length;k++)
+        {
+            int row=i+dir[k][0];
+            int col=j+dir[k][1];
+            max=Math.max(max,solve(a,row,col,n,m,visited,dir));
+        }
+        visited[i][j]=false;
+        max+=a[i][j];
+        maxi=Math.max(maxi,max);
+        return max;
+    }
+}
