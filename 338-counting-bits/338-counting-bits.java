b40a4667a886fa645a64baafc386965ef3b2ede2
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-01 15:49:22 +0530

    Time: 3 ms (57.63%), Space: 48.7 MB (10.44%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -3,26 +3,34 @@ class Solution {
     {
         int ans[]=new int[n+1];
         
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        
         for(int i=0;i<=n;i++)
         {
-            int res=binary(i);
+            int res=binary(i,dp);
             ans[i]=res;
         }
         return ans;
     }
     
-    public int binary(int n)
+    public int binary(int n, int dp[])
     {
         int rem=0, num=n, count=0;
         
         while(num>0)
         {
+            if(dp[num]>=0){
+                count+=dp[num];
+                break;
+            }
             rem=num%2;
             if(rem==1)
                 count++;
             
             num=num/2;
         }
+        dp[n]=count;
         return count;
     }
 }
