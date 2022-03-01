aed01e06f7a8a22a268f12a01608664c1eed4c6c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-01 16:33:37 +0530

    Time: 39 ms (18.51%), Space: 104.7 MB (52.74%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstMissingPositive(int[] a) {
+        
+        HashSet<Integer>hm=new HashSet<>();
+        int mini=Integer.MAX_VALUE;
+        
+        for(int e:a){
+            if(e>=0 && e<mini)
+                mini=e;
+            
+            hm.add(e);
+        }
+        
+        if(mini>1 || mini==Integer.MAX_VALUE)
+            return 1;
+        
+        int min=Integer.MAX_VALUE;
+        for(int ele:a)
+        {
+            if(ele>=0){
+                int lm=ele-1;
+                int rm=ele+1;
+                
+                if(lm>0 && !hm.contains(lm))
+                    min=Math.min(min,lm);
+                
+                if(rm>0 &&!hm.contains(rm))
+                    min=Math.min(min,rm);
+            }
+        }
+        return min;
+    }
+}
