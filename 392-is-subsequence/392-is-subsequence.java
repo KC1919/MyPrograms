02743482241a831b5a176758640735559abf425b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-03 00:16:02 +0530

    Time: 1 ms (88.36%), Space: 42.4 MB (12.34%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -7,7 +7,7 @@ class Solution {
         if(s.length()==0 || t.length()==0)
             return true;
         
-        HashMap<Character,Integer>hm=new HashMap<>();
+        // HashMap<Character,Integer>hm=new HashMap<>();
         
         int count=0;
         int j=0;
@@ -19,18 +19,20 @@ class Solution {
             
             if(s.charAt(j)==t.charAt(i))
             {
-                if(j==0){
-                    hm.put(s.charAt(j),i);
-                    count++;
-                    j++;
-                }
-                else if(j>0 && i>hm.get(s.charAt(j-1)))
-                {
-                    hm.put(s.charAt(j),i);
-                    count++;
-                    j++;
-                }
-                else return false;
+                // if(j==0){
+                //     hm.put(s.charAt(j),i);
+                //     count++;
+                //     j++;
+                // }
+                // else if(j>0 && i>hm.get(s.charAt(j-1)))
+                // {
+                //     hm.put(s.charAt(j),i);
+                //     count++;
+                //     j++;
+                // }
+                count++;
+                j++;
+                // else return false;
             }
         }
         if(count==s.length())
