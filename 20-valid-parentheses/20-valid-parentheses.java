e9b8a17e63681b06dd9148e2d1bf05468c91cd6f
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-13 15:05:46 +0530

    Time: 2 ms (81.09%), Space: 42.7 MB (6.72%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s==null || s.length()==0)return true;
+        
+        Stack<Character>st=new Stack<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            
+            if(ch=='(' || ch=='[' || ch=='{')
+                st.push(ch);
+            
+            else if(ch==')')
+            {
+                if(st.size()==0 || st.peek()!='(')
+                    return false;
+                
+                st.pop();
+            }
+            
+            else if(ch==']')
+            {
+                if(st.size()==0 || st.peek()!='[')
+                    return false;
+                
+                st.pop();
+            }
+            
+            else if(ch=='}')
+            {
+                if(st.size()==0 || st.peek()!='{')
+                    return false;
+                
+                st.pop();
+            }
+        }
+        if(st.size()!=0)return false;
+        
+        return true;
+    }
+}
