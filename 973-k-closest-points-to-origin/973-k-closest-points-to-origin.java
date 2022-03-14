c5af29f55f9dec241ad363fc56599c759a30a779
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-14 12:19:31 +0530

    Time: 48 ms (52.46%), Space: 117.5 MB (36.41%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -11,25 +11,9 @@ class Solution {
             this.dist=dist;
         }
     }
-    
     public int[][] kClosest(int[][] points, int k) 
     {
-        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->{
-            if((int)a.dist==(int)b.dist)
-            {
-                double d1=a.dist-(int)a.dist;
-                double d2=b.dist-(int)b.dist;
-                
-                if(d1<d2)
-                    return 1;
-                else if(d1>d2)
-                    return -1;
-                else
-                    return 0;
-            }
-            else
-                return (int)b.dist-(int)a.dist;
-        });
+        Pair dp[]=new Pair[points.length];
         
         for(int i=0;i<points.length;i++)
         {
@@ -38,19 +22,32 @@ class Solution {
             
             double dist=Math.sqrt((int)Math.pow(x-0,2)+(int)Math.pow(y-0,2));
             
-            pq.add(new Pair(i,dist));
-            
-            if(pq.size()>k)
-                pq.remove();
+            dp[i]=new Pair(i,dist);
         }
+        
+        Arrays.sort(dp,(a,b)->{
+            if((int)a.dist==(int)b.dist)
+            {
+                double d1=a.dist-(int)a.dist;
+                double d2=b.dist-(int)b.dist;
+                
+                if(d1<d2)
+                    return -1;
+                else if(d1>d2)
+                    return 1;
+                else
+                    return 0;
+            }
+            else
+                return (int)a.dist-(int)b.dist;
+        });
         
         int ans[][]=new int[k][2];
         
         for(int i=0;i<k;i++)
         {
-            Pair p=pq.remove();
-            ans[i][0]=points[p.ind][0];
-            ans[i][1]=points[p.ind][1];
+            ans[i][0]=points[dp[i].ind][0];
+            ans[i][1]=points[dp[i].ind][1];
         }
         return ans;
     }
