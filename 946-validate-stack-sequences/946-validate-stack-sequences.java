cb760668f5b806064b0709eb668d82204f5fa92f
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-16 15:34:49 +0530

    Time: 3 ms (83.38%), Space: 42.3 MB (71.23%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -5,17 +5,24 @@ class Solution {
         
         Stack<Integer>st=new Stack<>();
         
+        HashSet<Integer>hm=new HashSet<>();
+        
         while(i<pushed.length && j<popped.length)
         {
             if(st.size()>0 && popped[j]==st.peek())
             {
                     st.pop();
+                    hm.remove(popped[j]);
                     j++;
             }
             
             else if(pushed[i]!=popped[j])
-            {       
+            {
+                if(st.size()>0 && st.peek()!=popped[j] && hm.contains(popped[j]))
+                    return false;
+                
                 st.push(pushed[i]);
+                hm.add(pushed[i]);
                 i++;
             }
             else if(pushed[i]==popped[j])
