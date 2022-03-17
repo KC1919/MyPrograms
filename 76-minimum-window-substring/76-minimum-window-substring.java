ba74797db50cf7ceee6d49d4062a911681e62a85
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-18 00:28:33 +0530

    Time: 47 ms (17.89%), Space: 47.6 MB (30.15%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,11 +1,11 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        
+    public String minWindow(String s, String t) 
+    {
         if(t.length()>s.length())
             return "";
         
+        HashMap<Character, Integer>hm=new HashMap<>();
         HashMap<Character,Integer>tm=new HashMap<>();
-        HashMap<Character,Integer>hm=new HashMap<>();
         
         for(int i=0;i<t.length();i++)
         {
@@ -13,62 +13,39 @@ class Solution {
             tm.put(ch,tm.getOrDefault(ch,0)+1);
         }
         
-        int start=0, end=0;
-        
+        int count=0, max=Integer.MAX_VALUE, si=-1, ei=-1;
         int j=0;
-        int count=0;
-        int min=Integer.MAX_VALUE;
         
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
             
-            if(tm.containsKey(ch))
-            {
-                hm.put(ch,hm.getOrDefault(ch,0)+1);
-                if(hm.get(ch)<=tm.get(ch))
-                {
-                    count+=1;
-                }
-            }
-            else
-            {
-                hm.put(ch,hm.getOrDefault(ch,0)+1);
-            }
+            if(tm.containsKey(ch) && hm.get(ch)<=tm.get(ch))
+                count++;
             
             while(j<s.length() && count==t.length())
             {
-                if(i-j+1<min)
+                if((i-j+1)<max)
                 {
-                     min=i-j+1;
-                     start=j;
-                     end=i;
+                    max=i-j+1;
+                    si=j;
+                    ei=i;
                 }
-                        
+                
                 char rem=s.charAt(j);
-                if(tm.containsKey(rem))
-                {
-                    if(hm.containsKey(rem))
-                    hm.put(rem,hm.get(rem)-1);
-                            
-                    if(hm.get(rem)==0)
-                    {
-                        hm.remove(rem);
-                        count-=1;
-                    }
-                            
-                    else if(hm.get(rem)<tm.get(rem))
-                    {
-                        count-=1;
-                    }
-                }
-                else
-                {
-                    hm.remove(rem);
-                }
+                
+                hm.put(rem,hm.get(rem)-1);
+                
+                if(tm.containsKey(rem) && hm.get(rem)<tm.get(rem))
+                    count--;
+                
                 j++;
             }
         }
-        return min==Integer.MAX_VALUE?"":s.substring(start,end+1);
+    
+        if(si<0 || ei<0)return "";
+        
+        return s.substring(si,ei+1);
     }
 }
