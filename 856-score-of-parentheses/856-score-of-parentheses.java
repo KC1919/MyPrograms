5e1641f187d1daf34d2ce01f376670d4d6d91c8c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-18 20:26:42 +0530

    Time: 1 ms (68.63%), Space: 40.2 MB (71.60%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,63 +1,42 @@
-class Solution 
-{
-    public int scoreOfParentheses(String s) 
-    {
-        Stack<String>st=new Stack<>();
+class Solution {
+    public int scoreOfParentheses(String s) {
         
-        Stack<Integer>ss=new Stack<>();
+        if(s.length()==2)return 1;
+        
+        Stack<String>st=new Stack<>();
+        int count=0;
         
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
             
             if(ch=='(')
-            {
                 st.push("(");
-            }
             
             else if(ch==')')
             {
-                if(st.size()>0)
-                {
+                if(st.peek().equals("(")){
+                    st.pop();
+                    st.push(1+"");
+                }
+                   
+                else{
                     int sum=0;
-                    
-                    if(!st.peek().equals("("))
+                    while(st.size()>0 && !st.peek().equals("("))
                     {
-                        while(st.size()>0 && !st.peek().equals("("))
-                        {
-                            st.pop();
-                            sum+=ss.pop();
-                        }
-                        if(st.size()==0)
-                        {
-                            st.push(sum+"");
-                            ss.push(sum);
-                        }
-                        else
-                        {
-                            st.pop();
-                            st.push((2*sum)+"");
-                            ss.push(2*sum);
-                        }
+                        sum+=Integer.parseInt(st.pop());
                     }
-                    else
-                    {
-                        st.pop();
-                        st.push(1+"");
-                        ss.push(1);
-                    }
+
+                    sum=2*sum;
+                    st.pop();
+                    st.push(sum+"");
                 }
             }
-        }   
-        
-        int res=0;
-        
-        while(ss.size()!=0)
-        {
-            res+=ss.pop();
         }
         
-        return res;
+        while(st.size()>0)
+            count+=Integer.parseInt(st.pop());
         
+        return count;
     }
 }
