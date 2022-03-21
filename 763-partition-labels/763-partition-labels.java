dfe47557fc018035bed40877c78988361961b424
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-21 14:32:44 +0530

    Time: 5 ms (71.86%), Space: 42.3 MB (67.48%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) 
+    {
+        
+        HashMap<Character,Integer>hm=new HashMap<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            hm.put(ch,i);
+        }
+        
+        List<Integer>list=new ArrayList<>();
+        int start=0, end=0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            
+            if(hm.get(ch)>end)
+                end=hm.get(ch);
+            
+            if(i==end)
+            {
+                int span=end-start+1;
+                list.add(span);
+                start=i+1;
+            }
+        }
+        
+        return list;
+    }
+}
