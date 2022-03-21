11fdfe093e291f725f4473e178566eb6b3306bbe
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-21 14:36:56 +0530

    Time: 7 ms (48.01%), Space: 43.1 MB (27.69%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -4,8 +4,12 @@ class Solution {
         
         HashMap<Character,Integer>hm=new HashMap<>();
         
-        for(int i=0;i<s.length();i++){
+        for(int i=s.length()-1;i>=0;i--){
             char ch=s.charAt(i);
+            
+            if(hm.containsKey(ch))
+                continue;
+            
             hm.put(ch,i);
         }
         
