1cb7aa17156a864a20506df18d65915ceba0ba53
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-24 00:18:42 +0530

    Time: 16 ms (68.38%), Space: 49 MB (53.23%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,44 +1,52 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character,Integer>hm=new HashMap<>();
+        
+        int[] count = new int[26];
 		
 		Queue<Integer>que=new LinkedList<>();
 		
 		for(int i=0;i<s.length();i++)
 		{
 		    char ch=s.charAt(i);
-		    
-		    if(!hm.containsKey(ch)){
-		        hm.put(ch,1);
+		    int index = ch - 'a';
+            
+		    if(count[index]==0){
+		        count[index]++;
 		        que.add(i);
 		    }
 		    
 		    else
 		    {
-		        hm.put(ch,hm.get(ch)+1);
-		        
+		        count[index]++;
+	
 		        if(que.size()>0)
                 {
 		            if(s.charAt(que.peek())==ch){
 		                que.remove();
-                        hm.put(ch,hm.get(ch)-1);
+                        count[index]++;
                     }
 	
-                    while(que.size()>0 && hm.get(s.charAt(que.peek()))>1)
-                    {
-                        hm.put(s.charAt(que.peek()),hm.get(s.charAt(que.peek()))-1);
-                        que.remove();
-                    }
+                    else if(count[s.charAt(que.peek())-'a']>1)
+		            {
+                        int idx=s.charAt(que.peek())-'a';
+		                count[idx]--;
+		                que.remove();
+		            }
 		        }
 		    }
 		}
         
-        
+        while(que.size()>0 && count[s.charAt(que.peek())-'a']>1)
+        {
+		    int idx=s.charAt(que.peek())-'a';
+		    count[idx]--;
+		    que.remove();
+		}
         
         if(que.size()==0)
         return -1;
         
         else
-		return (hm.get(s.charAt(que.peek()))>1)?-1:que.peek();
+		return (count[s.charAt(que.peek())-'a']>1)?-1:que.peek();
     }
 }
