88acec4285150ec42b41d3c2ea7eecf6c38c113e
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-23 17:33:09 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (59.76%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.java
+++ 991-broken-calculator/991-broken-calculator.java
@@ -2,11 +2,12 @@ class Solution {
     public int brokenCalc(int startValue, int target) {
         int ans = 0;
         while (target > startValue) {
-            ans++;
             if (target % 2 == 1)
                 target++;
             else
                 target /= 2;
+            
+            ans++;
         }
 
         return ans + startValue - target;
