ae8bf8ccf58f3b36be9e6a35b0be48c4008357e2
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-25 15:18:29 +0530

    Time: 2 ms (78.58%), Space: 41.7 MB (72.65%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,36 +1,36 @@
 class Solution {
-    public int twoCitySchedCost(int[][] costs) {
-        int n = costs.length; // number of people
-        int[][] memo = new int[(n/2) + 1][(n/2) + 1]; // n/2 is capacity of each city
-        return dfs(n - 1, n / 2, n / 2, costs, memo);
-    }
     
-    int dfs(int index, int num_a, int num_b, int[][] costs, int[][] memo) {
-        // terminate
-        if (index < 0) {
-            return 0;
+    class Pair
+    {
+        int val;
+        int idx;
+        
+        Pair(int val, int idx)
+        {
+            this.val=val;
+            this.idx=idx;
         }
-        
-        if (memo[num_a][num_b] > 0) return memo[num_a][num_b];
-
-        int[] currCosts = costs[index];
-        int ans = 0;
-        
-        // constraints
-        if (num_a == 0) {
-            ans = currCosts[1] + dfs(index - 1, num_a, num_b - 1, costs, memo); 
-        } else if (num_b == 0) {
-            ans = currCosts[0] + dfs(index - 1, num_a - 1, num_b, costs, memo);
-        } else {
-            int min_b = currCosts[1] + dfs(index - 1, num_a, num_b - 1, costs, memo);
-            int min_a = currCosts[0] + dfs(index - 1, num_a - 1, num_b, costs, memo);
-            ans = Math.min(min_b, min_a);
-        }
-        
-        memo[num_a][num_b] = ans;
-        return memo[num_a][num_b];
     }
-    
-    
-    
+    public int twoCitySchedCost(int[][] a) {
+        
+        Pair diff[]=new Pair[a.length];
+        
+        for(int i=0;i<a.length;i++)
+            diff[i]=new Pair(a[i][1]-a[i][0],i);
+        
+        Arrays.sort(diff,(b,c)->{
+            return b.val-c.val;
+        });
+        
+        int k=a.length/2;
+        int sum=0;
+        
+        for(int i=0;i<k;i++)
+            sum+=a[diff[i].idx][1];
+        
+        for(int i=k;i<a.length;i++)
+            sum+=a[diff[i].idx][0];
+        
+        return sum;
+    }
 }
