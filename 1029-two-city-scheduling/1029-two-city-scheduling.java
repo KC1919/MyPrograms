612090c8a7934d8455550526280ef7beaf59133f
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-25 15:12:51 +0530

    Time: 3 ms (45.38%), Space: 42 MB (60.48%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    class Pair
+    {
+        int val;
+        int idx;
+        
+        Pair(int val, int idx)
+        {
+            this.val=val;
+            this.idx=idx;
+        }
+    }
+    public int twoCitySchedCost(int[][] a) {
+        
+        Pair diff[]=new Pair[a.length];
+        
+        for(int i=0;i<a.length;i++)
+        {
+            diff[i]=new Pair(a[i][1]-a[i][0],i);
+        }
+        
+        Arrays.sort(diff,(b,c)->{
+            return b.val-c.val;
+        });
+        
+        int k=a.length/2;
+        
+        int sum=0;
+        
+        for(int i=0;i<k;i++)
+        {
+            sum+=a[diff[i].idx][1];
+        }
+        
+        for(int i=k;i<a.length;i++)
+        {
+            sum+=a[diff[i].idx][0];
+        }
+        
+        return sum;
+    }
+}
