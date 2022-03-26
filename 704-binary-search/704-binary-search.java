711b8168b045962b946275e8ff36db931541333f
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-26 14:47:58 +0530

    Time: 0 ms (100.00%), Space: 54.7 MB (26.81%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] a, int target) 
+    {
+        int l=0, h=a.length-1;
+        
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            
+            if(target>a[mid])
+                l=mid+1;
+            
+            else if(target<a[mid])
+                h=mid-1;
+            
+            else
+                return mid;
+        }
+        
+        return -1;
+    }
+}
