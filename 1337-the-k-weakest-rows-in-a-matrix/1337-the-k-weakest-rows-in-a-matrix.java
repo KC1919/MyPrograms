bd83307702b17d3130323a93fba863f7109f01fe
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-27 11:20:51 +0530

    Time: 5 ms (24.47%), Space: 48.8 MB (40.02%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,41 +1,35 @@
-class Solution {
+class Solution 
+{
+    class Pair
+    {
+        int count;
+        int row;
+        Pair(int count, int row)
+        {
+            this.count=count;
+            this.row=row;
+        }
+    }
+    
     public int[] kWeakestRows(int[][] mat, int k) 
     {
-        class Pair
-        {
-            int count;
-            int row;
-            Pair(int count, int row)
-            {
-                this.count=count;
-                this.row=row;
-            }
-        }
         PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->{
-            int c1=a.count, i1=a.row;
-            int c2=b.count, i2=b.row;
-            
-            if(c1==c2)
+            if(a.count==b.count)
                 return b.row-a.row;
             return b.count-a.count;});
-        
         
         for(int i=0;i<mat.length;i++)
         {
             int c=0;
-            int j=0, m=mat[i].length-1;
-            
-            while(j<=m && mat[i][j]!=mat[i][m])
+            for(int j=0;j<mat[i].length;j++)
             {
-                c++;
-                m--;
-                j++;
+                if(mat[i][j]==1)
+                    c++;
+                else
+                    break;
             }
-            if(mat[i][j]==0)
-                pq.add(new Pair(c,i));
-            else if(mat[i][m]==1)
-                pq.add(new Pair(m+1,i));
             
+            pq.add(new Pair(c,i));
             if(pq.size()>k)
                 pq.remove();
         }
@@ -44,9 +38,7 @@ class Solution {
         int p=k-1;
         
         while(pq.size()>0)
-        {
             res[p--]=pq.remove().row;
-        }
         
         return res;
     }
