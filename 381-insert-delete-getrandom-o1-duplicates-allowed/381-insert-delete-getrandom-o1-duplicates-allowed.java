4aefe2e97fb6259596eef931eb2a289966385cea
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-29 00:18:27 +0530

    Time: 50 ms (71.85%), Space: 98.4 MB (26.94%) - LeetHub

--- 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.java
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.java
@@ -1,61 +1,38 @@
-class RandomizedCollection {
-    
-    HashMap<Integer,HashSet<Integer>>hm;
-    ArrayList<Integer>list;
-    Random r;
+public class RandomizedCollection {
+    ArrayList<Integer> lst;
+    HashMap<Integer, Set<Integer>> idx;
+    java.util.Random rand = new java.util.Random();
+    /** Initialize your data structure here. */
+
     public RandomizedCollection() {
-        r=new Random();
-        hm=new HashMap<>();
-        list=new ArrayList<>();
+        lst = new ArrayList<Integer>();
+        idx = new HashMap<Integer, Set<Integer>>();
     }
-    
+
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     public boolean insert(int val) {
-        if(hm.containsKey(val) && hm.get(val).size()>0)
-        {
-            list.add(val);
-            hm.get(val).add(list.size()-1);
-            return false;
-        }
-        
-        else
-        {
-            list.add(val);
-            hm.put(val,new HashSet<>());
-            hm.get(val).add(list.size()-1);
-            return true;
-        }
+        if (!idx.containsKey(val)) idx.put(val, new LinkedHashSet<Integer>());
+        idx.get(val).add(lst.size());
+        lst.add(val);
+        return idx.get(val).size() == 1;
     }
-    
+
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     public boolean remove(int val) {
-        if(!hm.containsKey(val) || hm.get(val).size()==0)
-            return false;
-        
-        int vidx=hm.get(val).iterator().next();
-        int lidx=list.size()-1;
-        hm.get(val).remove(vidx);
-            
-        int lval=list.get(lidx);
-            
-        list.set(vidx,lval);
-            
-        hm.get(lval).add(vidx);
-        hm.get(lval).remove(lidx);
-        list.remove(lidx);
-            
+        if (!idx.containsKey(val) || idx.get(val).size() == 0) return false;
+        int remove_idx = idx.get(val).iterator().next();
+        idx.get(val).remove(remove_idx);
+        int last = lst.get(lst.size() - 1);
+        lst.set(remove_idx, last);
+        idx.get(last).add(remove_idx);
+        idx.get(last).remove(lst.size() - 1);
+
+        lst.remove(lst.size() - 1);
         return true;
-        
     }
-    
+
+    /** Get a random element from the collection. */
     public int getRandom() {
-        int ridx=r.nextInt(list.size());
-        return list.get(ridx);
+        return lst.get(rand.nextInt(lst.size()));
     }
 }
-
-/**
- * Your RandomizedCollection object will be instantiated and called as such:
- * RandomizedCollection obj = new RandomizedCollection();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
