0c8eee02f8f943a9843e777e0f312ca0c7f5fd51
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-03-28 19:08:09 +0530

    Time: 1 ms (81.31%), Space: 43.2 MB (75.28%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -5,36 +5,23 @@ class Solution {
         
         while(l<=h)
         {
-            while (l < h && a[l] == a[l + 1])
-                ++l;
-            while (l < h && a[h] == a[h - 1])
-                --h;
+            while (l < h && a[l] == a[l + 1]) l++;
+            while (l < h && a[h] == a[h - 1]) h--;
             
             int mid=(l+h)/2;
             
-            if(a[mid]==target)
-            {
-                return true;
-            }
+            if(a[mid]==target) return true;
             
             else if(a[l]<=a[mid])
             {
-                if(target>=a[l] && target<a[mid])
-                {
-                    h=mid-1;
-                }
-                else
-                    l=mid+1;
+                if(target>=a[l] && target<a[mid]) h=mid-1;
+                else l=mid+1;
             }
             
             else
             {
-                if(target>a[mid] && target<=a[h])
-                {
-                    l=mid+1;
-                }
-                else
-                    h=mid-1;
+                if(target>a[mid] && target<=a[h]) l=mid+1;
+                else h=mid-1;
             } 
         }
         return false;
