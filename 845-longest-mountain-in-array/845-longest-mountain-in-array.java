2d6bbd6989448f5228db7fb3cde209c16285441d
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-02 23:59:18 +0530

    Time: 2 ms (94.20%), Space: 43 MB (93.79%) - LeetHub

---
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int longestMountain(int[] a) {
+        
+        int lc=0, rc=0, max=0;
+        
+        for(int i=0;i<a.length;i++)
+        {
+            if(i>0 && a[i]>a[i-1])
+            {
+                if(rc!=0)
+                {
+                    lc=0;
+                    rc=0;
+                }
+                lc++;
+            }
+            
+            else if(i>0 && a[i]<a[i-1])
+            {
+                if(lc!=0)
+                    rc++;
+                else
+                {
+                    lc=0;
+                    rc=0;
+                }
+            }
+            else if(i>0 && a[i]==a[i-1])
+            {
+                lc=0;
+                rc=0;
+            }
+            
+            if(lc>0 && rc>0)max=Math.max(lc+rc+1,max);
+        }
+        
+        return max<3?0:max;
+    }
+}
