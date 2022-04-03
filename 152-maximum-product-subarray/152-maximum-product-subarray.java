c53673232915523391a091476138fe7f38f142f7
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-03 15:07:56 +0530

    Time: 1 ms (97.46%), Space: 42.4 MB (89.73%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,29 +1,14 @@
 class Solution {
-    class Pair
-    {
-        int prev;
-        int curr;
-        
-        Pair(int prev, int curr)
-        {
-            this.prev=prev;
-            this.curr=curr;
-        }
-    }
     
     public int maxProduct(int[] a) {
         
         int max=0;
         
-        // Pair dp[]=new Pair[a.length];
-        
-        // dp[0]=new Pair(0,a[0]);
         max=a[0];
         int oldPrev=0, oldCurr=a[0];
         
         for(int i=1;i<a.length;i++)
         {
-            // Pair p=dp[i-1];
             int newPrev=0, newCurr=0;
             
             if(oldPrev<0 && oldCurr<0)
@@ -36,7 +21,6 @@ class Solution {
             {
                 newPrev=oldPrev*a[i];
                 newCurr=oldCurr*a[i];
-                // dp[i]=new Pair(p.prev*a[i],p.curr*a[i]);
             }
             
             else if(oldPrev==0 || oldCurr==0)
@@ -44,28 +28,24 @@ class Solution {
                 if(oldPrev==0 && oldCurr==0){
                     newPrev=a[i];
                     newCurr=a[i];
-                    // dp[i]=new Pair(a[i],a[i]);
                 }
                     
                 else if(oldPrev==0){
                     newPrev=oldCurr*a[i];
                     newCurr=a[i];
-                    // dp[i]=new Pair(oldCurr*a[i],a[i]);
                 }
                 
                 else if(oldCurr==0){
                     newPrev=oldPrev*a[i];
                     newCurr=a[i];
-                    // dp[i]=new Pair(p.prev*a[i],a[i]);
                 }
-                    
             }
             else
             {
                 newPrev=oldPrev*a[i];
                 newCurr=oldCurr*a[i];
-                // dp[i]=new Pair(p.prev*a[i],p.curr*a[i]);
             }
+            
             oldPrev=newPrev;
             oldCurr=newCurr;
             max=Math.max(max,Math.max(newCurr,newPrev));
