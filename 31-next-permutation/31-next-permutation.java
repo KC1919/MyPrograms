12e429bd0737081c9154ed6d51e4c674b604d9c0
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-03 11:03:03 +0530

    Time: 1 ms (85.00%), Space: 43.8 MB (41.82%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void nextPermutation(int[] a) 
+    {
+        int ind=-1;
+        
+        for(int i=a.length-1;i>0;i--)
+        {
+            if(a[i]>a[i-1])
+            {
+                ind=i-1;
+                break;
+            }
+        }
+        
+        if(ind==-1){
+            reverse(a,0,a.length-1);
+            return;
+        }
+        
+        int idx=-1, min=Integer.MAX_VALUE;
+        
+        for(int i=a.length-1;i>ind;i--)
+        {
+            if(a[i]>a[ind] && (a[i]-a[ind])<min)
+            {
+                min=a[i]-a[ind];
+                idx=i;
+            }
+        }
+        int temp=a[ind];
+        a[ind]=a[idx];
+        a[idx]=temp;
+        
+        reverse(a,ind+1,a.length-1);
+    }
+    
+    public void reverse(int a[], int start, int end)
+    {
+        while(start<end)
+        {
+            int temp=a[start];
+            a[start]=a[end];
+            a[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
