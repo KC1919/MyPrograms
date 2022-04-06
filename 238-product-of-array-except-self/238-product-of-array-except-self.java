ff1548b01e15b1321073027d7709ebb42ef12744
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-06 18:55:57 +0530

    Time: 1 ms (100.00%), Space: 50.5 MB (87.23%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] productExceptSelf(int[] a) {
         int n=a.length;
-        int pre[]=new int[n];
-        int post[]=new int[n];
+        int ans[]=new int[n];
+        
         
         int prefix=1;
         
         for(int i=0;i<n;i++)
         {
-            pre[i]=prefix;
+            ans[i]=prefix;
             prefix*=a[i];
         }
         
@@ -16,17 +16,9 @@ class Solution {
         
         for(int i=n-1;i>=0;i--)
         {
-            post[i]=postfix;
+            ans[i]*=postfix;
             postfix*=a[i];
         }
-        
-        int ans[]=new int[n];
-        
-        for(int i=0;i<n;i++)
-        {
-            ans[i]=pre[i]*post[i];
-        }
-        
         return ans;
     }
 }
