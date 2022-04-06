88c3dc6f9f94065ba5b4c508627d804f6f4cacf7
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-06 20:42:06 +0530

    Time: 2 ms (81.81%), Space: 57.4 MB (73.90%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -3,19 +3,21 @@ class Solution {
         int n=a.length;
         int ans[]=new int[n];
         
-        
+        //Here first we calculate the prefix product for every ith position
         int prefix=1;
         
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             ans[i]=prefix;
             prefix*=a[i];
         }
         
+        //then we start traversing the array from the end, and take postfix product, when we reach the ith position we have with us the postfix product we calculated till ith position, So we multiply the prefix product already stored at ith position with the postfix product and we store the result at the ith position.
+        
+        //using this strategy, we are able to save space of storing prefix and postfix in seperate arrays.
+        
         int postfix=1;
         
-        for(int i=n-1;i>=0;i--)
-        {
+        for(int i=n-1;i>=0;i--){
             ans[i]*=postfix;
             postfix*=a[i];
         }
