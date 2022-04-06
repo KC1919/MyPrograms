dc2786b53e1eeb1893c522dd55057d89a750f417
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-06 18:45:25 +0530

    Added solution - LeetHub

---
+++ Product array puzzle - GFG/product-array-puzzle.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+        	int[] array = new int[n];
+        	for (int i=0; i<n ; i++ ) {
+        		array[i] = sc.nextInt();
+        	}
+            Solution ob = new Solution();
+            long[] ans = new long[n];
+            ans = ob.productExceptSelf(array, n); 
+
+           	for (int i = 0; i < n; i++) { 
+				System.out.print(ans[i]+" ");
+			} 
+            System.out.println();
+            t--;
+        }
+    } 
+} 
+  
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution 
+{ 
+	public static long[] productExceptSelf(int a[], int n) 
+	{ 
+        long pre[]=new long[n];
+        long post[]=new long[n];
+        
+        long prefix=1;
+        
+        for(int i=0;i<n;i++)
+        {
+            pre[i]=prefix;
+            prefix*=a[i];
+        }
+        
+        long postfix=1;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            post[i]=postfix;
+            postfix*=a[i];
+        }
+        
+        long ans[]=new long[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=pre[i]*post[i];
+        }
+        
+        return ans;
+	} 
+} 
