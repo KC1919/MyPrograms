3acb4eac58af548ac46d38f32840470f76e657c1
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-08 12:17:11 +0530

    Time: 1 ms (57.48%), Space: 42.5 MB (51.59%) - LeetHub

---
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumDifference(int[] a) {
+        
+        int max=-1, min=a[0];
+        
+        
+        for(int i=1;i<a.length;i++)
+        {
+            if(a[i]<=min)
+            {
+                min=a[i];
+            }
+            else
+                max=Math.max(a[i]-min,max);
+                
+        }
+        
+        return max;
+    }
+}
