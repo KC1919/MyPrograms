299435b051d89b36e832707f8d485465447ebcaf
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-09 23:37:52 +0530

    Time: 13 ms (45.68%), Space: 50.8 MB (85.47%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -1,29 +1,76 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int N = graph.length;
-        int[] color = new int[N];
-        List<Integer> ans = new ArrayList();
-
-        for (int i = 0; i < N; ++i)
-            if (dfs(i, color, graph))
-                ans.add(i);
-        return ans;
-    }
-
-    // colors: WHITE 0, GRAY 1, BLACK 2;
-    public boolean dfs(int node, int[] color, int[][] graph) {
-        if (color[node] > 0)
-            return color[node] == 2;
-
-        color[node] = 1;
-        for (int nei: graph[node]) {
-            if (color[node] == 2)
-                continue;
-            if (color[nei] == 1 || !dfs(nei, color, graph))
-                return false;
+        
+        int nodes=graph.length;
+        HashSet<Integer>hm=new HashSet<>();
+        Integer dp[]=new Integer[nodes];
+        
+        for(int i=0;i<graph.length;i++){
+            if(graph[i].length==0){
+                hm.add(i);
+                dp[i]=1;
+            }
         }
-
-        color[node] = 2;
-        return true;
+        
+        boolean visited[]=new boolean[nodes];
+        List<Integer>list=new ArrayList<>();
+        
+        for(int i=0;i<nodes;i++)
+        {
+            if(!hm.contains(i) && dp[i]==null){
+                boolean res=travelGraph(graph,i,visited,dp,hm);
+                dp[i]=res==false?0:1;
+            }
+        }
+        
+        
+        for(int i=0;i<nodes;i++)
+        {
+            if(dp[i]==1)
+                list.add(i);
+        }
+        
+        return list;
+    }
+    
+    public boolean travelGraph(int graph[][], int src, boolean visited[], Integer dp[], HashSet<Integer>hm)
+    {
+        if(hm.contains(src))
+            return true;
+        
+        if(dp[src]!=null)
+        {
+            if(dp[src]==0)
+                return false;
+            
+            return true;
+        }
+            
+        visited[src]=true;
+        
+        boolean myRes=true;
+        
+        int flag=0;
+        
+        for(int e:graph[src])
+        {
+            if(visited[e]==true)
+                return false;
+                
+            else if(visited[e]==false)
+            {
+                boolean res=travelGraph(graph,e,visited,dp,hm);
+                myRes=res&myRes;
+                
+                flag=1;
+            }
+        }
+        
+        if(flag==0)
+            myRes=false;
+        
+        dp[src]=myRes==false?0:1;
+        visited[src]=false;
+        return myRes;
     }
 }
