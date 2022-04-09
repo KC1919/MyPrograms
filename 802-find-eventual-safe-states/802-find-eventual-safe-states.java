caa40cc073b329a45036bc66bf172e21b081d01c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-09 23:04:22 +0530

    Time: 21 ms (42.33%), Space: 69.7 MB (58.34%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -1,40 +1,74 @@
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] G) {
-        int N = G.length;
-        boolean[] safe = new boolean[N];
-
-        List<Set<Integer>> graph = new ArrayList();
-        List<Set<Integer>> rgraph = new ArrayList();
-        for (int i = 0; i < N; ++i) {
-            graph.add(new HashSet());
-            rgraph.add(new HashSet());
-        }
-
-        Queue<Integer> queue = new LinkedList();
-
-        for (int i = 0; i < N; ++i) {
-            if (G[i].length == 0)
-                queue.offer(i);
-            for (int j: G[i]) {
-                graph.get(i).add(j);
-                rgraph.get(j).add(i);
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        
+        int nodes=graph.length;
+        HashSet<Integer>hm=new HashSet<>();
+        Integer dp[]=new Integer[nodes];
+        
+        for(int i=0;i<graph.length;i++){
+            if(graph[i].length==0){
+                hm.add(i);
+                dp[i]=1;
             }
         }
-
-        while (!queue.isEmpty()) {
-            int j = queue.poll();
-            safe[j] = true;
-            for (int i: rgraph.get(j)) {
-                graph.get(i).remove(j);
-                if (graph.get(i).isEmpty())
-                    queue.offer(i);
+        
+        boolean visited[]=new boolean[nodes];
+        
+        for(int i=0;i<nodes;i++)
+        {
+            if(!hm.contains(i) && dp[i]==null){
+                boolean res=travelGraph(graph,i,visited,dp,hm);
+                dp[i]=res==false?0:1;
             }
         }
-
-        List<Integer> ans = new ArrayList();
-        for (int i = 0; i < N; ++i) if (safe[i])
-            ans.add(i);
-
-        return ans;
+        List<Integer>list=new ArrayList<>();
+        
+        for(int i=0;i<nodes;i++)
+        {
+            if(dp[i]==1)
+                list.add(i);
+        }
+        
+        return list;
+    }
+    
+    public boolean travelGraph(int graph[][], int src, boolean visited[], Integer dp[], HashSet<Integer>hm)
+    {
+        if(hm.contains(src))
+            return true;
+        
+        if(dp[src]!=null){
+            if(dp[src]==0)
+                return false;
+            
+            return true;
+        }
+            
+        visited[src]=true;
+        
+        boolean myRes=true;
+        
+        int flag=0;
+        
+        for(int e:graph[src])
+        {
+            if(visited[e]==true)
+                return false;
+                
+            else if(visited[e]==false)
+            {
+                boolean res=travelGraph(graph,e,visited,dp,hm);
+                myRes=res&myRes;
+                
+                flag=1;
+            }
+        }
+        
+        if(flag==0)
+            myRes=false;
+        
+        dp[src]=myRes==false?0:1;
+        visited[src]=false;
+        return myRes;
     }
 }
