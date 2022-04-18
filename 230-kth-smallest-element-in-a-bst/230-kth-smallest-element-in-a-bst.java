f910604729778b20ba75f6e20383abe48ce9ba53
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-18 19:35:35 +0530

    Time: 1 ms (70.51%), Space: 42.1 MB (91.65%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -18,28 +18,20 @@ class Solution {
     public int kthSmallest(TreeNode node, int k) {
         Stack<TreeNode>st=new Stack<>();
         
-        st.push(node);
         
         while(true)
         {
             while(node!=null)
             {
-                node=st.peek();
-                
-                if(node.left!=null)
-                    st.push(node.left);
-                
+                st.push(node);
                 node=node.left;
             }
             
             node=st.pop();
             
-            if(k--==1){
+            if(--k==0){
                 return node.val;
             }
-            if(node.right!=null)
-                st.push(node.right);
-            
             node=node.right;
         }
     }
