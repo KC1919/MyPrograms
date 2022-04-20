9cc99a62936d9b25c54451dcd45b46d3d7913d72
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-04-20 14:38:22 +0530

    Time: 24 ms (41.43%), Space: 52.3 MB (13.10%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -14,29 +14,26 @@
  * }
  */
 class BSTIterator {
-	Stack<TreeNode> stack;
-	public BSTIterator(TreeNode root) {
-		stack = new Stack<>();
-		TreeNode node = root;
-		updateStack(node);                                      // update stack
-	}
-	public int next() {
-		TreeNode toRemove = stack.pop();
-		updateStack(toRemove.right);                             // before return node, first update stack further        
-		return toRemove.val;
-	}
-	public boolean hasNext() {
-		return !stack.isEmpty();    
-	}
-	// -------------------
-	public void updateStack(TreeNode node){
-		while(node != null){
-			stack.add(node);
-			node = node.left;
-		}
-	}
+    Queue<Integer> qu;
+    public BSTIterator(TreeNode root) {
+        qu=new LinkedList<>();
+        inOrder(root);
+    }
+    public void inOrder(TreeNode root){
+      if(root==null)return;
+      inOrder(root.left);
+      qu.add(root.val);
+      inOrder(root.right);
+    }
+    
+    public int next() {
+        return qu.remove();
+    }
+    
+    public boolean hasNext() {
+        return !qu.isEmpty();
+    }
 }
-
 /**
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator obj = new BSTIterator(root);
