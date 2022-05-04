58a7c251ba524af492b82516435ecd650592bcaa
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-04 23:57:12 +0530

    Time: 4 ms (47.66%), Space: 68.7 MB (58.32%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,12 +1,15 @@
 class Solution {
     public int longestOnes(int[] a, int k) {
+        
         int zc=0, max=0, count=0;
         int j=0;
         
         for(int i=0;i<a.length;i++)
         {
-            if(a[i]==1)
+            if(a[i]==1){
                 count++;
+                max=Math.max(max,count);
+            }
     
             else if(a[i]==0)
             {
@@ -23,11 +26,11 @@ class Solution {
                         count--;
                         j++;
                     }
+                    max=Math.max(max,count);
                 }
                 else
                     count=0;
             }
-            max=Math.max(max,count);
         }
         return max;
         
