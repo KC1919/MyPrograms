a6e61eeb4c9e259cb9fa100578d428a6e4873e0c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-04 23:07:56 +0530

    Time: 106 ms (5.15%), Space: 99.1 MB (5.16%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,18 +1,42 @@
 class Solution 
 {
-    public int maxOperations(int[] nums, int k) 
+    public int maxOperations(int[] a, int k) 
     {
-        HashMap<Integer,Integer>map=new HashMap<>();
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        int n=a.length;
+            
+        for(int i=0;i<n;i++)
+        {
+            hm.put(a[i],hm.getOrDefault(a[i],0)+1);
+        }
+        
         int count=0;
-        for(int i=0;i<nums.length;i++){
-            //to check if that k-nums[i] present and had some value left or already paired
-            if(map.containsKey(k-nums[i])&&map.get(k-nums[i])>0){
-                count++;
-                map.put(k-nums[i],map.get(k-nums[i])-1);
-            }else{
-                //getOrDefault is easy way it directly checks if value is 0 returns 0 where I added 1
-                //and if some value is present then it return that value "similar to map.get(i)" and I added 1 on it 
-                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        for(int i=0;i<n;i++)
+        {
+            int comp=k-a[i];
+            
+            if(hm.containsKey(comp))
+            {
+                int freq=hm.get(a[i]);
+                int cfreq=hm.get(comp);
+                
+                if(a[i]==comp)
+                {
+                    if(freq>1)
+                    {
+                        count++;
+                        hm.put(a[i],freq-2);
+                    }
+                }
+                else
+                {
+                    if(freq>0 && cfreq>0)
+                    {
+                        count++;
+                        hm.put(a[i],freq-1);
+                        hm.put(comp,cfreq-1);
+                    }
+                }
             }
         }
         return count;
