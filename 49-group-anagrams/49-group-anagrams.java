db8ccecaa2dc9a8d8deaa29c3c9a86bcb35b65a0
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-07 23:03:13 +0530

    Time: 6 ms (94.23%), Space: 45.3 MB (90.40%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,14 +1,31 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) return new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            char[] ca = new char[26];
-            for (char c : s.toCharArray()) ca[c - 'a']++;
-            String keyStr = String.valueOf(ca);
-            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
-            map.get(keyStr).add(s);
+    public List<List<String>> groupAnagrams(String[] s) 
+    {
+        int count=0;
+        int n=s.length;
+        
+        List<List<String>>list=new ArrayList<>();
+        HashMap<String,Integer>hm=new HashMap<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            String word=s[i];
+            char sortWord[]=word.toCharArray();
+            Arrays.sort(sortWord);
+            String val=String.valueOf(sortWord);
+            
+            if(hm.containsKey(val)){
+                list.get(hm.get(val)).add(word);
+            }
+            else{
+                hm.put(val,count);
+                if(count==list.size()){
+                    list.add(new ArrayList<>());
+                }
+                list.get(count).add(word);
+                count++;
+            }
         }
-        return new ArrayList<>(map.values());
-    } 
+        return list;
+    }
 }
