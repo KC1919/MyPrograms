a42c4614127106cccc57ad7b2357101db825e10b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-08 16:59:35 +0530

    Time: 6 ms (55.54%), Space: 43.6 MB (59.29%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -3,13 +3,16 @@ class Solution {
         
         StringBuilder sb=new StringBuilder();
         
+        //firstly iterate through the whole string
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
             
+            //append only alphanumeric characters
             if(Character.isDigit(ch) || (ch>=97 && ch<=122))
                 sb.append(ch);
             
+            //append only alphanumeric characters,if capital convert them to small then append
             else if(ch>=65 && ch<=90){
                 sb.append(Character.toLowerCase(ch));
             }
@@ -17,14 +20,15 @@ class Solution {
         
         int left=0, right=sb.length()-1;
         
+        //check for palindrom
         while(left<right)
         {
-            if(sb.charAt(left)!=sb.charAt(right))
-                return false;
+            if(sb.charAt(left)!=sb.charAt(right)) //if any character mismatch
+                return false;    //return false, means not palindrome
             
             left++;
             right--;
         }
-        return true;
+        return true;  //is palindrome
     }
 }
