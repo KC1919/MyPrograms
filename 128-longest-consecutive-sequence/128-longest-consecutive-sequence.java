461ac64d61ab8f1122ed46ded3dd56d58c19a8e3
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-08 14:26:25 +0530

    Time: 28 ms (71.99%), Space: 62.1 MB (39.64%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -8,25 +8,25 @@ class Solution {
             hm.put(a[i],0);
         }
         
-        List<Integer>list=new ArrayList<>();
-        
         for(int i=0;i<a.length;i++)
         {
             if(!hm.containsKey(a[i]-1)){
-                // hm.put(a[i],1);
-                list.add(a[i]);
+                hm.put(a[i],1);
             }
         }
+        
         int max=0;
-        for(int i=0;i<list.size();i++)
+        for(Map.Entry<Integer,Integer>pair:hm.entrySet())
         {
-            int ele=list.get(i);
-            int count=0;
-            
-            while(hm.containsKey(ele+count))
-                count++;
-            
-            max=Math.max(count,max);
+            if(pair.getValue()==1)
+            {
+                int key=pair.getKey();
+                int count=0;
+                while(hm.containsKey(key+count))
+                    count++;
+                
+                max=Math.max(count,max);
+            }
         }
         return max;
     }
