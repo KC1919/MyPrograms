dfe6ec4367e94d39b4ec5fa4f16dd21b0b4f21dc
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-08 15:31:08 +0530

    Time: 9 ms (40.38%), Space: 46.3 MB (55.93%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -15,10 +15,12 @@ class Solution {
             {
                 if(a[i][j]!='.')
                 {
+                    //checking if the current element is already present in the row aur col
                     if(row.get(i).contains(a[i][j]) || col.get(j).contains(a[i][j])){
                         return false;
                     }
-
+                    
+                    //if not present we add it to the hashSet of the corresponding row and col
                     else{
                         row.get(i).add(a[i][j]);
                         col.get(j).add(a[i][j]);
@@ -34,11 +36,13 @@ class Solution {
     {
         HashSet<Character>hm=new HashSet<>();
         
+        // this runs till all the 3*3 subgrids are checked
         while(row<9 && col<9){
-        
+            
+            //start from the 1st row of every subgrid
             for(int i=row;i<row+3;i++)
             {
-                for(int j=col;j<col+3;j++)
+                for(int j=col;j<col+3;j++) //start from the 1st row of every subgrid
                 {
                     if(a[i][j]!='.')
                     {
@@ -49,12 +53,14 @@ class Solution {
                     }
                 }
             }
-            hm.clear();
-            col+=3;
+            hm.clear();//when we validate one subgrid we clear the hashmap, so that we                        //can process the next grid
+            col+=3; //we jump 3 cols to reach the start column of new subgrid
             
+            //if we exceed the column boundary, means we have to move to subgrid 
+            //below
             if(col==9){
-                col=0;
-                row+=3;
+                col=0;  //so we initialize the column to 0 
+                row+=3; //and we jump 3 rows becoz we have to jump to the subgrid below
             }
         }
         return true;
