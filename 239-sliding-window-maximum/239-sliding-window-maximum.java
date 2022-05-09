f40e3a27ce5c9b30028c8e1976edf7fd293af115
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-10 00:52:28 +0530

    Time: 35 ms (78.32%), Space: 61.3 MB (82.23%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,42 +1,37 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] a, int k) 
-    {
-        Stack<Integer>st=new Stack<>();
+    public int[] maxSlidingWindow(int[] a, int k) {
+       
         int n=a.length;
-        int rg[]=new int[n];
         
-        rg[n-1]=n;
-        st.push(n-1);
-        
-        
-        for(int i=n-2;i>=0;i--)
-        {
-            while(st.size()!=0 && a[i]>=a[st.peek()])
-            {
-                st.pop();
-            }
-            
-            if(st.size()!=0)
-            {
-                rg[i]=st.peek();
-            }
-            else
-            rg[i]=n;
-            
-            st.push(i);
-        }
         int ans[]=new int[n-k+1];
-        int j=0, c=0;
-        for(int i=0;i<=n-k;i++)
+        int c=0;
+        
+        Deque<Integer>que=new ArrayDeque<>();
+        
+        int max=0;
+        
+        for(int i=0;i<n;i++)
         {
-            if(j<i)
-            j=i;
+            //if the index at the peek of que is out of the window, then we remove it
+            if(!que.isEmpty() && que.peek()==i-k)
+                que.poll();
             
-            while(rg[j]<i+k)
-            {
-                j=rg[j];
+            //till the elements at indices contained in the que are smaller than the 
+            //current element, we keep on removing element from the last
+            //because we have to maintain monotonic increasing queue
+            //so if any elementcomes it removes all the smaller element from the que in the
+            //curent window being considered
+            
+            while(!que.isEmpty() && a[que.peekLast()]<a[i])
+                que.pollLast();
+            
+            //adding the current index in the que
+            que.offer(i);
+            
+            if(i>=k-1){
+                ans[c++]=a[que.peek()]; //the peek index will always have the greatest 
+                                        // element in the current window
             }
-            ans[c++]=a[j];
         }
         return ans;
     }
