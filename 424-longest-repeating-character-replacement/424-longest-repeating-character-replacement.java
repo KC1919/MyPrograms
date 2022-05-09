4cd26ddf56b147568e9632a7e903108373b79943
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-09 17:54:21 +0530

    Time: 24 ms (36.30%), Space: 44.3 MB (29.24%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -11,17 +11,24 @@ class Solution {
         
         for(int i=0;i<s.length();i++)
         {
-            char ch=s.charAt(i);
+            char ch=s.charAt(i); //extracting the charcater
             
-            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            hm.put(ch,hm.getOrDefault(ch,0)+1); //putting the character in the hashmap and
+                                                //incrementing its fequency by 1
             
+            //if some character has frequency greater than the current max frequency
+            //then we update the max frequency of the character in the substring
             maxf=Math.max(maxf,hm.get(ch));
             
-            if((i-j+1)-maxf<=k)
-            {
-                max=Math.max(max,i-j+1);
-            }
+            //and check if the difference in the length of the substring and number of 
+            //max frequency character is with the range of K, then we update our max length
+            //of the substring
             
+            if((i-j+1)-maxf<=k)
+                max=Math.max(max,i-j+1);
+            
+            //if the difference is greater than K,then we remove a character from the
+            // window of the substring
             else
             {
                 char rc=s.charAt(j);
