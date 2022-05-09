4ff5fb8e5c90904eefcead9dc7e09f98f138ede9
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-09 16:01:44 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (88.65%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,42 +1,31 @@
-class Solution 
-{
+class Solution {
     public List<String> letterCombinations(String digits) 
     {
         if(digits.length()==0)
             return new ArrayList<>();
         
-        // String keys[]={"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        HashMap<Character,String>hm=new HashMap<>();
-        hm.put('2',"abc");
-        hm.put('3',"def");
-        hm.put('4',"ghi");
-        hm.put('5',"jkl");
-        hm.put('6',"mno");
-        hm.put('7',"pqrs");
-        hm.put('8',"tuv");
-        hm.put('9',"wxyz");
-        
-        List<String>ans=new ArrayList<>();
-        
-        generateSeq(digits,hm,ans,new StringBuilder(),0);
-        return ans;
+        String keys[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String>list=new ArrayList<>();
+        StringBuilder sb=new StringBuilder();
+        combinations(digits,0,keys,list,sb);
+        return list;
     }
     
-    public void generateSeq(String digits,HashMap<Character,String>hm, List<String>list, StringBuilder ans, int ind)
+    public void combinations(String digits, int idx, String keys[], List<String>list, StringBuilder ans)
     {
-        if(ind==digits.length())
-        {
+        if(idx==digits.length()){
             list.add(ans.toString());
             return;
         }
+        char key=digits.charAt(idx);
         
-        String key=hm.get(digits.charAt(ind));
+        String s=keys[key-48];
         
-        for(int i=0;i<key.length();i++)
+        for(int i=0;i<s.length();i++)
         {
-            char ch=key.charAt(i);
+            char ch=s.charAt(i);
             ans.append(ch);
-            generateSeq(digits,hm,list,ans,ind+1);
+            combinations(digits,idx+1,keys,list,ans);
             ans.deleteCharAt(ans.length()-1);
         }
     }
