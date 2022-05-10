d9b02429add397b6d1b66935e816f337c22f3130
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-10 22:46:58 +0530

    Time: 35 ms (79.15%), Space: 132.6 MB (32.06%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -7,18 +7,24 @@ class Solution {
         Stack<Integer>st=new Stack<>();
         
         st.push(n-1);
-        ans[n-1]=0;
+        ans[n-1]=-1;
         for(int i=n-2;i>=0;i--){
             
-            while(st.size()>0 && temp[st.peek()]<=temp[i]){
-                st.pop();
+            int j=i+1;
+            while(j!=-1 && temp[j]<=temp[i]){
+                j=ans[j];
             }
-            if(st.size()==0)
-                ans[i]=0;
-            else
-                ans[i]=st.peek()-i;
+            ans[i]=j;
             
             st.push(i);
+        }
+        
+        for(int i=0;i<n;i++){
+            if(ans[i]==-1)
+                ans[i]=0;
+            else{
+                ans[i]=ans[i]-i;
+            }
         }
         return ans;
     }
