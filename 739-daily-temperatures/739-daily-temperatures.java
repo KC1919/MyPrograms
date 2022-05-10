66c68ec80032cbca0b54fba0479a4c76be5ac11d
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-10 22:48:25 +0530

    Time: 35 ms (79.15%), Space: 128.5 MB (54.66%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,31 +1,20 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temp) {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
         
-        int n=temp.length;
-        int ans[]=new int[n];
-        
-        Stack<Integer>st=new Stack<>();
-        
-        st.push(n-1);
-        ans[n-1]=-1;
-        for(int i=n-2;i>=0;i--){
-            
-            int j=i+1;
-            while(j!=-1 && temp[j]<=temp[i]){
-                j=ans[j];
+        for (int currDay = 0; currDay < n; currDay++) {
+            int currentTemp = temperatures[currDay];
+            // Pop until the current day's temperature is not
+            // warmer than the temperature at the top of the stack
+            while (!stack.isEmpty() && temperatures[stack.peek()] < currentTemp) {
+                int prevDay = stack.pop();
+                answer[prevDay] = currDay - prevDay;
             }
-            ans[i]=j;
-            
-            st.push(i);
+            stack.push(currDay);
         }
         
-        for(int i=0;i<n;i++){
-            if(ans[i]==-1)
-                ans[i]=0;
-            else{
-                ans[i]=ans[i]-i;
-            }
-        }
-        return ans;
+        return answer;
     }
 }
