958b9b7657ca078118d82b310d3a3bd8b6116141
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-10 22:20:10 +0530

    Time: 2 ms (61.27%), Space: 43.2 MB (68.25%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -9,17 +9,25 @@ class Solution {
     
     public void generate(int n, int op, int cp, StringBuilder combi, List<String>ans)
     {
+        //if all the parenthesis has been used, means we have got a possible valid
+        //sequence of paranthesis, hence we add it to the list of answers
         if(cp==0 && op==0 && combi.length()==n*2){
             String com=combi.toString();
             ans.add(com);
             return;
         }
         
+        //if there are opening parenthesis avalable then we take it in our sequence and
+        //make a call to the next level
         if(op>0){
             combi.append('(');
             generate(n,op-1,cp,combi,ans);
             combi.deleteCharAt(combi.length()-1);
         }
+        
+        //after the above call, now we have to check if we have any closing parenthesis
+        //available, then we can add them in our sequence and make a call to
+        //the next level
         
         if(cp>op){
             combi.append(')');
