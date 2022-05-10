ac77b23ac6c1e9210550ab54eedfe66d327239f7
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-10 22:54:14 +0530

    Time: 19 ms (88.31%), Space: 56.4 MB (75.86%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,20 +1,43 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] answer = new int[n];
-        Deque<Integer> stack = new ArrayDeque<>();
+    public int[] dailyTemperatures(int[] temp) {
         
-        for (int currDay = 0; currDay < n; currDay++) {
-            int currentTemp = temperatures[currDay];
-            // Pop until the current day's temperature is not
-            // warmer than the temperature at the top of the stack
-            while (!stack.isEmpty() && temperatures[stack.peek()] < currentTemp) {
-                int prevDay = stack.pop();
-                answer[prevDay] = currDay - prevDay;
+        int n=temp.length;
+        int ans[]=new int[n];
+        
+        Stack<Integer>st=new Stack<>();
+        
+        st.push(n-1);
+        ans[n-1]=-1;
+        
+        //used the jump strategy to reduce the time complexity
+        //instead of using a stack, this problem is an application of
+        // next greater element on the right
+        
+        for(int i=n-2;i>=0;i--){
+            
+            int j=i+1;
+            while(j!=-1 && temp[j]<=temp[i]){
+                j=ans[j];
             }
-            stack.push(currDay);
+            ans[i]=j;
+            
+            st.push(i);
         }
         
-        return answer;
+        //so we got our next greater index for every ith index
+        //we check if ith element does not have a next greater element(temperature), then
+        //we put 0 at that index, otherwise if it has next greater temperature,
+        //we calculate the gap between the days by subraction the ith index from the
+        //next greater temperature index
+        for(int i=0;i<n;i++){
+            if(ans[i]==-1)
+                ans[i]=0;
+            else{
+                ans[i]=ans[i]-i;
+            }
+        }
+        
+        //and finally we return the answer
+        return ans;
     }
 }
