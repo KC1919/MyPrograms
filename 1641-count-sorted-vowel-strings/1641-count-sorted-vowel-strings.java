a8b31c18a9f440205597af1337acedd10594cb19
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-11 23:00:50 +0530

    Time: 105 ms (10.07%), Space: 41.2 MB (23.34%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,16 +1,21 @@
 class Solution {
-    int a=1, e=1, i=1, o=1, u=1;
-    public int countVowelStrings(int n) { 
-    	for(int j=1; j<n; j++){
-        	increment();
-        }
-    	return a+e+i+o+u;
+    int count=0;
+    public int countVowelStrings(int n) {
+        char vowels[]={'a','e','i','o','u'};
+        count=0;
+        combinations(n,0,0,vowels);
+        return count;
     }
-
-    private void increment() {
-		a = a+e+i+o+u;
-		e = e+i+o+u;
-		i = i+o+u;
-		o = o+u;
-	}
+    
+    public void combinations(int n, int idx, int prev, char a[])
+    {
+        if(idx==n){
+            count++;
+            return;
+        }
+        
+        for(int i=prev;i<a.length;i++){
+            combinations(n,idx+1,i,a);
+        }
+    }
 }
