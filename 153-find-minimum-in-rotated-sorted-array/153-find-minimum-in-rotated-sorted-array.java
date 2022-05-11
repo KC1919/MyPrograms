5b0b910042ecb09decf93a85dcf7a2840e6eaef5
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-12 00:17:12 +0530

    Time: 1 ms (32.64%), Space: 42.5 MB (44.40%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -7,14 +7,25 @@ class Solution {
         {
             mid=(l+h)/2;
             
+            //we check if the element at left index is smaller than the element at
+            //mid index, means the part from left to mid is sorted
             if(a[l]<=a[mid]){
                 
+                //so we check if the element at left  index is greater than the 
+                //element at right index, means the element cannot be in this part
+                //so we discard this half and move to the right half of the array
                 if(a[l]>a[h])
                     l=mid+1;
                 
+                //else if the element at lower index is smaller than the element at right
+                //index, that means the whole part is sorted and the left index
+                //is the smallest element so we return it.
                 else
                     return a[l];
             }
+            
+            //if the element at left index is greater than the element at mid index
+            //that means the element is to the right of th
             else
                 h=mid;
         }
