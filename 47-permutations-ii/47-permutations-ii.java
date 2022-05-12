00aebaf2627e91793f5c20e11aa05a5f80462cdf
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-12 21:50:50 +0530

    Time: 3 ms (83.29%), Space: 47.7 MB (62.27%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,41 +1,32 @@
 class Solution {
-    public List<List<Integer>> permuteUnique(int[] a) 
-    {
-        List<List<Integer>>res=new ArrayList<>();
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        
-        for(int i=0;i<a.length;i++)
-        {
-            hm.put(a[i],hm.getOrDefault(a[i],0)+1);
-        }
-        
-        List<Integer>list=new ArrayList<>();
-        
-        for(int i=0;i<a.length;i++)
-            list.add(null);
-        
-        permutations(a,list,res,0,hm);
-        return res;
+    public List<List<Integer>> permuteUnique(int[] a) {
+        List<List<Integer>>ans=new ArrayList<>();
+        Arrays.sort(a);
+        permutations(a,new ArrayList<>(),0,ans);
+        return ans;
     }
     
-    public void permutations(int a[], List<Integer>list, List<List<Integer>>res, int idx, HashMap<Integer,Integer>hm)
-    {
-        if(idx==a.length)
-        {
-            res.add(new ArrayList<>(list));
+    public void permutations(int a[], List<Integer>list, int idx, List<List<Integer>>ans){
+        
+        if(idx==a.length){
+            ans.add(new ArrayList<>(list));
             return;
         }
         
-        for(int ele:hm.keySet())
-        {
-            if(hm.get(ele)>0)
-            {
-                list.set(idx,ele);
-                hm.put(ele,hm.get(ele)-1);
-                permutations(a,list,res,idx+1,hm);
-                hm.put(ele,hm.get(ele)+1);
-                list.set(idx,null);
-            }
+        for(int i=0;i<a.length;i++){
+            
+                if(i!=0 && a[i-1]==a[i])
+                    continue;
+                
+                else{
+                    if(a[i]>=-10){
+                        list.add(a[i]);
+                        a[i]+=-20;
+                        permutations(a,list,idx+1,ans);
+                        list.remove(list.size()-1);
+                        a[i]+=20;
+                    }
+                }
         }
     }
 }
