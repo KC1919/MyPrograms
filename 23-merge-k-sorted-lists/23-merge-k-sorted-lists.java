37b375dce68a33d4343654191c5c293c2ffd46af
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-13 15:02:40 +0530

    Time: 7 ms (54.22%), Space: 47.4 MB (47.12%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -15,27 +15,40 @@ class Solution {
             return a.val-b.val;
         });
         
+        //adding the head node of each list in the queue
         for(int i=0;i<lists.length;i++){
-            
             if(lists[i]!=null)
                 pq.add(lists[i]);
         }
         
+        //dummy list
         ListNode dummy=new ListNode();
+        
+        //pointer to the head of the dummy list
         ListNode temp=dummy;
         
         while(pq.size()>0){
+            
+            //take out the smallest node from the queue
             ListNode rem=pq.remove();
             
+            //link thsi node to the dummy list
             temp.next=rem;
+            
+            //move the dummy list pointer
             temp=temp.next;
             
+            //move to the next node of the removed node
             rem=rem.next;
             
+            //ifthe next node is not null, we add it to the priority queue
             if(rem!=null)
                 pq.add(rem);
             
         }
+        
+        //fnally return the head node of the dummy list formed after merging
+        //k lists
         return dummy.next;
     }
 }
