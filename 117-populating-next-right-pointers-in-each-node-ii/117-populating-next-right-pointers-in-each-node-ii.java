ddc6078057d9767c038e085c89d42ac6b235b700
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-13 13:08:19 +0530

    Time: 1 ms (71.97%), Space: 42.1 MB (78.25%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -31,16 +31,28 @@ class Solution {
         
         que.add(root);
         
+        //level-order traversal
         while(que.size()>0){
+            
+            //taking the current size of the queue, that tells the number of eleemnt in the
+            //current level, that is going to be processed, so that we only
+            //process those elements
             int size=que.size();
             
+            //process all the nodes of current level
             while(size-->0){
                 
+                //take out a node of the current level one by one
                 Node rem=que.remove();
+                
+                //then check if there are any nodes left in the queue of the current level
+                //if left then the current element next pointer will point to the
+                //node in the queue
                 if(size>=1){
                     rem.next=que.peek();
                 }
                 
+                //then we add the children of the removed node in the que
                 if(rem.left!=null)
                     que.add(rem.left);
                 
