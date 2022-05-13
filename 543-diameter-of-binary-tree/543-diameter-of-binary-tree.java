1d9b334797752c545d012a080bdd79ab5f5a115c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-13 23:20:45 +0530

    Time: 1 ms (49.84%), Space: 43.6 MB (32.56%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -35,19 +35,32 @@ class Solution {
             return new Pair();
         }
         
+        //made my pair
         Pair p=new Pair();
         
+        //got the result from the left subtree
         Pair lres=maxDia(node.left);
+        
+        //got the result fromt he right subtree
         Pair rres=maxDia(node.right);
         
+        //calculated the max diameter so far
         int maxdia=Math.max(lres.maxdia,rres.maxdia);
+        
+        //calculated the max height from the left and right result
         int myheight=Math.max(lres.myheight,rres.myheight)+1;
         
+        //calculated my diameter
         int mydia=lres.myheight+rres.myheight;
         
+        //stored the height in myheight variable of the pair class
         p.myheight=myheight;
+        
+        //then checked if my diameter is greater than the max diameter so far
+        //so update the max diameter varible with my diameter value, else put the
+        //max diameter value so far in it
         p.maxdia=Math.max(mydia,maxdia);
         
-        return p;
+        return p; //finally return the pair of information to the above node
     }
 }
