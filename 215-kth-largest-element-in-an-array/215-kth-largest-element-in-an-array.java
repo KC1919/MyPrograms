2f85858a563a1c5d597139f86d1857a97ef31de4
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-16 02:18:03 +0530

    Time: 4 ms (77.40%), Space: 44.9 MB (39.05%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -3,18 +3,19 @@ class Solution {
         
         PriorityQueue<Integer>pq=new PriorityQueue<>();
         
-        for(int i=0;i<a.length;i++){
-            
-            if(pq.size()<k)
-                pq.add(a[i]);
-            
-            else{
-                
-                if(pq.peek()<a[i]){
-                    pq.add(a[i]);
-                    pq.remove();
-                }
+        for(int i=0;i<k;i++)
+        {
+            pq.add(a[i]);
+        }
+        
+        for(int i=k;i<a.length;i++)
+        {
+            if(a[i]>pq.peek())
+            {
+                 pq.add(a[i]);
+                 pq.remove();
             }
+               
         }
         
         return pq.size()>0?pq.remove():-1;
