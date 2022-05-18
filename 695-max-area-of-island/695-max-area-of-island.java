268436c631ec903db6e45e09d00677b77fc07e59
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-18 22:54:47 +0530

    Time: 3 ms (72.39%), Space: 47.9 MB (21.27%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -3,7 +3,7 @@ class Solution {
         
         int n=a.length;
         int m=a[0].length;
-        boolean visited[][]=new boolean[n][m];
+        // boolean visited[][]=new boolean[n][m];
         int count[]=new int[1];
         
         int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
@@ -11,9 +11,10 @@ class Solution {
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visited[i][j]==false && a[i][j]==1){
+                if(a[i][j]==1){
+                    a[i][j]=0;
                     count[0]=0;
-                    component(a,i,j,n,m,visited,count,dir);
+                    component(a,i,j,n,m,count,dir);
                     max=Math.max(count[0],max);
                 }
             }
@@ -21,9 +22,8 @@ class Solution {
         return max;
     }
     
-    public void component(int a[][], int i, int j, int n, int m, boolean visited[][], int count[], int dir[][])
+    public void component(int a[][], int i, int j, int n, int m, int count[], int dir[][])
     {
-        visited[i][j]=true;
         count[0]++;
         
         for(int k=0;k<dir.length;k++){
@@ -31,9 +31,10 @@ class Solution {
             int row=dir[k][0]+i;
             int col=dir[k][1]+j;
             
-            if(row>=0 && row<n && col>=0 && col<m && visited[row][col]==false && a[row][col]==1)
+            if(row>=0 && row<n && col>=0 && col<m && a[row][col]==1)
             {
-                component(a,row,col,n,m,visited,count,dir);    
+                a[row][col]=0;
+                component(a,row,col,n,m,count,dir);    
             }
         }
     }
