600c150adbc43187554b3a147b28c62748afb8bd
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-18 22:18:43 +0530

    Time: 18 ms (42.55%), Space: 136.4 MB (60.42%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,38 +1,43 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        
+    public List<List<String>> partition(String s) 
+    {
         List<List<String>>res=new ArrayList<>();
-        
-        palinParti(s,0,s.length()-1,new ArrayList<>(),res);
+        helper(s,res,new ArrayList<String>());
         return res;
-        
     }
     
-    public void palinParti(String s, int si, int ei, List<String>list, List<List<String>>res)
+    public void helper(String s, List<List<String>>res, List<String> list)
     {
-        if(si>ei){
+        if(s.length()==0)
+        {
             res.add(new ArrayList<>(list));
             return;
         }
         
-        for(int i=si;i<=ei;i++){
+        for(int i=0;i<s.length();i++)
+        {
+            String rem=s.substring(0,i+1);
             
-            if(isPalin(s,si,i)==true){
-                list.add(s.substring(si,i+1));
-                palinParti(s,i+1,ei,list,res);
+            if(palindrome(rem))
+            {
+                list.add(rem);
+                helper(s.substring(i+1),res,list);
                 list.remove(list.size()-1);
             }
         }
     }
     
-    public boolean isPalin(String s, int si, int ei){
+    public boolean palindrome(String s)
+    {
+        int start=0, end=s.length()-1;
         
-        while(si<=ei){
-            if(s.charAt(si)!=s.charAt(ei))
+        while(start<end)
+        {
+            if(s.charAt(start)!=s.charAt(end))
                 return false;
             
-            si++;
-            ei--;
+            start++;
+            end--;
         }
         return true;
     }
