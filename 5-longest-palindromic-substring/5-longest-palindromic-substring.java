7f5f10a092e76dc7a0725a2656ece8029d127be7
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-19 16:56:15 +0530

    Time: 379 ms (25.64%), Space: 84.4 MB (18.63%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,49 +1,51 @@
 class Solution {
     public String longestPalindrome(String s) {
         
-        int li=0, ri=0, max=0;
-        for(int i=0;i<s.length();i++)
-        {
-            int a1[]=palindrome(s,i,i);
+        int n=s.length();
+        
+        boolean dp[][]=new boolean[n][n];
+        
+        int max=1;
+        int si=-1, ei=-1;
+        
+        for(int gap=0;gap<n;gap++){
             
-            if(a1[0]!=-1 && a1[1]!=-1 && a1[1]-a1[0]+1>max)
-            {
-                max=a1[1]-a1[0]+1;
-                li=a1[0];
-                ri=a1[1];
-            }
-            int a2[]=palindrome(s,i,i+1);
-            if(a2[0]!=-1 && a2[1]!=-1 && a2[1]-a2[0]+1>max)
-            {
-                max=a2[1]-a2[0]+1;
-                li=a2[0];
-                ri=a2[1];
-            }
-        }
-        
-        if(li==-1 || ri==-1)
-            return "";
-        
-        return s.substring(li,ri+1);
-    }
-    
-    public int[] palindrome(String s, int left, int right)
-    {
-        int li=-1, ri=-1, max=0;
-        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right))
-        {
-            if(s.charAt(left)==s.charAt(right))
-            {
-                if((right-left+1)>max)
-                {
-                    li=left;
-                    ri=right;
-                    max=right-left+1;
+            for(int i=0,j=gap;j<n;i++,j++){
+                
+                char start=s.charAt(i);
+                char end=s.charAt(j);
+                
+                if(gap==0){
+                    dp[i][j]=true;
+                    si=i;
+                    ei=j;
                 }
-                left--;
-                right++;
-            }  
+                
+                else if(gap==1){
+                    
+                    if(start==end){
+                        dp[i][j]=true;
+                        max=2;
+                        si=i;
+                        ei=j;
+                    }
+                }
+                
+                else{
+                    
+                    if(start==end && dp[i+1][j-1]==true){
+                        dp[i][j]=true;
+                        
+                        if(j-i+1>max){
+                            max=j-i+1;
+                            si=i;
+                            ei=j;
+                        }
+                    }
+                }
+            }
         }
-        return new int[]{li,ri};
+        
+        return (si!=-1 && ei!=-1)?s.substring(si,ei+1):"";
     }
 }
