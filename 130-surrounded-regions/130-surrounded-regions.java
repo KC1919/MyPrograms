616dfe02b8dc6b1bd5b4e033b3452f61d0d88e06
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-20 01:57:08 +0530

    Time: 2 ms (78.22%), Space: 44 MB (97.00%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,115 +1,60 @@
 class Solution {
     
-    class Pair 
+    class Pair
     {
         int row;
         int col;
-        boolean isPart;
+        boolean border;
         
-        Pair(int row, int col, boolean isPart) 
-        {
-            this.row = row;
-            this.col = col;
-            this.isPart = isPart;
+        Pair(int row, int col, boolean border){
+            this.row=row;
+            this.col=col;
+            this.border=border;
         }
     }
-
-    public void solve(char[][] a) 
-    {
-        int n = a.length;
-        int m = a[0].length;
-
-        boolean visited[][] = new boolean[n][m];
     
-        workRow(a,0,m,visited);
-        workRow(a,n-1,m,visited);
-        workCol(a,n,0,visited);
-        workCol(a,n,m-1,visited);
+    public void solve(char[][] a) {
         
-        for (int i = 0; i < n; i++) 
-        {
-            for (int j = 0; j < m; j++) 
-            {
-                if (a[i][j] == 'O' && visited[i][j] == false) 
-                {
-                    ArrayList<Pair> comp = new ArrayList<>();
-                    island(a, i, j, n, m, comp, visited);
-                    for (int k = 0; k < comp.size(); k++) 
-                    {
-                        Pair p = comp.get(k);
-                        if (p.isPart == false)
-                            a[p.row][p.col] = 'X';
+        Queue<Pair>que=new LinkedList<>();
+        
+        int n=a.length;
+        int m=a[0].length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(a[i][j]=='O'){
+                    if(i==0 || j==0 || i==n-1 || j==m-1){
+                        a[i][j]='Y';
+                        que.add(new Pair(i,j,true));
                     }
                 }
             }
         }
-    }
-    
-    public void workRow(char a[][], int row, int col, boolean visited[][])
-    {
-        for (int j = 0; j < col; j++) 
-        {
-            if (a[row][j] == 'O' && visited[row][j] == false) 
-            {
-                ArrayList<Pair> comp = new ArrayList<>();
-                island(a, row, j, a.length, a[0].length, comp, visited);
-                for (int k = 0; k < comp.size(); k++) 
-                {
-                    Pair p = comp.get(k);
-                    if (p.isPart == false)
-                        a[p.row][p.col] = 'X';
+        
+        int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
+        
+        while(que.size()>0){
+            Pair rem=que.remove();
+            
+            for(int k=0;k<dir.length;k++){
+                int row=dir[k][0]+rem.row;
+                int col=dir[k][1]+rem.col;
+                
+                if(row>=0 && row<n && col>=0 && col<m && a[row][col]=='O'){
+                    que.add(new Pair(row,col,true));
+                    a[row][col]='Y';
                 }
             }
         }
-    }
-    
-    public void workCol(char a[][], int row, int col, boolean visited[][])
-    {
-        for (int j = 0; j < row; j++) 
-        {
-            if (a[j][col] == 'O' && visited[j][col] == false) 
-            {
-                ArrayList<Pair> comp = new ArrayList<>();
-                island(a, j, col, a.length, a[0].length, comp, visited);
-                for (int k = 0; k < comp.size(); k++) 
-                {
-                    Pair p = comp.get(k);
-                    if (p.isPart == false)
-                        a[p.row][p.col] = 'X';
-                }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(a[i][j]=='Y')
+                    a[i][j]='O';
+            
+                else if(a[i][j]=='O')
+                    a[i][j]='X';
             }
         }
-    }
-
-    public void island(
-        char a[][], int i, int j, int n, int m, ArrayList<Pair> comp, boolean visited[][]) {
-        if (i < 0 || j < 0 || i > n - 1 || j > m - 1 || a[i][j] == 'X' || visited[i][j] == true)
-            return;
-
-        visited[i][j] = true;
-
-        if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
-            comp.add(new Pair(i, j, true));
-        }
-
-        else if ((i - 1 == 0 && a[i - 1][j] == 'O') || (i + 1 == n - 1 && a[i + 1][j] == 'O')
-            || (j - 1 == 0 && a[i][j - 1] == 'O') || (j + 1 == m - 1 && a[i][j + 1] == 'O')) {
-            comp.add(new Pair(i, j, true));
-        }
-
-        else if (comp.size() >= 1) {
-            if (comp.get(comp.size() - 1).isPart == true)
-                comp.add(new Pair(i, j, true));
-
-            else
-                comp.add(new Pair(i, j, false));
-
-        } else
-            comp.add(new Pair(i, j, false));
-
-        island(a, i - 1, j, n, m, comp, visited);
-        island(a, i + 1, j, n, m, comp, visited);
-        island(a, i, j - 1, n, m, comp, visited);
-        island(a, i, j + 1, n, m, comp, visited);
     }
 }
