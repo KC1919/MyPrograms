eba5c0c02dc15f148fa691e895eb16ef275a416b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-19 11:23:25 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (71.02%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] price) {
+        
+        if(price.length==1)
+            return price[0];
+        
+        else if(price.length==2)
+            return Math.max(price[0],price[1]);
+        
+        
+        int n=price.length;
+        int dp[]=new int[n];
+        
+        dp[n-1]=price[n-1];
+        dp[n-2]=price[n-2];
+        dp[n-3]=price[n-3]+price[n-1];
+        
+        for(int i=n-4;i>=0;i--){
+            dp[i]=Math.max(dp[i+2],dp[i+3])+price[i];
+        }
+        
+        return Math.max(dp[0],dp[1]);
+    }
+}
