516f587794c5c823769dcaf03aadaed68dd307e6
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-20 14:45:07 +0530

    Time: 223 ms (25.35%), Space: 41.3 MB (79.00%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -13,10 +13,14 @@ class Solution {
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                
+                //we only make calls from those cells whose character match
+                //to the starting character of the word to be searched
                 if(a[i][j]==sc){
                     sb.append(a[i][j]);
                     boolean res=findWord(a,i,j,n,m,visited,sb,word,dir);
                     sb.deleteCharAt(sb.length()-1);
+                    
                     if(res==true)
                         return true;
                 }
@@ -32,20 +36,32 @@ class Solution {
             
             visited[i][j]=true;
             
+            //searching in all 4 direction from the current cell
             for(int k=0;k<4;k++){
                 int row=dir[k][0]+i;
                 int col=dir[k][1]+j;
                 
+                //picking the character only if it is valid
+                //means it is present in the word
                 if(row>=0 && row<n && col>=0 && col<m && a[row][col]==word.charAt(sb.length()) && visited[row][col]==false)
                 {
+                    //pappend the character
                     sb.append(a[row][col]);
+                    
+                    //make call to search the next character in sequence of the word
                     boolean res=findWord(a,row,col,n,m,visited,sb,word,dir);
+                    
+                    //if the returned result is true, means the word was formed
+                    //so we return true
                     if(res==true)
                         return true;
                     
+                    //backtracking ,deleting the appended character
                     sb.deleteCharAt(sb.length()-1);
                 }
             }
+        
+            //marking the cell as unvisited
             visited[i][j]=false;
             return false;
         }
