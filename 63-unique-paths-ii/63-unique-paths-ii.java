b64a7b4416dbdfbe0a5c54400e2e80977580468a
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-20 12:49:37 +0530

    Time: 1 ms (52.25%), Space: 43.2 MB (5.49%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] a) {
+        
+        if(a[0][0]!=0)
+            return 0;
+        
+        int n=a.length;
+        int m=a[0].length;
+        
+        Integer dp[][]=new Integer[n][m];
+        int dir[][]={{1,0},{0,1}};
+        int res=uniqPath(a,0,0,n,m,dp,dir);
+        
+        return dp[0][0];
+    }
+    
+    public int uniqPath(int a[][], int i, int j, int n, int m, Integer dp[][], int dir[][]){
+        
+        if(i==n-1 && j==m-1 && a[i][j]==0){
+            dp[i][j]=1;
+            return Integer.MAX_VALUE;
+        }
+        
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+        
+        for(int k=0;k<2;k++){
+            int row=dir[k][0]+i;
+            int col=dir[k][1]+j;
+            
+            if(row>=0 && row<n && col>=0 && col<m && a[row][col]!=1){
+                
+                int res=uniqPath(a,row,col,n,m,dp,dir);
+                if(res==Integer.MAX_VALUE){
+                    dp[i][j]=1;
+                }
+                else if(res!=Integer.MAX_VALUE && res>0){
+                    if(dp[i][j]==null)
+                        dp[i][j]=res;
+                    
+                    else
+                        dp[i][j]+=res;
+                }
+            }
+        }
+        if(dp[i][j]==null)
+            dp[i][j]=0;
+        
+        return dp[i][j];
+    }
+}
