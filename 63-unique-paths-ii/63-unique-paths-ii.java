df9cf19e1b8b8503f419bb3613c08de7c0e020d1
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-20 13:02:42 +0530

    Time: 1 ms (52.25%), Space: 41.8 MB (69.26%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -16,36 +16,38 @@ class Solution {
     
     public int uniqPath(int a[][], int i, int j, int n, int m, Integer dp[][], int dir[][]){
         
-        if(i==n-1 && j==m-1 && a[i][j]==0){
+        //if the robot successfully eaches the bottom right cell
+        //we return 1, indicating success
+        if(i==n-1 && j==m-1 && a[i][j]==0)
             return dp[i][j]=1;
-        }
         
-        if(dp[i][j]!=null){
+        //if the robot reahces a cell which has already been explored before by some other 
+        //cell, so its answer must be in the dp matrix, so we return in
+        // instead of recalculating
+        if(dp[i][j]!=null)
             return dp[i][j];
-        }
         
-        int tres=0;
+        //total paths from the (i,j) cell
+        int totalPaths=0;
+        
+        //movinf in down and right direction from the current cell
         for(int k=0;k<2;k++){
+            
             int row=dir[k][0]+i;
             int col=dir[k][1]+j;
             
             if(row>=0 && row<n && col>=0 && col<m && a[row][col]!=1){
                 
+                //getting the answer from the down and right cell
                 int res=uniqPath(a,row,col,n,m,dp,dir);
-                tres+=res;
-//                 if(res==Integer.MAX_VALUE){
-//                     dp[i][j]=1;
-//                 }
-//                 else if(res!=Integer.MAX_VALUE && res>0){
-//                     if(dp[i][j]==null)
-//                         dp[i][j]=res;
-                    
-//                     else
-//                         dp[i][j]+=res;
-//                 }
+                
+                //adding the answer to total paths
+                totalPaths+=res;
             }
         }
         
-        return dp[i][j]=tres;
+        //storing the total paths from (i,j) cell in the dp matrix and returning the
+        //answer to the cell where the call came from
+        return dp[i][j]=totalPaths;
     }
 }
