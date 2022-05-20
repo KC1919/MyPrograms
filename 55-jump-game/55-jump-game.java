3bba7d86e6327111a843287c177e862956737a4d
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-20 17:22:28 +0530

    Time: 3 ms (56.70%), Space: 67.6 MB (56.86%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -3,19 +3,13 @@ class Solution {
         
         int n=a.length;
         
-        int dp[]=new int[n];
-        
-        dp[n-1]=1;
-        
+        int goal=n-1;
         for(int i=n-2;i>=0;i--){
-            for(int jump=1;jump<=a[i];jump++){
-                if(dp[i+jump]==1){
-                    dp[i]=1;
-                    break;
-                }
-            }
+            if(i+a[i]>=goal)
+                goal=i;
         }
-        if(dp[0]==1)
+        
+        if(goal==0)
             return true;
         
         return false;
