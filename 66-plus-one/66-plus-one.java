e03e24988176557589e45353053c6546766c1601
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-25 00:52:25 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (55.71%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -13,21 +13,34 @@ class Solution {
             
         for(int i=a.length-2;i>=0;i--){
             
+            //if carry is there
             if(carry>0){
+                //use the carry
+                a[i]+=carry;
+                
+                // reset the carry to 0
                 carry=0;
-                a[i]+=1;
             }
             
+            //if the digit becomes more than 9
             if(a[i]>9){
+                
+                //make the digit as 0
                 a[i]=0;
+                
+                //and generate a carry of 1
                 carry=1;
             }
         }
         
-        
+        //if carry is still left means every digit has been changed
         if(carry==1){
-            a=new int[n+1];
-            a[0]=1;
+            
+            //so we allocate memory to accomodate 1 more digit
+            
+            a=new int[n+1]; //copying the previous elements in 1 to n indices
+            
+            a[0]=1;  //and putting 1 at the head
         }
         return a;
     }
