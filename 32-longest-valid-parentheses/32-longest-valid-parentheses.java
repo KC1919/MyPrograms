5b0db3053b3790f74b9572686d16dae41ee5d167
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-24 12:51:17 +0530

    Time: 7 ms (45.40%), Space: 44 MB (20.69%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -6,12 +6,14 @@ public class Solution {
         stack.push(-1);
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (stack.empty()) {
-                    stack.push(i);
-                } else {
+                stack.push(i); //pushing the index of the opening bracket
+            } else { //if closing bracket
+                stack.pop(); //we pop the peek index
+                if (stack.empty()) {  //if the stack gets empty
+                    stack.push(i);    //we push the closing bracket's index
+                } else { //else we calculate the length, y taking the difference of the 
+                        //curent closing bracket index and the index of the opening 
+                        //bracket at the peek of the stack
                     maxans = Math.max(maxans, i - stack.peek());
                 }
             }
