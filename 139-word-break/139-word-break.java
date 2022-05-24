44da8b3b8132a2dca70b6f6c40c551890b65f37b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-25 01:17:43 +0530

    Time: 4 ms (84.69%), Space: 43.5 MB (54.60%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -5,13 +5,22 @@ class Solution {
     
     boolean helper(String s, List<String>wordDict, HashMap<String,Boolean>answer)
     {
-        if(s.length()==0)
+        //if the whole string is used, means given list of words
+        //were found in the string
+        if(s.length()==0){
+            
+            // so we return true to mark success
             return true;
+        }
         
+        //we check if the remaining substring has been processed before by some other call
+        //so we must be having the answer for it, so we return the stored answer
+        //instead of processing it agin
         if(answer.containsKey(s)){
             return answer.get(s);
         }
         
+        //we take
         for(String word:wordDict)
         {
             if(s.indexOf(word)==0)
