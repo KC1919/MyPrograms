2c9f4eb0e00766deb0d7501ed87dd25492f5884a
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-24 12:48:51 +0530

    Time: 7 ms (45.40%), Space: 43.2 MB (46.45%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,31 +1,21 @@
 public class Solution {
+
     public int longestValidParentheses(String s) {
-        int left = 0, right = 0, maxlength = 0;
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                left++;
+                stack.push(i);
             } else {
-                right++;
-            }
-            if (left == right) {
-                maxlength = Math.max(maxlength, 2 * right);
-            } else if (right >= left) {
-                left = right = 0;
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
             }
         }
-        left = right = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == '(') {
-                left++;
-            } else {
-                right++;
-            }
-            if (left == right) {
-                maxlength = Math.max(maxlength, 2 * left);
-            } else if (left >= right) {
-                left = right = 0;
-            }
-        }
-        return maxlength;
+        return maxans;
     }
 }
