2d3524c56a84ebd2aa4dae5a470fca9209cf5059
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-24 20:49:42 +0530

    Time: 118 ms (7.37%), Space: 52.3 MB (8.98%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int change(int amount, int[] a) {
+        
+        int n=a.length;
+        
+        Integer dp[][]=new Integer[n][amount+1];
+        
+        targetSum(a,0,amount,dp);
+        return dp[0][amount]==null?0:dp[0][amount];
+    }
+    
+    public int  targetSum(int a[], int idx, int target, Integer dp[][]){
+        
+        if(idx==a.length || target==0){
+            if(target==0){
+                return dp[idx][target]=1;
+            }
+            return 0;
+        }
+        
+        if(dp[idx][target]!=null){
+            return dp[idx][target];
+        }
+        
+        int res=0;
+        if(target-a[idx]>=0){
+            res=targetSum(a,idx,target-a[idx],dp);
+            dp[idx][target]=res;
+        }
+        
+        for(int i=idx+1;i<a.length;i++){
+            
+            if(target-a[i]>=0){
+                res+=targetSum(a,i,target-a[i],dp);
+            }
+        }
+        
+        return dp[idx][target]=res;
+    }
+}
