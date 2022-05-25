da9fdad7abf376502a5dff89f651ce8199ddfd66
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-25 17:16:35 +0530

    Time: 3 ms (35.22%), Space: 45 MB (39.98%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -17,48 +17,24 @@ class Solution {
             int newPrev=0;
             int newCurr=0;
             
-//             if(a[i]>0){
                 
-                if(oldPrev<0 && oldCurr<0){
+            if(oldPrev<0 && oldCurr<0){
+                newPrev=a[i];
+                newCurr=Math.min(oldPrev,oldCurr)*a[i];
+            }
+                
+            else{
+                
+                if(oldPrev==0 || oldCurr==0){
                     newPrev=a[i];
-                    newCurr=Math.min(oldPrev,oldCurr)*a[i];
+                    newCurr=a[i];
                 }
                 
                 else{
-                    
-                    if(oldPrev==0 || oldCurr==0){
-                        newPrev=a[i];
-                        newCurr=a[i];
-                    }
-                    
-                    else{
-                        newPrev=oldPrev*a[i];
-                        newCurr=oldCurr*a[i];
-                    }
+                    newPrev=oldPrev*a[i];
+                    newCurr=oldCurr*a[i];
                 }
-            // }
-            
-//             else if(a[i]<0){
-                
-//                 if(oldPrev<0 && oldCurr<0){
-//                     newPrev=a[i];
-//                     newCurr=Math.min(oldPrev,oldCurr)*a[i];
-//                 }
-                
-//                 else{
-                    
-//                     if(oldPrev==0 || oldCurr==0){
-//                         newPrev=a[i];
-//                         newCurr=a[i];
-//                     }
-                    
-//                     else{
-//                         newPrev=oldPrev*a[i];
-//                         newCurr=oldCurr*a[i];
-//                     }
-//                 }
-//             }
-            
+            }
             max=Math.max(max,Math.max(newCurr,newPrev));
             
             oldPrev=newPrev;
