c8d2836cbe35aca1290128c3b9c7307d5d25c97b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-25 15:55:40 +0530

    Time: 4 ms (91.62%), Space: 44.4 MB (49.33%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -7,24 +7,30 @@ class Solution {
         int n=a.length;
         int dp[]=new int[n];
         
-        //answer of last element would always be 1, because there are 
-        // no elements right to it so it cannot make any subsequence
+        //store the 1st element 
         dp[0]=a[0];
         
         //this stores the longest subsequence so far
         int len=1;
         
+        //stat from the 2nd element
         for(int i=1;i<n;i++){
             
+            //find the position of the element where it can be inserted into the dp array
+            //we put the element at found insert position in the dp array
             int pos=findPos(dp,len-1,a[i]);
                 dp[pos]=a[i];
-                
+            
+            //if the position exceeds the current number of elements in the dp array, we increment the
+            //number of elements by 1, which tells the longest increasing subsequence
             if(pos==len)
                 len++;
         }
         return len;
     }
     
+    
+    //function to find the insert position
     private int findPos(int a[], int high, int target){
         
         int l=0, h=high;
