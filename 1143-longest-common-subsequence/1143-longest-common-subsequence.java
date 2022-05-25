c081b76344a4140681adfe807b0e5d2ed0d93efa
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-25 19:22:11 +0530

    Time: 14 ms (76.43%), Space: 46.3 MB (53.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,22 +4,26 @@ class Solution {
         // Integer dp[][]=new Integer[s1.length()][s2.length()];
         // return LCS(s1, s1.length()-1,s2,s2.length()-1,dp);
         
-        int dp[][]=new int[s1.length()+1][s2.length()+1];
         
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
+        //Tabulation approach
+        int dp[][]=new int[s1.length()][s2.length()];
+        
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
                 
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
+                if(s1.charAt(i)==s2.charAt(j)){
+                    dp[i][j]=(i>0 && j>0)?1+dp[i-1][j-1]:1;
                 }
                 else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]=(i>0)?dp[i-1][j]:0;
+                    dp[i][j]=(j>0)?Math.max(dp[i][j],dp[i][j-1]):dp[i][j];
                 }
             }
         }
-        return dp[s1.length()][s2.length()];
+        return dp[s1.length()-1][s2.length()-1];
     }
     
+    //Memoization approach
     private int LCS(String s1, int idx1, String s2, int idx2, Integer dp[][]){
         
         if(idx1<0 || idx2<0)
