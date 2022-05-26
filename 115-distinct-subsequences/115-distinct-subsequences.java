d27101ad77feb869b019370318c6670895083684
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-27 00:13:58 +0530

    Time: 32 ms (30.24%), Space: 50 MB (58.83%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,34 +1,50 @@
 class Solution {
-    public int numDistinct(String s, String t) 
-    {
-        Integer dp[][]=new Integer[s.length()+1][t.length()+1];
-        helper(s,t,s.length(),t.length(),dp);
-        return dp[s.length()][t.length()];
+    public int numDistinct(String s, String t) {
         
+        if(s.length()<t.length())
+            return 0;
+        
+        Integer dp[][]=new Integer[s.length()][t.length()];
+        countDistinct(s,0,t,0,dp);
+        return dp[0][0];
     }
     
-    int helper(String s, String t, int i, int j, Integer dp[][])
-    {
-        if(i<j)
-            return dp[i][j]=0;
+    private int countDistinct(String s, int i, String t, int j, Integer dp[][]){
         
-        if(j==0)
-        {
+        if(i==s.length() && j<t.length())
+            return 0;
+        
+        if(j==t.length()){
             return 1;
         }
         
         if(dp[i][j]!=null)
             return dp[i][j];
         
-        if(s.charAt(i-1)==t.charAt(j-1))
-        {
-            int lres=helper(s,t,i-1,j-1,dp);
-            int rres=helper(s,t,i-1,j,dp);
-            return dp[i][j]=lres+rres;
+        
+        // if the character matches in both the strings, we have 2 choices
+        if(s.charAt(i)==t.charAt(j)){
+            
+            //we can meither mmove both the pointers
+            int moveBothPointerRes=countDistinct(s,i+1,t,j+1,dp);
+            
+            //or we can only move the ith pointer, to cover the subsequence if there are duplicate
+            //adjacent characters that match to the character in t string, so they 
+            //also contribute in forming a possible subsequence, so we explore that path also
+            int moveIthPointerRes=countDistinct(s,i+1,t,j,dp);
+            
+            //and store the sum returned from both the choices
+            dp[i][j]=moveBothPointerRes+moveIthPointerRes;
         }
+        
+        //if the character do not match, we simply move the ith pointer, because we have to find a matching 
+        //character corresponding to the jth position in string t, for moving the jth pointer
+        //because we have to match the whole sting and we can't skip any character of string t
+        //so we dont move jth pointer until we get a matching character corresponding to it
+        
         else
-        {
-            return dp[i][j]=helper(s,t,i-1,j,dp);
-        }
+            dp[i][j]=countDistinct(s,i+1,t,j,dp);
+            
+        return dp[i][j];
     }
 }
