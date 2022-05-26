99473eee7db017f2cacffde200917317cb1ca3de
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-05-26 16:32:04 +0530

    Time: 88 ms (43.45%), Space: 72.3 MB (15.94%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,34 +1,36 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
         
-        StringBuilder sb=new StringBuilder(s);
+        int dp[][]=new int[s.length()][s.length()];
         
-        String revS=sb.reverse().toString();
-        
-        return LCS(s,revS);
-    }
-    
-    public int LCS(String s1, String s2) {
-        
-        // Integer dp[][]=new Integer[s1.length()][s2.length()];
-        // return LCS(s1, s1.length()-1,s2,s2.length()-1,dp);
-        
-        
-        //Tabulation approach
-        int dp[][]=new int[s1.length()][s2.length()];
-        
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
+        for(int gap=0;gap<s.length();gap++){
+            
+            for(int i=0,j=gap;j<s.length();i++,j++){
                 
-                if(s1.charAt(i)==s2.charAt(j)){
-                    dp[i][j]=(i>0 && j>0)?1+dp[i-1][j-1]:1;
+                if(gap==0){
+                    dp[i][j]=1;
                 }
+                
+                else if(gap==1){
+                    if(s.charAt(i)==s.charAt(j)){
+                        dp[i][j]=2;
+                    }
+                    else{
+                        dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+                    }
+                }
+                
                 else{
-                    dp[i][j]=(i>0)?dp[i-1][j]:0;
-                    dp[i][j]=(j>0)?Math.max(dp[i][j],dp[i][j-1]):dp[i][j];
+                    if(s.charAt(i)==s.charAt(j)){
+                        dp[i][j]=2+dp[i+1][j-1];
+                    }
+                    else{
+                        dp[i][j]=Math.max(dp[i+1][j-1],Math.max(dp[i+1][j],dp[i][j-1]));
+                    }
                 }
             }
         }
-        return dp[s1.length()-1][s2.length()-1];
+        
+        return dp[0][s.length()-1];
     }
 }
