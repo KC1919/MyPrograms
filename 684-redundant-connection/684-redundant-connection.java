ab406220c628e1c67a887bf4347bcf7ab0cbe887
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-01 15:51:00 +0530

    Time: 2 ms (35.45%), Space: 43.7 MB (58.20%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,32 +1,42 @@
 class Solution {
-    Set<Integer> seen = new HashSet();
     int MAX_EDGE_VAL = 1000;
 
     public int[] findRedundantConnection(int[][] edges) {
-        ArrayList<Integer>[] graph = new ArrayList[MAX_EDGE_VAL + 1];
-        for (int i = 0; i <= MAX_EDGE_VAL; i++) {
-            graph[i] = new ArrayList();
-        }
-
+        DSU dsu = new DSU(MAX_EDGE_VAL + 1);
         for (int[] edge: edges) {
-            seen.clear();
-            if (!graph[edge[0]].isEmpty() && !graph[edge[1]].isEmpty() &&
-                    dfs(graph, edge[0], edge[1])) {
-                return edge;
-            }
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
+            if (!dsu.union(edge[0], edge[1])) return edge;
         }
         throw new AssertionError();
     }
-    public boolean dfs(ArrayList<Integer>[] graph, int source, int target) {
-        if (!seen.contains(source)) {
-            seen.add(source);
-            if (source == target) return true;
-            for (int nei: graph[source]) {
-                if (dfs(graph, nei, target)) return true;
-            }
+}
+
+class DSU {
+    int[] parent;
+    int[] rank;
+
+    public DSU(int size) {
+        parent = new int[size];
+        for (int i = 0; i < size; i++) parent[i] = i;
+        rank = new int[size];
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public boolean union(int x, int y) {
+        int xr = find(x), yr = find(y);
+        if (xr == yr) {
+            return false;
+        } else if (rank[xr] < rank[yr]) {
+            parent[xr] = yr;
+        } else if (rank[xr] > rank[yr]) {
+            parent[yr] = xr;
+        } else {
+            parent[yr] = xr;
+            rank[xr]++;
         }
-        return false;
+        return true;
     }
 }
