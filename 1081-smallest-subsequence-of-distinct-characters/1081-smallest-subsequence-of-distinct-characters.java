4aff62a4fcfbccdeb6995109d63a8b237039e855
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-02 22:13:10 +0530

    Time: 4 ms (50.21%), Space: 42.1 MB (55.35%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -15,8 +15,13 @@ class Solution {
         
         int n=s.length();
         
+        //this keeps the track of the character 
+        // mapped  to-> Pair(userStatus, lastPos)
         HashMap<Character,Pair>hm=new HashMap<>();
         
+        
+        //we map all the characters with their last position of occurrence in the 
+        //string
         for(int i=s.length()-1;i>=0;i--)
         {
             char ch=s.charAt(i);
@@ -30,26 +35,47 @@ class Solution {
         
         Stack<Character>st=new Stack<>();
         
+        //now we start iterating the string
         for(int i=0;i<s.length();i++)
         {
+            //we take the ith character
             char ch=s.charAt(i);
             
+            //check if that character has been used before or not
+            //if not used, we check
             if(hm.get(ch).used!=true)
             {
+                //we remove all the characters from the stack that have come before
+                //the ith character and that are greater than the ith character
+                //and their last index is also greater than the ith index,
+                //that measn that those greater characters should come after
+                //the ith character, because we have to give the lexicographically
+                //smallest string as the answer
+                
                 while(st.size()>0 && ch<st.peek() && hm.get(st.peek()).lastPos>i)
                 {
+                    //we mark the character being removes as unused
                     hm.get(st.peek()).used=false;
                     st.pop();
                 }
-            
+                
+                //push the current character to the stack
                 st.push(ch);
+                
+                //and mark its status as used
                 hm.get(ch).used=true;
             }
         }
         
+        
+        //when the whole string is traversed only distinct characters are left into
+        //the stack and that to in lexicographical order
+        //so we pop then one by one and append them to the string builder
         while(st.size()>0)
             sb.append(st.pop());
         
+        //return the string after reversing, because the stack reverses the order
+        //so to correct the order we need to reverse the string
         return sb.reverse().toString();
     }
 }
