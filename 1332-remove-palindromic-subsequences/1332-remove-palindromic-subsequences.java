7587e68571a5a47228b85e1e739b3bf2b9878014
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-08 12:07:24 +0530

    Time: 1 ms (18.06%), Space: 41.2 MB (72.92%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        
+        if(s.length()==0){
+            return 0;
+        }
+        
+        else if(s.length()==1 || palindrome(s)){
+            return 1;
+        }
+        
+        int a=0,b=0;
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            
+            if(ch=='a'){
+                a++;
+            }
+            else{
+                b++;
+            }
+        }
+        
+        if(a>0 && b>0){
+            return 2;
+        }
+        
+        else{
+            return 1;
+        }
+        
+    }
+    
+    private boolean palindrome(String s){
+        
+        int l=0, r=s.length()-1;
+        
+        while(l<r){
+            
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+}
