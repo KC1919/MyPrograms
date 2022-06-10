06118d558c10aaae0ebebd80659fb7a255afdc1f
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-10 21:35:14 +0530

    Time: 80 ms (37.23%), Space: 54.2 MB (76.36%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minFallingPathSum(int[][] a) {
+        
+        int n=a.length;
+        
+        Integer dp[][]=new Integer[n][n];
+        
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=a[n-1][j];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            
+            for(int j=0;j<n;j++){
+                
+                int min=Integer.MAX_VALUE;
+                for(int col=0;col<n;col++){
+                    
+                    if(col!=j){
+                        min=Math.min(min,dp[i+1][col]);
+                    }
+                }
+                dp[i][j]=min+a[i][j];
+            }
+        }
+        
+        int min=Integer.MAX_VALUE;
+        
+        for(int col=0;col<n;col++){
+            min=Math.min(min,dp[0][col]);
+        }
+        return min;
+    }
+}
