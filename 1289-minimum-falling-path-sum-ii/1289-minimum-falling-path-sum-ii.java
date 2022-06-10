129fdc415aa4ab8383f9055cc4e2ff5ad4fe58e8
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-10 21:39:48 +0530

    Time: 35 ms (61.96%), Space: 56.5 MB (19.56%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -5,13 +5,8 @@ class Solution {
         
         int dp[][]=new int[n][n];
         
-        int omin=Integer.MAX_VALUE;
-        
         for(int j=0;j<n;j++){
             dp[n-1][j]=a[n-1][j];
-            
-            if(n==1)
-            omin=Math.min(omin,dp[n-1][j]);
         }
         
         for(int i=n-2;i>=0;i--){
@@ -26,11 +21,14 @@ class Solution {
                     }
                 }
                 dp[i][j]=min+a[i][j];
-                if(i==0){
-                    omin=Math.min(omin,dp[i][j]);
-                }
             }
         }
-        return omin;
+        
+        int min=Integer.MAX_VALUE;
+        
+        for(int col=0;col<n;col++){
+            min=Math.min(min,dp[0][col]);
+        }
+        return min;
     }
 }
