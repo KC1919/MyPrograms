dee3b34ba8355463c012c23b3024ca848b64145b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-16 10:34:51 +0530

    Time: 37 ms (81.31%), Space: 42.5 MB (87.20%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -6,21 +6,34 @@ class Solution {
         
         for(int i=0;i<len;i++)
         {
-            
+            //this condition saves us from calculating further
+            //because the (len-i) fives the length of the remaining string left to
+            //processed, and the maximum palindrome it can generate is twice its
+            //length, so if we have already got a max length palindromic substring
+            //whose length is greater than the maximum length that may be formed
+            //with rest of the remaining string at some position of i, so its invain
+            //to calculate that, so we break
             if(max>(len-i)*2)
                 break;
             
+            //this gives the longest palindromic substring starting with a single
+            //midpoint, which is in case of odd length palindromic string
             int a1[]=palindrome(s,i,i);
             
-            if(a1[0]!=-1 && a1[1]!=-1 && a1[1]-a1[0]+1>max)
-            {
+            //checking if current position can give the longer palindromic substring
+            if(a1[0]!=-1 && a1[1]!=-1 && a1[1]-a1[0]+1>max){
                 max=a1[1]-a1[0]+1;
                 li=a1[0];
                 ri=a1[1];
             }
+            
+            //this gives the longest palindromic substring starting with two
+            //midpoints, which is in case of the even length palindromic string
+            //which have 2 middle characters same, so we start left pointer from the
+            //1st mid point and the right pointer from the 2nd midpoint and expand
             int a2[]=palindrome(s,i,i+1);
-            if(a2[0]!=-1 && a2[1]!=-1 && a2[1]-a2[0]+1>max)
-            {
+            
+            if(a2[0]!=-1 && a2[1]!=-1 && a2[1]-a2[0]+1>max){
                 max=a2[1]-a2[0]+1;
                 li=a2[0];
                 ri=a2[1];
@@ -33,9 +46,12 @@ class Solution {
         return s.substring(li,ri+1);
     }
     
+    //returning the start and end indices of the longest palindromic substring
+    //that can be enerated starting from the left and right passed indices
     public int[] palindrome(String s, int left, int right)
     {
         int li=-1, ri=-1, max=0;
+        
         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right))
         {
             if(s.charAt(left)==s.charAt(right))
