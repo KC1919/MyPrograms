e333738cc7563fa3928f52ae31797a94eabda1d1
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-17 21:53:28 +0530

    Time: 5 ms (8.53%), Space: 43.9 MB (23.43%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans;
+    Set<TreeNode> covered;
+    public int minCameraCover(TreeNode root) {
+        ans = 0;
+        covered = new HashSet();
+        covered.add(null);
+
+        dfs(root, null);
+        return ans;
+    }
+
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            dfs(node.left, node);
+            dfs(node.right, node);
+
+            if (par == null && !covered.contains(node) ||
+                    !covered.contains(node.left) ||
+                    !covered.contains(node.right)) {
+                ans++;
+                covered.add(node);
+                covered.add(par);
+                covered.add(node.left);
+                covered.add(node.right);
+            }
+        }
+    }
+}
