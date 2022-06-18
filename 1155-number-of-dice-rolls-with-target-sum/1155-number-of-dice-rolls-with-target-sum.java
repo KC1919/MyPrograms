4cd0bef4014dd32fcc4c64624ab2c57abffbbee8
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-19 00:55:32 +0530

    Time: 13 ms (88.80%), Space: 43.6 MB (33.16%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -6,32 +6,45 @@ class Solution {
     
     private static long countways(int m, int n, int idx, int target, Long dp[][]){
         
+        //if all the dices have been thrown
         if(idx==n){
             
+            //and the target has been made, we return 1 to signify success
             if(target==0){
                 return 1;
             }
             return 0;
         }
         
+        //if the answer to the current state is already stored so we return that
         if(dp[idx][target]!=null){
             return dp[idx][target];
         }
         
+        //take a count variable to store the total number of ways
         long count=0;
         
+        //check if the dice number is in range
         if(idx<n){
             
+            //every dice has 1 to k options to chose from
             for(int i=1;i<=m;i++){
                 
+                //check if the current chosen number is less that or greater than the target
                 if(i<=target){
+                    
+                    //make call to the next dice with the remaining target sum
                     long res=countways(m,n,idx+1,target-i,dp);
+                    
+                    //adding to the count the returned number of ways
                     if(res>0){
                         count+=res;
                     }
                 }
             }
         }
+        
+        //storing the answer in the dp table
         return dp[idx][target]=count%1000000007;
     }
 }
