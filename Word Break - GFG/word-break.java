f07e45ff608e892dc8574b83ab0c2fa0b815bc40
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-18 22:48:54 +0530

    Added solution - LeetHub

--- Word Break - GFG/word-break.java
+++ Word Break - GFG/word-break.java
@@ -36,17 +36,23 @@ class Sol
     {
         HashSet<String>hm=new HashSet<>();
         
+        HashMap<String,Integer>hmap=new HashMap<>();
+        
         for(String word:list){
             hm.add(word);
         }
         
-        return breakWord(s,0,hm);
+        return breakWord(s,0,hm,hmap);
     }
     
-    private static int breakWord(String s, int idx, HashSet<String>hm){
+    private static int breakWord(String s, int idx, HashSet<String>hm, HashMap<String,Integer>hmap){
         
         if(idx==s.length()){
             return 1;
+        }
+        
+        if(hmap.containsKey(s.substring(idx))){
+            return hmap.get(s.substring(idx));
         }
         
         for(int i=idx;i<s.length();i++){
@@ -54,12 +60,15 @@ class Sol
             String word=s.substring(idx,i+1);
             
             if(hm.contains(word)){
-                int res=breakWord(s,i+1,hm);
+                int res=breakWord(s,i+1,hm,hmap);
                 if(res==1){
+                    hmap.put(word,1);
                     return 1;
                 }
             }
         }
+        
+        hmap.put(s.substring(idx),0);
         return 0;
     }
 }
