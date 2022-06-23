2dbc3af0fa9483e60e187fd6db7dfb5b0bc873d8
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-23 20:48:21 +0530

    Time: 208 ms (6.85%), Space: 50.6 MB (79.63%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -6,17 +6,17 @@ public class Solution {
         for (int i = 0; i < courses.length; i++) {
             if (time + courses[i][0] <= courses[i][1]) {
                 time += courses[i][0];
-                count++;
+                courses[count++] = courses[i];
             } else {
                 int max_i = i;
-                for (int j = 0; j < i; j++) {
+                for (int j = 0; j < count; j++) {
                     if (courses[j][0] > courses[max_i][0])
                         max_i = j;
                 }
                 if (courses[max_i][0] > courses[i][0]) {
                     time += courses[i][0] - courses[max_i][0];
+                    courses[max_i] = courses[i];
                 }
-                courses[max_i][0] = -1;
             }
         }
         return count;
