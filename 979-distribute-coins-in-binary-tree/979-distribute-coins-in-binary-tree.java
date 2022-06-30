6b4fabab888bfed5a64bd97178f59c16d6969e38
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-06-30 17:33:45 +0530

    Time: 1 ms (58.54%), Space: 42.3 MB (67.39%) - LeetHub

--- 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
@@ -7,10 +7,22 @@ class Solution {
     }
 
     public int dfs(TreeNode node) {
-        if (node == null) return 0;
-        int L = dfs(node.left);
-        int R = dfs(node.right);
-        ans += Math.abs(L) + Math.abs(R);
-        return node.val + L + R - 1;
+        if (node == null) 
+            return 0;
+        
+        int lres = dfs(node.left);
+        int rres = dfs(node.right);
+        
+        //this stores the steps required
+        ans += Math.abs(lres) + Math.abs(rres);
+        
+        //this returns the required or excess coins to the parent depending on the
+        //coins they have with them(excess or deficient)
+        
+        //we are doing -1 because 1 coin is needed for the current node
+        //so whatever coins we have in total after getting the result from our
+        //children, we take 1 coin for ourself and return rest of the coins to the
+        //parent
+        return node.val + lres + rres - 1;
     }
 }
