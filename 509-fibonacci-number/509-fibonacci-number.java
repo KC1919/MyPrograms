8cd1195b5b9817cb973ecff2ef63a176393ba687
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-06 14:15:11 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (28.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -5,7 +5,27 @@ class Solution {
             return n;
         }
         
-        return fib(n-1)+fib(n-2);
+        int dp[]=new int[n+1];
         
+        Arrays.fill(dp,-1);
+        fibo(n,dp);
+        
+        return dp[n];
+    }
+    
+    private int fibo(int n, int dp[]){
+        
+        if(n==0 || n==1){
+            return dp[n]=n;
+        }
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        int fm1=fibo(n-1,dp);
+        int fm2=fibo(n-2,dp);
+        
+        return dp[n]=fm1+fm2;
     }
 }
