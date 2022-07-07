931dfee9e2a0098875bea9fb2a774fba38b541ac
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-08 01:09:31 +0530

    Time: 7 ms (68.33%), Space: 49.9 MB (19.79%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,41 +1,67 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < prerequisites.length; i++) {
-            int[] arr = prerequisites[i];
-            if (map.containsKey(arr[0])) {
-                map.get(arr[0]).add(arr[1]);
-            } else {
-                List<Integer> l = new ArrayList<>();
-                l.add(arr[1]);
-                map.put(arr[0], l);
+    
+    class Edge
+    {
+        int src;
+        int nbr;
+        
+        Edge(int src, int nbr){
+            this.src=src;
+            this.nbr=nbr;
+        }
+    }
+    public boolean canFinish(int vtces, int[][] a) {
+        
+        ArrayList<Edge>graph[]=new ArrayList[vtces];
+        
+        for(int i=0;i<vtces;i++){
+            graph[i]=new ArrayList<>();
+        }
+        
+        for(int i=0;i<a.length;i++){
+            int src=a[i][0];
+            int nbr=a[i][1];
+            
+            graph[src].add(new Edge(src,nbr));
+        }
+        
+        boolean visited[]=new boolean[vtces];
+        boolean dfsvis[]=new boolean[vtces];
+        
+        for(int i=0;i<vtces;i++){
+            if(visited[i]==false){
+                if(detect(graph,i,visited,new boolean[vtces])){
+                    return false;
+                }
             }
         }
-        boolean[] visited = new boolean[numCourses];
-        boolean[] recStack = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            if (isCyclic(i, visited, recStack, map)) {
-                return false;
-            }
-        }
+        
         return true;
     }
-    public boolean isCyclic(int i, boolean[] visited, boolean[] recStack, HashMap<Integer, List<Integer>> map) {
-        if (recStack[i]) {
-            return true;
-        }
-        if (visited[i]) {
-            return false;
-        }
-        visited[i] = true; 
-        recStack[i] = true; 
-        for (int j = 0; j < map.getOrDefault(i, new ArrayList<>()).size(); j++) {
-            if (isCyclic(map.get(i).get(j), visited, recStack, map)) {
+    
+    private boolean detect(ArrayList<Edge>graph[], int src, boolean visited[], boolean dfsvisit[])
+    {
+        
+        visited[src]=true;
+        dfsvisit[src]=true;
+        
+        for(Edge e:graph[src]){
+            if(dfsvisit[e.nbr]==true && visited[e.nbr]==true){
                 return true;
             }
+            
+            if(visited[e.nbr]==false){
+                boolean res=detect(graph,e.nbr,visited,dfsvisit);    
+                
+                if(res==true){
+                    return true;
+                }
+            }
         }
-        recStack[i] = false; 
-  
+        
+        dfsvisit[src]=false;
+        
         return false;
     }
+    
 }
