01ad5d7ea1b30613a41c80c8fa682d5e0a8949a1
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-07 12:06:28 +0530

    Time: 2 ms (88.75%), Space: 42.5 MB (49.25%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,48 +1,58 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         
-        if(s1.length()+s2.length()<s3.length())
+        int n=s1.length();
+        int m=s2.length();
+        
+        if(s3.length()>n+m){
             return false;
+        }
         
-        Integer dp[][]=new Integer[s1.length()+1][s2.length()+1];
+        Boolean dp[][]=new Boolean[n+1][m+1];
         
-        return check(s1,0,s2,0,s3,0,dp);
+        return interleave(s1,0,n,s2,0,m,s3,0,dp);
     }
     
-    private boolean check(String s1, int i, String s2, int j, String s3, int idx, Integer dp[][]){
-        
-        if(i==s1.length() && j==s2.length()){
-            dp[i][j]=1;
-            return true;
+    private boolean interleave(String s1, int idx1, int n, String s2, int idx2, int m, String s3, int idx3, Boolean dp[][])
+    {
+        if(idx1==n && idx2==m){
+            return dp[idx1][idx2]=true;
         }
         
-        if(dp[i][j]!=null){
-            return dp[i][j]==0?false:true;
+        if(dp[idx1][idx2]!=null){
+            return dp[idx1][idx2];
         }
         
-        if(i<s1.length() && j<s2.length() && idx<s3.length() && s1.charAt(i)==s3.charAt(idx) && s2.charAt(j)==s3.charAt(idx)){
-            boolean res1=check(s1,i+1,s2,j,s3,idx+1,dp);
-            boolean res2=check(s1,i,s2,j+1,s3,idx+1,dp);
+        if(idx1<n && idx2<m && idx3<s3.length() && s1.charAt(idx1)==s2.charAt(idx2) && s2.charAt(idx2)==s3.charAt(idx3)){
             
-            if(res1 || res2){
-                dp[i][j]=1;
-                return true;
+            boolean res1=interleave(s1,idx1+1,n,s2,idx2,m,s3,idx3+1,dp);
+            
+            if(res1==true){
+                return dp[idx1][idx2]=true;
+            }
+            boolean res2=interleave(s1,idx1,n,s2,idx2+1,m,s3,idx3+1,dp);
+            
+            if(res2==true){
+                return dp[idx1][idx2]=true;
             }
         }
         
-        else if(i<s1.length() && idx<s3.length() && s1.charAt(i)==s3.charAt(idx)){
-            boolean res=check(s1,i+1,s2,j,s3,idx+1,dp);
-            dp[i][j]=res==true?1:0;
-            return res;
+        else if(idx1<n && idx3<s3.length() && s1.charAt(idx1)==s3.charAt(idx3)){
+            boolean res=interleave(s1,idx1+1,n,s2,idx2,m,s3,idx3+1,dp);
+            
+            if(res==true){
+                return dp[idx1][idx2]=true;
+            }
         }
-                
-        else if(j<s2.length() && idx<s3.length() && s2.charAt(j)==s3.charAt(idx)){
-            boolean res=check(s1,i,s2,j+1,s3,idx+1,dp);
-            dp[i][j]=res==true?1:0;
-            return res;
+        
+        else if(idx2<m && idx3<s3.length() && s2.charAt(idx2)==s3.charAt(idx3)){
+            boolean res=interleave(s1,idx1,n,s2,idx2+1,m,s3,idx3+1,dp);
+            
+            if(res==true){
+                return dp[idx1][idx2]=true;
+            }
         }
-               
-        dp[i][j]=0;
-        return false;
+        
+        return dp[idx1][idx2]=false;
     }
 }
