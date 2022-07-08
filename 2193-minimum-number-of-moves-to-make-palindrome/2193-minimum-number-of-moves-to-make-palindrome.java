c9deb889a2113878263dab87987e3cb5886f9726
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-09 00:00:00 +0530

    Time: 15 ms (64.45%), Space: 43.2 MB (36.31%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.java
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.java
@@ -1,42 +1,62 @@
 class Solution {
-    public int minMovesToMakePalindrome(String s) {
+    public int minMovesToMakePalindrome(String str) {
+        int n=str.length();
+        char[] s=str.toCharArray();
+        int lptr=0;
+        int rptr=n-1;
         
-        int left=0;
-        int right=s.length()-1;
-        
-        char a[]=s.toCharArray();
-        
-        int count=0;
-        
-        while(left<right){
+        int res=0;
+        while(lptr<rptr){
+            int r=rptr;
             
-            if(a[left]!=a[right]){
-                int r=right;
-                while(a[left]!=a[r])
-                   r--;
-                
-                if(left==r){
-                    char temp=a[left];
-                    a[left]=a[left+1];
-                    a[left+1]=temp;
-                    count++;
-                    
-                    continue;
-                }
-                
-                else{
-                    for(int j=r;j<right;j++){
-                        char temp=a[j];
-                        a[j]=a[j+1];
-                        a[j+1]=temp;
-                           
-                        count++;
-                    }
-                } 
+            //if the left and the right character matches, then nothing to do
+            //move both the pointer ahead
+            if(s[lptr]==s[r]){
+                lptr++;
+                rptr--;
+                continue;
             }
-            left++;
-            right--;
+            
+            //if the left and right character mismatch
+            //then keep moving the right pointer till any matching character
+            //is reached
+            while(s[lptr]!=s[r]) r--;
+            
+            
+            //if no matching chracter is found, and the right pointer reached
+            //left pointer, that means this charzcter will become the middle 
+            //character, since it does not have any pair
+            
+            //so we swap the character with the character on its right
+            if(lptr==r){
+                swap(s,lptr,lptr+1);
+                
+                //increment the count of swaps by 1
+                res++;
+            }
+            else{
+                
+                //if the matching character is found, we take it to the right
+                // pointer, to make corresponding match to the left
+                while(r!=rptr){
+                    
+                    //keep swapping the chracter with its right character
+                    //till the right pointer is reached
+                    swap(s,r,r+1);
+                    r++;
+                    
+                    //keep incrementing the count of swaps
+                    res++;
+                }
+            }
         }
-        return count;
+        return res;
+    }
+    
+    //function to swap the characters
+    public void swap(char[] s,int l,int r){
+        char temp=s[l];
+        s[l]=s[r];
+        s[r]=temp;
     }
 }
