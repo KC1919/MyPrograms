078e60255c96c49a2cba61ee84b48a310edb9807
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-09 16:18:27 +0530

    Time: 51 ms (46.31%), Space: 42.1 MB (86.47%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -4,53 +4,79 @@ class Solution {
     public int maxLength(List<String> list) {
         
         max=0;
-        subsequence(list,0,new ArrayList<>());
+        subsequence(list,0,new ArrayList<>(),"",new StringBuilder());
         return max;
     }
     
-    private void subsequence(List<String>list, int idx, List<String>res)
+    private void subsequence(List<String>list, int idx, List<String>res, String s, StringBuilder sb)
     {
         if(idx==list.size()){
             if(res.size()>0){
                 
-                StringBuilder sb=new StringBuilder();
-                
+                // StringBuilder sb=new StringBuilder();
+                int size=0;
                 for(String ele:res){
-                    sb.append(ele);
+                    // sb.append(ele);
+                    size+=ele.length();
                 }
                 
-                if(check(sb.toString())){
-                    max=Math.max(max,sb.length());
-                }
+                // if(check(sb.toString())){
+                    max=Math.max(max,size);
+                // }
             }
             return;
         }
         
         //choose
-        res.add(list.get(idx));
-        subsequence(list,idx+1,res);
-        res.remove(res.size()-1);
+        
+        if(checko(s) && checko(list.get(idx)) && check(s,list.get(idx))){
+            res.add(list.get(idx));
+            sb.append(list.get(idx));
+            subsequence(list,idx+1,res,sb.toString(),sb);
+            res.remove(res.size()-1);
+            sb=new StringBuilder(s);
+        }
+        
             
         //don't choose
-        subsequence(list,idx+1,res);
+        subsequence(list,idx+1,res,s,sb);
     }
     
     
-    private boolean check(String s)
+    private boolean check(String s, String t)
     {
+        HashSet<Character>hm=new HashSet<>();
+        
+        for(int i=0;i<t.length();i++){
+            
+            char ch=t.charAt(i);
+            
+            hm.add(ch);
+        }
+        
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(hm.contains(ch)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean checko(String s){
+        
         HashSet<Character>hm=new HashSet<>();
         
         for(int i=0;i<s.length();i++){
             
             char ch=s.charAt(i);
             
-            if(hm.contains(ch)){
+            if(hm.contains(ch))
                 return false;
-            }
             
             hm.add(ch);
         }
-        
         return true;
     }
 }
