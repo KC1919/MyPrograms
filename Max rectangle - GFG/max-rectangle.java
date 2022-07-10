479832f4e48a5816a20bd76c78e7535b6016b1ec
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-10 20:47:07 +0530

    Added solution - LeetHub

--- Max rectangle - GFG/max-rectangle.java
+++ Max rectangle - GFG/max-rectangle.java
@@ -66,37 +66,32 @@ class Solution {
         for(int i=1;i<n;i++)
         {
             int j=i-1;
-            while(j>=0 && heights[j]>=heights[i])
-            {
+            while(j>=0 && heights[j]>=heights[i]){
                 j=ls[j];
             }
+            
             if(j==-1)
-            {
                 ls[i]=-1;
-            }
+            
             else
-            {
                 ls[i]=j;
-            }
         }
+        
         int rs[]=new int[n];
         rs[n-1]=n;
         
         for(int i=n-2;i>=0;i--)
         {
             int j=i+1;
-            while(j<n && heights[j]>=heights[i])
-            {
+            while(j<n && heights[j]>=heights[i]){
                 j=rs[j];
             }
+            
             if(j==n)
-            {
                 rs[i]=n;
-            }
+                
             else
-            {
                 rs[i]=j;
-            }
         }
         
         int max=0;
