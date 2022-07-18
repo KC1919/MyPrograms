08d012d93a9ee34c89b840a7d8d6b9762fb01111
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-18 15:12:17 +0530

    Time: 1 ms (74.47%), Space: 42.5 MB (54.41%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,31 +1,39 @@
 class Solution {
     public int search(int[] a, int target) {
         
+        
         int l=0, h=a.length-1;
         
-        while(l<=h)
-        {
+        while(l<=h){
+            
             int mid=(l+h)/2;
             
-            if(a[mid]==target)
+            if(a[mid]==target){
                 return mid;
+            }
             
-            else if(a[l]<=a[mid]){
-                if(target>=a[l] && target<a[mid]){
+            else if(a[0]<=a[mid]){
+                
+                if(target>=a[0] && target<=a[mid]){
                     h=mid-1;
                 }
-                else
+                
+                else{
                     l=mid+1;
+                }
             }
             
-            else{
-                if(target>a[mid] && target<=a[h])
+            else if(a[mid]<=a[h]){
+                if(target>=a[mid] && target<=a[h]){
                     l=mid+1;
+                }
                 
-                else
+                else{
                     h=mid-1;
+                }
             }
         }
+        
         return -1;
     }
 }
