20bc102185848ef156706905f8a5b0e7891ea573
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-19 01:59:35 +0530

    Time: 1 ms (100.00%), Space: 74 MB (46.46%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -9,10 +9,8 @@ class Solution {
             sum+=a[i];
             max=Math.max(sum,max);
             
-            if(sum<0){
+            if(sum<0)
                 sum=0;
-                continue;
-            }
         }
         return max;
     }
