2006da42d50fe4599e8035362c76a2e4ea351763
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-18 15:55:44 +0530

    Time: 175 ms (8.64%), Space: 73.9 MB (6.89%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,76 +1,43 @@
 class Solution {
-    
     public int coinChange(int[] coins, int amount) {
         
-        if(amount==0)
-            return 0;
+        int n=coins.length;
+        Integer dp[][]=new Integer[n][amount+1];
         
-        Integer dp[]=new Integer[amount+1];
+        int res=minCoins(coins,0,amount,dp);
         
-        int res=coinSum(coins,amount,dp);
-        
-        //so when the function willbe completed, we will be having the result in
-        //the dp array at the amount'th position, and we return it
-        
-        return dp[amount]==0?-1:dp[amount];
+        return res==Integer.MAX_VALUE?-1:res-1;
     }
     
-    public int coinSum(int a[], int target, Integer dp[]){
+    private int minCoins(int a[], int idx, int target, Integer dp[][]){
         
-        if(target==0){
-            dp[target]=0;
-            return 10001; //this tells that the target was achieved
+        if(idx==a.length || target==0){
+            
+            if(target==0){
+                return 1;
+            }
+            
+            return 0;
         }
         
-        //if there is a value already at the amount'th index, that means that amount
-        //has been calculated before, so instead of recalculating it we return the stored
-        //answer
-        if(dp[target]!=null)
-            return dp[target];
+        if(dp[idx][target]!=null){
+            return dp[idx][target];
+        }
         
-        //we take the minimum coins to form a target as the maximum value
         int min=Integer.MAX_VALUE;
         
-        //then we have the option to choose any coins any number of time
-        for(int coin:a){
+        if(target>=a[idx]){
+            int res=minCoins(a,idx,target-a[idx],dp);
+            min=Math.min(res,min);
+        }
         
-            //we check if the coin value is less than or equal to the target amount
-            if(coin<=target){
-                
-                //we use the coins value and then make call to calculate the new reduced
-                //target
-                int res=coinSum(a,target-coin,dp);
-                
-                //this condition denotes the target was reached successfully
-                if(res==10001){
-                    //so we put 1 at the target'th position, which means 1 coin was needed
-                    //reach the target(means that coin value was present in the coins array)
-                    return dp[target]=1;
-                }
-                
-                //else this condition tells that the returned result
-                //denotes the number of coins required to achive the target
-                else if(res!=10001 && res>0){
-                    //we keep updating our min value variable with the returned result
-                    min=Math.min(min,res);
-                    
-                    //and also keep updating the target'th index value accordingly
-                    //because wee nedd the minimum number of coins to form the target
-                    dp[target]=min;
-                }
+        for(int i=idx+1;i<a.length;i++){
+            if(target>=a[i]){
+                int res=minCoins(a,i,target-a[i],dp);
+                min=Math.min(res,min);
             }
         }
         
-        //if the min is not updated, means the ytarget was not formed
-        if(min==Integer.MAX_VALUE)
-            dp[target]=0; //so we put 0 at the target'th index
-        
-        //else this means the target was achieved and to form current target
-        //adding 1 coin to the returned result
-        else
-            dp[target]+=1;
-        
-        //finally returning the result to the below call
-        return dp[target];
+        return dp[idx][target]=min==Integer.MAX_VALUE?min:min+1;
     }
 }
