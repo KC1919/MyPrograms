67084699c70a3d09eb25cedfef9c6046d94f6882
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-18 20:27:46 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (37.89%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,24 +3,16 @@ class Solution {
         
         int n=a.length;
         
-        if(n==1){
-            return a[0];
+        int incl=0, excl=0;
+        
+        for(int i=0;i<n;i++){
+            
+            int nincl=excl+a[i];
+            int nexcl=Math.max(incl,excl);
+            
+            incl=nincl;
+            excl=nexcl;
         }
-        
-        if(n==2){
-            return Math.max(a[0],a[1]);
-        }
-        
-        int dp[]=new int[n];
-        
-        dp[n-1]=a[n-1];
-        dp[n-2]=a[n-2];
-        dp[n-3]=dp[n-1]+a[n-3];
-        
-        for(int i=n-4;i>=0;i--){
-            dp[i]=Math.max(dp[i+2],dp[i+3])+a[i];
-        }
-        
-        return Math.max(dp[0],dp[1]);
+        return Math.max(incl,excl);
     }
 }
