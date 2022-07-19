7a00d8681c661e8cdf575b25b9d737a593ebdd41
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-19 11:31:09 +0530

    Time: 21 ms (51.34%), Space: 47 MB (81.88%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,84 +1,35 @@
-class Solution 
-{
-    public List<Integer> findAnagrams(String s, String p) 
-    {
-        int slen=s.length();
-        int plen=p.length();
-        ArrayList<Integer>list=new ArrayList<>();
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
         
-        if(plen>slen)
-            return new ArrayList<>();
-        
-        if(slen==20001)
-        {
-            list.add(0);
-            list.add(10001);
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+        List<Integer> list = new ArrayList<>();
+
+        if(s.length()<p.length())
             return list;
-        }
-        
-        if(slen==20098)
-        {
-            for(int i=0;i<=10062;i++)
-            {
-                list.add(i);
-            }
-            return list;
-        }
-        
-        HashMap<Character,Integer>smap=new HashMap<>();
-        HashMap<Character,Integer>pmap=new HashMap<>();
-        
-        for(int i=0;i<plen;i++)
-        {
-            char ch=p.charAt(i);
-            pmap.put(ch,pmap.getOrDefault(ch,0)+1);
-        }
-        
-        for(int i=0;i<plen;i++)
-        {
-            char ch=s.charAt(i);
-            smap.put(ch,smap.getOrDefault(ch,0)+1);
-        }
-        
-        int j=plen;
-        int k=0;
 
-        
-        while(j<slen)
-        {
-            if(compare(pmap,smap))
-            {
-                list.add(k);
-            }
-            
-            char ich=s.charAt(j);
-            smap.put(ich,smap.getOrDefault(ich,0)+1);
-            
-            char dch=s.charAt(k);
-            
-            if(smap.get(dch)==1)
-                smap.remove(dch);
-
-            else
-                smap.put(dch,smap.get(dch)-1);
-
-            j++;
-            k++;
+        for(int i=0; i<p.length(); i++){
+            freq1[s.charAt(i)-'a']++;
+            freq2[p.charAt(i)-'a']++;
         }
-        
-        if(compare(pmap,smap))
-            list.add(k);
-        
-        return list;
-    }
-    
-    public boolean compare(HashMap<Character,Integer>pmap, HashMap<Character,Integer>smap)
-    {
-        for(char ele:smap.keySet())
-        {
-            if(pmap.getOrDefault(ele,0)!=smap.get(ele))
-                return false;
+
+        int start=0;
+        int end=p.length();
+
+        if(Arrays.equals(freq1,freq2))
+            list.add(start);
+
+        while(end<s.length()){
+
+            freq1[s.charAt(start)-'a']--;
+            freq1[s.charAt(end)-'a']++;
+
+            if(Arrays.equals(freq1,freq2))
+            list.add(start+1);
+
+            start++;
+            end++;
         }
-        return true;
+        return list;  
     }
 }
