9e1ee92983a38fadc195c71d4e81464d623648da
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-20 10:23:22 +0530

    Time: 3 ms (51.58%), Space: 57.9 MB (37.97%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,26 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] a) {
+        
         int n=a.length;
-        int ans[]=new int[n];
+        int self[]=new int[n];
         
-        //Here first we calculate the prefix product for every ith position
-        int prefix=1;
-        
+        int prod=1;
         for(int i=0;i<n;i++){
-            ans[i]=prefix;
-            prefix*=a[i];
+            self[i]=prod;
+            prod*=a[i];
         }
         
-        //then we start traversing the array from the end, and take postfix product, when we reach the ith position we have with us the postfix product we calculated till ith position, So we multiply the prefix product already stored at ith position with the postfix product and we store the result at the ith position.
-        
-        //using this strategy, we are able to save space of storing prefix and postfix in seperate arrays.
-        
-        int postfix=1;
-        
+        prod=1;
         for(int i=n-1;i>=0;i--){
-            ans[i]*=postfix;
-            postfix*=a[i];
+            self[i]*=prod;
+            prod*=a[i];
         }
-        return ans;
+        
+        return self;
     }
 }
