d43b3db1f1701ec5b4232fc71f0ab220773ee47d
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-20 23:10:40 +0530

    Time: 10 ms (17.63%), Space: 54.9 MB (79.58%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] sortedSquares(int[] a) {
+        
+        for(int i=0;i<a.length;i++){
+            a[i]=a[i]*a[i];
+        }
+        
+        Arrays.sort(a);
+        
+        return a;
+    }
+}
