ed5581586d2311a15ed174c73b7f9f8dcfcc35c4
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-20 11:54:21 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (38.30%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,35 +1,36 @@
 class Solution {
     public int findMin(int[] a) {
         
-        int l=0, h=a.length-1, mid=-1;
+        int high=a.length-1;
+        int low=0;
+        int mid=-1;
         
-        while(l<=h)
-        {
-            mid=(l+h)/2;
+        while(low<=high){
+            mid=(low+high)/2;
             
-            //we check if the element at left index is smaller than the element at
-            //mid index, means the part from left to mid is sorted
-            if(a[l]<=a[mid]){
+            if(a[low]<=a[mid]){
                 
-                //so we check if the element at left  index is greater than the 
-                //element at right index, means the element cannot be in this part
-                //so we discard this half and move to the right half of the array
-                if(a[l]>a[h])
-                    l=mid+1;
-                
-                //else if the element at lower index is smaller than the element at right
-                //index, that means the whole part is sorted and the left index
-                //is the smallest element so we return it.
-                else
-                    return a[l];
+                if(a[low]<=a[high]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
             
-            //if the element at left index is greater than the element at mid index
-            //that means the element is between the left index and the mid index
-            //so we shift the right pointer to the mid index
-            else
-                h=mid;
+            else{
+                
+                if(a[mid]<=a[high]){
+                    high=mid;
+                }
+                
+                else{
+                    low=mid;
+                }
+                
+            }
         }
-        return a[l];
+        
+        return a[low];
     }
 }
