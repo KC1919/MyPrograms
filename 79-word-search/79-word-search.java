3d0e293d000b10ed00ddb59adc38527273fb73d7
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-20 22:10:55 +0530

    Time: 200 ms (37.97%), Space: 41.9 MB (69.16%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -5,64 +5,46 @@ class Solution {
         int m=a[0].length;
         
         boolean visited[][]=new boolean[n][m];
-        StringBuilder sb=new StringBuilder();
-        
-        int dir[][]={{1,0},{-1,0},{0,-1},{0,1}};
-        
-        char sc=word.charAt(0);
+        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
         
         for(int i=0;i<n;i++){
+            
             for(int j=0;j<m;j++){
                 
-                //we only make calls from those cells whose character match
-                //to the starting character of the word to be searched
-                if(a[i][j]==sc){
-                    sb.append(a[i][j]);
-                    boolean res=findWord(a,i,j,n,m,visited,sb,word,dir);
-                    sb.deleteCharAt(sb.length()-1);
-                    
-                    if(res==true)
+                if(word.charAt(0)==a[i][j] && visited[i][j]==false){
+                                    
+                    boolean res=search(a,1,i,j,word,visited,dir,1);
+                    if(res){
                         return true;
+                    }
                 }
             }
         }
         return false;
     }
-    public boolean findWord(char a[][], int i, int j, int n, int m, boolean visited[][], StringBuilder sb, String word, int dir[][])
-        {
-            if(sb.length()==word.length()){
-                return true;
-            }
+    
+    private boolean search(char a[][], int idx, int i, int j, String word, boolean visited[][], int dir[][], int count){
+        
+        if(count==word.length()){
+            return true;
+        }
+        
+        visited[i][j]=true;
+        
+        for(int k=0;k<4;k++){
+            int row=dir[k][0]+i;
+            int col=dir[k][1]+j;
             
-            visited[i][j]=true;
-            
-            //searching in all 4 direction from the current cell
-            for(int k=0;k<4;k++){
-                int row=dir[k][0]+i;
-                int col=dir[k][1]+j;
-                
-                //picking the character only if it is valid
-                //means it is present in the word
-                if(row>=0 && row<n && col>=0 && col<m && a[row][col]==word.charAt(sb.length()) && visited[row][col]==false)
-                {
-                    //pappend the character
-                    sb.append(a[row][col]);
-                    
-                    //make call to search the next character in sequence of the word
-                    boolean res=findWord(a,row,col,n,m,visited,sb,word,dir);
-                    
-                    //if the returned result is true, means the word was formed
-                    //so we return true
-                    if(res==true)
-                        return true;
-                    
-                    //backtracking ,deleting the appended character
-                    sb.deleteCharAt(sb.length()-1);
+            if(row>=0 && col>=0 && row<a.length && col<a[0].length && visited[row][col]==false && a[row][col]==word.charAt(idx))
+            {
+                boolean res=search(a,idx+1,row,col,word,visited,dir,count+1);   
+                if(res){
+                    return true;
                 }
             }
-        
-            //marking the cell as unvisited
-            visited[i][j]=false;
-            return false;
         }
+        
+        visited[i][j]=false;
+        return false;
+    }
 }
