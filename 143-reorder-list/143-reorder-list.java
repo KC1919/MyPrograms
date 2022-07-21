6c07d29fabfec6fc058277f5a789e526f4fc6af9
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-21 14:26:36 +0530

    Time: 1 ms (100.00%), Space: 54.5 MB (29.14%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -11,55 +11,41 @@
 class Solution {
     public void reorderList(ListNode head) {
         
-        if(head==null || head.next==null)
+        if(head==null || head.next==null){
             return;
+        }
         
-        ListNode slow=head, fast=head, prev=null;
+        ListNode slow=head, fast=head, prev=null;;
         
-        //first we find the mid point of the list
         while(fast!=null && fast.next!=null){
             prev=slow;
             slow=slow.next;
             fast=fast.next.next;
         }
         
-        //then we reverse the right half of the list
-        ListNode currRight=reverse(slow); //returns the head of the reversed list
+        prev.next=null;
+        ListNode rhead=reverse(slow);
         
-        prev.next=null; //making the next of the last node of the left half equal to null
-                        //so that there occurs no cycle
+        slow=head;
         
-        //now we start from the head of left half of the list
-        //and head of the reversed right half of the list
-        //means the last node of the list
-        ListNode currLeft=head, nextLeft=null, nextRight=null;
-        int turn=0;
-        
-        //now we iterate through both the halves of the list, till any one half of the 
-        //list ends
-        while(currLeft!=null && currRight!=null){
+        while(slow!=null && rhead!=null){
             
-            //if the turn is even, we make link from the currentLeft node in the left half
-            // of the list to the currRight in the right half of the list 
-            if(turn%2==0){
-                nextLeft=currLeft.next; //storing the next node, before changing the links
-                currLeft.next=currRight; //move to the next node in the left half
-                currLeft=nextLeft;
-                turn++; //this indicates which side turn it is to change the link
+            ListNode lnext=slow.next;
+            ListNode rnext=rhead.next;
+            
+            slow.next=rhead;
+            slow=lnext;
+            
+            if(slow==null){
+                break;
             }
             
-            //and if the trn is odd, we make link from the currentRight node in the right
-            //half of the list to the currLeft in the left half of the list
-            else{
-                nextRight=currRight.next;//storing the next node, before changing the links
-                currRight.next=currLeft; //move to the next node in the right half
-                currRight=nextRight;
-                turn++;
-            }
+            rhead.next=slow;
+            rhead=rnext;
         }
     }
     
-    public ListNode reverse(ListNode node){
+    private ListNode reverse(ListNode node){
         
         ListNode curr=node, prev=null, next=null;
         
@@ -69,6 +55,7 @@ class Solution {
             prev=curr;
             curr=next;
         }
+        
         return prev;
     }
 }
