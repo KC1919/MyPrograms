42747697f8e3e765444f6ac27979bf7297fedf04
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-21 10:39:48 +0530

    Time: 7 ms (13.80%), Space: 47.7 MB (7.65%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -15,16 +15,28 @@ public class Solution {
         if(head==null || head.next==null){
             return false;
         }
-        ListNode slow=head, fast=head.next;
+//         ListNode slow=head, fast=head.next;
         
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-            if(fast==slow){
+//         while(fast!=null && fast.next!=null){
+//             fast=fast.next.next;
+//             slow=slow.next;
+//             if(fast==slow){
+//                 return true;
+//             }
+//         }
+        
+        HashSet<ListNode>hm=new HashSet<>();
+        
+        ListNode node=head;
+        
+        while(node!=null){
+            
+            if(hm.contains(node)){
                 return true;
             }
+            hm.add(node);
+            node=node.next;
         }
-        
         return false;
     }
 }
