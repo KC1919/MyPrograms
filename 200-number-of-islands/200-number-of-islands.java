b46d96622029756171a1373254b8f04520117a09
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-22 10:25:55 +0530

    Time: 7 ms (33.71%), Space: 57.4 MB (47.50%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,40 +1,35 @@
 class Solution {
-    public int numIslands(char[][] a) {
+    public int numIslands(char[][] grid) {
         
-        int n=a.length;
-        int m=a[0].length;
+        int n=grid.length;
+        int m=grid[0].length;
         
         boolean visited[][]=new boolean[n][m];
-        int dir[][]={{0,-1},{0,1},{1,0},{-1,0}};
-        int count[]=new int[1];
-        int c=0;
+        int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
+        int count=0;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visited[i][j]==false && a[i][j]=='1'){
-                    count[0]=0;
-                    components(a,i,j,n,m,visited,count,dir);
-                    if(count[0]>0)
-                        c++;
+                if(visited[i][j]==false && grid[i][j]=='1'){
+                    count++;
+                    solve(grid,i,j,n,m,visited,dir);
                 }
             }
         }
-        return c;
+        return count;
     }
     
-    public void components(char a[][], int i, int j, int n, int m, boolean visited[][], int count[], int dir[][]){
-        
+    private void solve(char a[][], int i, int j, int n, int m, boolean visited[][], int dir[][]){
         visited[i][j]=true;
-        count[0]++;
         
         for(int k=0;k<dir.length;k++){
-            
             int row=dir[k][0]+i;
             int col=dir[k][1]+j;
             
-            if(row>=0 && row<n && col>=0 && col<m && visited[row][col]==false && a[row][col]=='1'){
-                components(a,row,col,n,m,visited,count,dir);
+            if(row>=0 && row<n && col>=0 && col<m && visited[row][col]==false && a[i][j]=='1'){
+                solve(a,row,col,n,m,visited,dir);
             }
         }
+        
     }
 }
