65f8fc020391c750d96887255f8ccdbcd22af56e
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-23 15:48:12 +0530

    Time: 16 ms (62.54%), Space: 42.2 MB (92.57%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        ArrayList<TreeNode>list=new ArrayList<>();
+        
+        nodeRootPath(root,list,target);
+        
+        List<Integer>klist=new ArrayList<>();
+        
+        for(int i=0;i<list.size();i++){
+            List<Integer>temp=new ArrayList<>();
+            
+            TreeNode block=i>0?list.get(i-1):null;
+            getNodes(list.get(i),temp,k-i,block);
+            
+            if(temp.size()>0){
+                klist.addAll(temp);
+            }
+        }
+        
+        return klist;
+    }
+    
+    private boolean nodeRootPath(TreeNode node, ArrayList<TreeNode>list, TreeNode target){
+        
+        if(node==null){
+            return false;
+        }
+        
+        if(node==target){
+            list.add(node);
+            return true;
+        }
+        
+        boolean lres=nodeRootPath(node.left,list,target);
+        
+        if(lres){
+            list.add(node);
+            return true;
+        }
+        boolean rres=nodeRootPath(node.right,list,target);
+        
+        if(rres){
+            list.add(node);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private void getNodes(TreeNode node, List<Integer>list, int level,TreeNode block){
+        
+        if(node==null || node==block){
+            return;
+        }
+        
+        if(level==0){
+            list.add(node.val);
+        }
+        
+        getNodes(node.left,list,level-1,block);
+        getNodes(node.right,list,level-1,block);
+    }
+}
