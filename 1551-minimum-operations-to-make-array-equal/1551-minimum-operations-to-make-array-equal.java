85d8ad83205ce81728ce8689ca963c7ce2caded9
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-07-24 11:56:23 +0530

    Time: 10 ms (5.06%), Space: 44.7 MB (8.16%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -9,16 +9,16 @@ class Solution {
         
         int min=Integer.MAX_VALUE;
         
-        if(n%2==0){
-            int res1=minSteps(a,n/2);
-            int res2=minSteps(a,n/2-1);
-            min=Math.min(res1,res2);
-        }
+//         if(n%2==0){
+//             int res1=minSteps(a,n/2);
+//             int res2=minSteps(a,n/2-1);
+//             min=Math.min(res1,res2);
+//         }
         
-        else{
+//         else{
             int res1=minSteps(a,n/2);
             min=Math.min(res1,min);
-        }
+        // }
         
         return min;
     }
@@ -27,11 +27,11 @@ class Solution {
         
         int sum=0;
         for(int i=0;i<a.length;i++){
-            
             if(i!=target){
                 sum+=Math.abs(a[target]-a[i]);
             }
         }
+        
         return sum/2;
     }
 }
