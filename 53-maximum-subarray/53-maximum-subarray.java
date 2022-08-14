9ea45598af0c5b49bfc31532ad1f12851695bc38
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-15 02:03:49 +0530

    Time: 2 ms (74.13%), Space: 73 MB (77.14%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int maxSubArray(int[] a) {
-        
-        //Kadane's algorithm
-        int max=-10001;
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
         int sum=0;
         
-        for(int i=0;i<a.length;i++){
-            sum+=a[i];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            
             max=Math.max(sum,max);
             
-            if(sum<0)
+            if(sum<0){
                 sum=0;
+            }
         }
+        
         return max;
     }
 }
