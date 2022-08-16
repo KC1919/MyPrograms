309395f10dd26a503435d936139cd4e55d23a8fc
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-16 14:42:13 +0530

    Time: 1 ms (43.73%), Space: 44.8 MB (83.56%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -16,78 +16,46 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         
-        HashMap<Node,Integer>hm=new HashMap<>();
-        HashMap<Integer,Node>nm=new HashMap<>();
-        
-        int count=0;
+        HashMap<Node, Node>hm=new HashMap<>();
         
         Node n=head;
         
-        while(n!=null){
-            hm.put(n,count);
-            n=n.next;
-            count++;
-        }
+        Node nhead=null, prev=null;
         
+        while(n!=null){
+            Node nn=new Node(n.val);
+            
+            if(nhead==null){
+                nhead=nn;
+            }
+            
+            if(prev!=null){
+                prev.next=nn;
+            }
+            
+            prev=nn;
+            hm.put(n,nn);
+            n=n.next;
+        }
         
         n=head;
         
-        Node prev=null;
-        
         while(n!=null){
             
-            //curr node
             Node curr=n;
+            Node copy=hm.get(curr);
             
-            //curr random node
-            Node crand=n.random;
-            
-            //curr node position
-            int cnpos=hm.get(curr);
-            
-            //new node
-            Node nn;
-            
-            if(nm.containsKey(cnpos))
-                nn=nm.get(cnpos);
-            
-            else
-                nn=new Node(curr.val);
-            
-            if(crand==null)
-                nn.random=null;
-            
-            else{
-                
-                //curr random node position
-                int crpos=hm.get(crand);
-                
-                //if curr rand node pos is same as curr node pos
-                //that means the random pointer of curr node points to itself
-                if(cnpos==crpos)
-                    nn.random=nn;
-                
-                //check if random node is present in the new list of nodes
-                else if(nm.containsKey(crpos))
-                    nn.random=nm.get(crpos);
-
-                //if not present create new random node of the curr new node
-                //and put it int the hasmap of new nodes after attaching the new random
-                //to the currr new node
-                else{ 
-                    Node nrn=new Node(crand.val);
-                    nn.random=nrn;
-                    nm.put(crpos,nrn);
-                }
+            if(curr.random==null){
+                copy.random=null;
             }
             
-            if(prev!=null)
-                prev.next=nn;
+            else{
+                copy.random=hm.get(curr.random);
+            }
             
-            prev=nn;
-            nm.put(cnpos,nn);
             n=n.next;
         }
-        return nm.get(0);
+        
+        return nhead;
     }
 }
