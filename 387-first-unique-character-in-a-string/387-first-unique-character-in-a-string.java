b7bfa7c6b1513d9a3dd4953d35e789812c78ab2c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-16 19:36:45 +0530

    Time: 62 ms (21.50%), Space: 54 MB (39.16%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,52 +1,18 @@
 class Solution {
     public int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
         
-        int[] count = new int[26];
-		
-		Queue<Integer>que=new LinkedList<>();
-		
-		for(int i=0;i<s.length();i++)
-		{
-		    char ch=s.charAt(i);
-		    int index = ch - 'a';
-            
-		    if(count[index]==0){
-		        count[index]++;
-		        que.add(i);
-		    }
-		    
-		    else
-		    {
-		        count[index]++;
-	
-		        if(que.size()>0)
-                {
-		            if(s.charAt(que.peek())==ch){
-		                que.remove();
-                        count[index]++;
-                    }
-	
-                    else if(count[s.charAt(que.peek())-'a']>1)
-		            {
-                        int idx=s.charAt(que.peek())-'a';
-		                count[idx]--;
-		                que.remove();
-		            }
-		        }
-		    }
-		}
-        
-        while(que.size()>0 && count[s.charAt(que.peek())-'a']>1)
-        {
-		    int idx=s.charAt(que.peek())-'a';
-		    count[idx]--;
-		    que.remove();
-		}
-        
-        if(que.size()==0)
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1) 
+                return i;
+        }
         return -1;
-        
-        else
-		return (count[s.charAt(que.peek())-'a']>1)?-1:que.peek();
     }
 }
