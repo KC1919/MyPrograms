b5de47bc71763a8c250fe59b86f0548a1cfc9e1f
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-16 12:58:22 +0530

    Time: 1 ms (63.26%), Space: 46.5 MB (5.47%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -9,88 +9,73 @@
  * }
  */
 class Solution {
+    
+    ListNode oh=null, ot=null;
+    ListNode th=null, tt=null;
+    
     public ListNode reverseKGroup(ListNode head, int k) {
         
-        ListNode start=head, end=head, fhead=null, frhead=null;
+        ListNode next=null, node=head;
         
-        while(true){
+        int size=length(head);
+        
+        while(size>=k){
             
-            //if start node or end node reaches null, means the list has been processed
-            //so we return the head of the new list formed
-            if(start==null || end==null)
-                return frhead;
+            int count=k;
             
-            //take a variable c, which holds the group count
-            int c=k;
+            size-=k;
             
-            //next variable stores the start node of the next group
-            ListNode next=null;
-            
-            //we find the end node of the current group
-            while(c>1 && end.next!=null){
-                end=end.next;
-                c--;
+            while(count-->0){
+                next=node.next;
+                node.next=null;
+                addFirst(node);
+                node=next;
             }
             
-            //if the end node was not reached of a group, that means the remaining nodes
-            //could not form a group
-            
-            if(c>1){
-                
-                //so we link the head of the remaining node to the last
-                //end node of the last group
-                fhead.next=start;
-                return frhead; ///and return the head of the new list
+            if(oh==null){
+                oh=th;
+                ot=tt;
             }
             
-            //get the head of the next group
-            next=end.next;
-            
-            //detach the last node of curretn group from the first node of next group
-            end.next=null;
-            
-            //then reverse the group, from the start node to the end node
-            //of the current group
-            ListNode rhead=reverse(start,end);
-            
-            //if it is the first group, so we assign the firstHead(fhead) to the 
-            //original head of the list, from where the linking of the next
-            //group will start
-            if(fhead==null){
-                fhead=start;
-                frhead=rhead;  //assign the first reverseHead that will be the head
-                               //node of the final list that has to be returned
-            }
-            
-            //if it is not the first reversed group 
             else{
-                
-                //then we link the firstHead node of previous group to the 
-                //returned reverseHeadof the current group
-                fhead.next=rhead;
-                fhead=start;   //and make the start of the current group as the firsthead
-            }                  //of the current group that will be used to link to the 
-                               //head of the next group
+                ot.next=th;
+                ot=tt;
+            }
             
-            start=next;  //assigning the start to the head node of the next group
-            end=next;    //and end also to the head of the next group
+            th=null;
+            tt=null;
+        }
+        
+        
+        if(size>0){
+            ot.next=next;
+        }
+        
+        return oh;
+    }
+    
+    public void addFirst(ListNode node){
+        
+        if(th==null){
+            th=node;
+            tt=node;
+        }
+        
+        else{
+            node.next=th;
+            th=node;
         }
     }
     
-    //reversing the nodes from start node till end node
-    public ListNode reverse(ListNode node, ListNode end){
+    public int length(ListNode node){
         
-        ListNode curr=node, prev=null, next=null;
+        int count=0;
         
-        while(curr!=end){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+        while(node!=null){
+            node=node.next;
+            count++;
         }
         
-        curr.next=prev;
-        prev=curr;
-        return prev;
+        return count;
     }
 }
