aff708ff21bfda48b61eda30a1e9f3a06efb756b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-19 22:10:36 +0530

    Time: 4 ms (79.55%), Space: 44.8 MB (81.40%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -2,18 +2,17 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         
         boolean col[]=new boolean[n];
-        boolean row[]=new boolean[n];
         boolean ldiag[]=new boolean[2*n-1];
         boolean rdiag[]=new boolean[2*n-1];
         
         List<List<String>>res=new ArrayList<>();
         
-        placeQueen(n,0,0,col,row,ldiag,rdiag,res,new ArrayList<>());
+        placeQueen(n,0,0,col,ldiag,rdiag,res,new ArrayList<>());
         
         return res;
     }
     
-    private void placeQueen(int n, int placed, int r, boolean col[], boolean row[], boolean ldiag[], boolean rdiag[], List<List<String>>res, List<Integer>list){
+    private void placeQueen(int n, int placed, int r, boolean col[], boolean ldiag[], boolean rdiag[], List<List<String>>res, List<Integer>list){
         
         if(placed==n){
             
@@ -47,7 +46,7 @@ class Solution {
                 rdiag[r-j+n-1]=true;
                 list.add(j);
                 
-                placeQueen(n,placed+1,r+1,col,row,ldiag,rdiag,res,list);
+                placeQueen(n,placed+1,r+1,col,ldiag,rdiag,res,list);
                 
                 col[j]=false;
                 ldiag[r+j]=false;
