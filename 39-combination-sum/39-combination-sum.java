bfe254d97e2847f2f851ae65e8a9d1a9d5a49663
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-23 23:42:02 +0530

    Time: 4 ms (86.70%), Space: 44.8 MB (82.98%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,32 +1,26 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] a, int target) {
         
         List<List<Integer>>res=new ArrayList<>();
-        
-        combinations(candidates,0,0,target,new ArrayList<>(),res);
+        combi(a,0,new ArrayList<>(),res,target);
         return res;
     }
     
-    public void combinations(int a[], int idx, int sum, int target, List<Integer>list, List<List<Integer>>res)
-    {
-        if(idx==a.length)
-            return;
+    private void combi(int a[], int idx, List<Integer>list, List<List<Integer>>res, int target){
         
-        if(sum==target){
-            res.add(new ArrayList<>(list));
+        if(idx==a.length || target==0){
+            
+            if(target==0){
+                res.add(new ArrayList<>(list));
+            }
+            
             return;
         }
         
-        if(sum+a[idx]<=target){
-            list.add(a[idx]);
-            combinations(a,idx,sum+a[idx],target,list,res);
-            list.remove(list.size()-1);
-        }
-        
-        for(int i=idx+1;i<a.length;i++){
-            if(sum+a[i]<=target){
+        for(int i=idx;i<a.length;i++){
+            if(a[i]<=target){
                 list.add(a[i]);
-                combinations(a,i,sum+a[i],target,list,res);
+                combi(a,i,list,res,target-a[i]);
                 list.remove(list.size()-1);
             }
         }
