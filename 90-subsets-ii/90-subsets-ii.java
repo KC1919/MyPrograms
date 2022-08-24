e95064cd2438ae193b80663409c0639488441f98
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-24 15:33:13 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (97.11%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,48 +1,33 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         
-        // HashMap<Integer,Integer>hm=new HashMap<>();
-        
-        // for(int i=0;i<nums.length;i++)
-        //     hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        
         Arrays.sort(nums);
-        
         List<List<Integer>>res=new ArrayList<>();
+        List<Integer>list=new ArrayList<>();
         
-        subsets(nums,0,new ArrayList<>(),res);
+        subsetDup(nums,0,new int[nums.length],list,res);
         return res;
     }
     
-    public void subsets(int a[], int idx, List<Integer>list, List<List<Integer>>res)
-    {
+    private void subsetDup(int a[], int idx, int slots[], List<Integer>list, List<List<Integer>>res){
         
         if(idx==a.length){
             res.add(new ArrayList<>(list));
             return;
         }
-        subsets(a,idx+1,list,res);
         
-        // for(int ele:hm.keySet()){
-            
-            // if(hm.get(ele)>0){
-                
-        if(idx>=0){
-            
-            if(idx>0 && a[idx]==a[idx-1]){
-                //nothing to do;
-            }
-            
-            else{
-                list.add(a[idx]);
-                a[idx]=a[idx]-20;
-                // hm.put(ele,hm.get(ele)-1);
-                subsets(a,idx+1,list,res);
-                // hm.put(ele,hm.get(ele)+1);
-                list.remove(list.size()-1);
-                a[idx]+=20;
-            }
+        if(idx>0 && a[idx]==a[idx-1] && slots[idx-1]==0){
+            subsetDup(a,idx+1,slots,list,res);
         }
         
+        else{
+            slots[idx]=1;
+            list.add(a[idx]);
+            subsetDup(a,idx+1,slots,list,res);
+            list.remove(list.size()-1);
+            slots[idx]=0;
+            
+            subsetDup(a,idx+1,slots,list,res);
+        }
     }
 }
