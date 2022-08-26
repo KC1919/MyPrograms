aa154cfa7b4e69abfb5b4d256d36e1545ecddcfa
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-26 12:46:23 +0530

    Time: 15 ms (95.31%), Space: 75.9 MB (82.62%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,14 +1,9 @@
 class Solution {
     public int largestRectangleArea(int[] a) {
         
-        if(a.length==1)
-            return a[0];
-        
-        int max=0;
         int n=a.length;
         
         int ls[]=new int[n];
-        int rs[]=new int[n];
         
         ls[0]=-1;
         
@@ -16,24 +11,37 @@ class Solution {
             
             int j=i-1;
             
-            int area=-1;
             while(j!=-1 && a[i]<=a[j]){
-                rs[j]=i;
                 j=ls[j];
             }
             
             ls[i]=j;
         }
         
+        int rs[]=new int[n];
+        
+        rs[n-1]=n;
+        
+        for(int i=n-2;i>=0;i--){
+            
+            int j=i+1;
+            
+            while(j!=n && a[i]<=a[j]){
+                j=rs[j];
+            }
+            
+            rs[i]=j;
+        }
+        
+        int max=0;
+        
         for(int i=0;i<n;i++){
             
-            int rsmall=rs[i]==0?n:rs[i];
+            int wid=(rs[i]-ls[i])-1;
             
-            int span=(rsmall-ls[i])-1;
-            int area=a[i]*span;
-            
-            max=Math.max(area,max);
+            max=Math.max(max,a[i]*wid);
         }
+        
         return max;
     }
 }
