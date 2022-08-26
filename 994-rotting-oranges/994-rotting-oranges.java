6154f8f9a8263589447aa22f07b06374645cce90
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-26 16:37:58 +0530

    Time: 4 ms (46.26%), Space: 42.5 MB (79.24%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,66 +1,71 @@
 class Solution {
+    
+     class Pair
+     {
+        int i;
+        int j;
+        
+        Pair(int i, int j){
+            this.i=i;
+            this.j=j;
+        }
+     }
     public int orangesRotting(int[][] a) {
+        Queue<Pair>que=new LinkedList<>();
         
-        Queue<Integer>que=new LinkedList<>();
-        
+        int cone=0;
         int n=a.length;
         int m=a[0].length;
-        
-        int oc=0;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(a[i][j]==2){
-                    int idx=(i*m) + j;
-                    
-                    // System.out.println(idx+" ");
-                    que.add(idx);
+                    que.add(new Pair(i,j));
                 }
                 
-                else if(a[i][j]==1)
-                    oc++;
+                else if(a[i][j]==1){
+                    cone++;
+                }
             }
         }
         
-        if(oc==0)
-            return 0;
-        
+        int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
         int time=0;
         
-        int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
-        
         while(que.size()>0){
-            
             int size=que.size();
             int flag=0;
             
             while(size-->0){
-                flag=1;
-                int ridx=que.remove();
-                int r=ridx/m;
-                int c=ridx%m;
-
-                for(int k=0;k<dir.length;k++){
-
-                    int row=dir[k][0]+r;
-                    int col=dir[k][1]+c;
-
-                    if(row>=0 && row<n && col>=0 && col<m && a[row][col]==1){
+                
+                Pair rem=que.remove();
+                
+                for(int k=0;k<4;k++){
+                    int row=dir[k][0]+rem.i;
+                    int col=dir[k][1]+rem.j;
+                    
+                    if(row>=0 && col>=0 && row<n && col<m && a[row][col]==1){
                         a[row][col]=2;
-
-                        int idx=(row*m)+col;
-                        que.add(idx);
-                        oc--;
+                        que.add(new Pair(row,col));
+                        flag=1;
+                        cone--;
                     }
                 }
             }
-            if(flag==1)
+            
+            if(flag==1){
                 time++;
+            }
+            
+            if(cone==0){
+                break;
+            }
         }
         
-        if(oc==0)
-            return time-1;
+        if(cone!=0){
+            return -1;
+        }
         
-        return -1;
+        return time;
     }
 }
