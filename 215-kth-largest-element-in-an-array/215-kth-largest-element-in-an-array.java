aedc48bdc7d26270603e0ceb17d4cdd743d50259
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-29 16:19:21 +0530

    Time: 27 ms (58.81%), Space: 74.3 MB (26.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,23 +1,47 @@
 class Solution {
     public int findKthLargest(int[] a, int k) {
         
-        PriorityQueue<Integer>pq=new PriorityQueue<>();
-        
-        for(int i=0;i<k;i++)
-        {
-            pq.add(a[i]);
-        }
-        
-        for(int i=k;i<a.length;i++)
-        {
-            if(a[i]>pq.peek())
-            {
-                 pq.add(a[i]);
-                 pq.remove();
-            }
-               
-        }
-        
-        return pq.size()>0?pq.remove():-1;
+        return quickSelect(a,a.length-k,0,a.length-1);
     }
+    
+    private int quickSelect(int a[], int k, int lo, int hi){
+        
+        int pivot=a[hi];
+        int pi=partition(a,lo,hi,pivot);
+        
+        if(pi==k){
+            return a[k];
+        }
+        
+        else if(k<pi){
+            return quickSelect(a,k,lo,pi-1);
+        }
+        
+        else{
+            return quickSelect(a,k,pi+1,hi);
+        }
+    }
+    
+    private int partition(int a[], int lo, int hi, int pivot){
+        
+        int i=lo, j=lo;
+        
+        while(i<=hi){
+            
+            if(a[i]>pivot){
+                i++;
+            }
+            
+            else{
+                int t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+                
+                i++;
+                j++;
+            }
+        }
+        
+        return j-1;
+    } 
 }
