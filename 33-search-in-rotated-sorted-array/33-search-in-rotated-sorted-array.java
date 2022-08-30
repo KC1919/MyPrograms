0f2006e55ce9c22a2b5f94b58817e7d97a4da92d
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-30 18:18:14 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (14.16%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,47 +1,40 @@
 class Solution {
     public int search(int[] a, int target) {
         
-       int l=0, h=a.length-1;
+        int lo=0, hi=a.length-1;
         
-        while(l<=h)
-        {
-            int mid=(l+h)/2;
+        int mid=-1;
+        
+        while(lo<=hi){
+            mid=(lo+hi)/2;
             
-            //if the target is found, we return its index
-            if(a[mid]==target)
+            if(target==a[mid]){
                 return mid;
+            }
             
-            //check if the left part is sorted
-            else if(a[l]<=a[mid]){
+            else if(a[mid]>=a[lo]){
                 
-                //if the left part is sorted, then we check the possibility of target to
-                //lie in that sorted part
-                if(target>=a[l] && target<a[mid]){
-                    
-                    //if yes, then we move our high pointer to mid-1
-                    h=mid-1;
+                if(target<a[mid] && target>=a[lo]){
+                    hi=mid-1;
                 }
                 
-                //if there is no possibility, then we move our low pointer to mid+1, to
-                //search in the right part
-                else
-                    l=mid+1;
+                else{
+                    lo=mid+1;
+                }
             }
             
-            //if the left part is not sorted, means right part is sorted,
-            //because any one part will always be sorted
             else{
                 
-                //so we check if the target can lie in the right sorted part,
-                if(target>a[mid] && target<=a[h])
-                    l=mid+1;
+                if(target>a[mid] && target<=a[hi]){
+                    lo=mid+1;
+                }
                 
-                //if there is no possibility to lie in the right half,we move our high
-                //pointer to mid-1, to search in the left half
-                else
-                    h=mid-1;
+                else{
+                    hi=mid-1;
+                }
             }
         }
+        
         return -1;
     }
 }
