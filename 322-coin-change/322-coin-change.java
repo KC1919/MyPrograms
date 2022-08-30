973844d70220f2249a1a12e0641633898e327caf
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-30 15:52:30 +0530

    Time: 40 ms (36.18%), Space: 47.4 MB (26.16%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,23 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         
-        int n=coins.length;
         Integer dp[]=new Integer[amount+1];
-        
-        int res=minCoins(coins,0,amount,dp);
-        
-        return res==Integer.MAX_VALUE?-1:res-1;
+        minCoins(coins,amount,dp);
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
     }
     
-    private int minCoins(int a[], int idx, int target, Integer dp[]){
+    private int minCoins(int a[], int target, Integer dp[]){
         
-        if(idx==a.length || target==0){
-            
-            if(target==0){
-                return 1;
-            }
-            
-            return Integer.MAX_VALUE;
+        if(target==0){
+            return dp[target]=0;
         }
         
         if(dp[target]!=null){
@@ -25,15 +17,9 @@ class Solution {
         }
         
         int min=Integer.MAX_VALUE;
-        
-        if(target>=a[idx]){
-            int res=minCoins(a,idx,target-a[idx],dp);
-            min=Math.min(res,min);
-        }
-        
         for(int i=0;i<a.length;i++){
             if(target>=a[i]){
-                int res=minCoins(a,i,target-a[i],dp);
+                int res=minCoins(a,target-a[i],dp);
                 min=Math.min(res,min);
             }
         }
