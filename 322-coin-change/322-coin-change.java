677282441754902eb2b4331a7fd58d181f67408a
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-08-30 16:32:48 +0530

    Time: 34 ms (44.13%), Space: 44.8 MB (74.70%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -3,7 +3,9 @@ class Solution {
         
         if(amount==0)return 0;
         
-        Integer dp[]=new Integer[amount+1];
+        int dp[]=new int[amount+1];
+        
+        Arrays.fill(dp,Integer.MAX_VALUE);
         
         for(int j=0;j<=amount;j++){
             for(int coin:coins){
@@ -16,18 +18,12 @@ class Solution {
                 }
                 
                 else{
-                    if(dp[j-coin]!=null && dp[j-coin]>0){
-                        
-                        if(dp[j]==null){
-                            dp[j]=dp[j-coin]+1;
-                        }
-                        else{
-                            dp[j]=Math.min(dp[j],dp[j-coin]+1);
-                        }
+                    if(dp[j-coin]>0 && dp[j-coin]!=Integer.MAX_VALUE){
+                        dp[j]=Math.min(dp[j],dp[j-coin]+1);
                     }
                 }
             }
         }
-        return dp[amount]==null?-1:dp[amount];
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
     }
 }
