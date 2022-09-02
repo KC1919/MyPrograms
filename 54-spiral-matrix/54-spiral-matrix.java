6df49e3c54d248d7406829d423eb9a8178eb15a8
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-02 11:35:52 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (19.62%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,77 +1,52 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] a) {
-        List<Integer>list=new ArrayList<>();
         
         int n=a.length;
         int m=a[0].length;
         
-        //handling the case of matrix with either single column or single row.
-        if(n==1 || m==1)
-        {
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<m;j++)
-                    list.add(a[i][j]);
-            }
-            return list;
-        }
+        int minRow=0, maxRow=n-1;
+        int minCol=0, maxCol=m-1;
         
+        int tel=n*m;
+        int count=0;
         
+        List<Integer>list=new ArrayList<>();
         
-        int sc=0, ec=m-1;
-        int sr=0, er=n-1;
-        
-        //we do this repeatidely for every square boundary that makes up the spiral matrix
-        while(sc<=ec && sr<=er)
-        {
-            travSpiral(a,sc,ec,sr,er,n,m,list);
-            sc++;
-            sr++;
-            ec--;
-            er--;
-        }
-        return list;
-    }
-    
-    public void travSpiral(int a[][], int sc, int ec, int sr, int er, int n, int m, List<Integer>list)
-    {
-        //traversing the top row of the square
-        for(int j=sc;j<=ec;j++)
-        {
-            if(a[sr][j]!=-101){
-                list.add(a[sr][j]);
-                a[sr][j]=-101;
+        while(count<tel){
+            
+            // Upper Wall
+            for(int i=minRow, j=minCol;j<=maxCol && count<tel; j++){
+                list.add(a[i][j]);
+                count++;
             }
             
+            minRow++;
+            
+            // right wall
+            for(int i=minRow, j=maxCol; i<=maxRow && count<tel; i++){
+                list.add(a[i][j]);
+                count++;
+            }
+            
+            maxCol--;
+            
+            // bottom wall
+            for(int i=maxRow, j=maxCol; j>=minCol&& count<tel; j--){
+                list.add(a[i][j]);
+                count++;
+            }
+            
+            maxRow--;
+            
+            // left wall
+            for(int i=maxRow, j=minCol; i>=minRow && count<tel; i--){
+                list.add(a[i][j]);
+                count++;
+            }
+            
+            minCol++;
         }
         
-        //traversing the last column of the square, from below the last cell of top row
-        for(int i=sr+1;i<=er;i++)
-        {
-            if(a[i][ec]!=-101){
-                list.add(a[i][ec]);
-                a[i][ec]=-101;
-            }
-        }
-        
-        //traversing the last row of the square, from after the last cell of the right column
-        // of the square
-        for(int j=ec-1;j>=sc && sr!=er;j--)
-        {
-            if(a[er][j]!=-101){
-                list.add(a[er][j]);
-                a[er][j]=-101;
-            }
-        }
-        
-        //traversing the 1st column of the square starting from above the 1st cell of 
-        // bottom row of the square
-        for(int i=er-1;i>sr;i--)
-        {
-            if(a[i][sc]!=-101){
-                list.add(a[i][sc]);
-                a[i][sc]=-101;
-            }
-        }
+        return list;
     }
 }
