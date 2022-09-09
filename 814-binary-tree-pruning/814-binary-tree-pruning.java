802def6614a915d6fb4d6a1d67f4a9272b76239c
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-09 18:33:48 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (76.49%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode pruneTree(TreeNode node) {
+        
+        if(node!=null && node.left==null && node.right==null && node.val==0){
+            return null;
+        }
+        boolean res=prune(node);
+        
+        if(res==false && node.val==0)return null;
+        
+        return node;
+    }
+    
+    private boolean prune(TreeNode node){
+        
+        if(node==null){
+            return false;
+        }
+        
+        boolean lres=prune(node.left);
+        
+        if(lres==false){
+            node.left=null;
+        }
+        
+        boolean rres=prune(node.right);
+        
+        if(rres==false){
+            node.right=null;
+        }
+        
+        if(lres==false && rres==false && node.val==0)return false;
+        
+        return true;
+    }
+}
