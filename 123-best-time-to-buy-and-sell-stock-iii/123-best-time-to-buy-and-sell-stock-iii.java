2b27660ff3b36715da94185cc6eb5eaa245ffd43
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-11 02:56:35 +0530

    Time: 9 ms (56.02%), Space: 83.3 MB (39.50%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,51 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+        
+//         int n = prices.length;
+//         int cap = 2; // no of transactions allowed
+//         int buy = 1; // can buy or not
+//         Integer[][][] dp = new Integer[n][2][3];
+//         return maxProfit(0, buy, cap, prices, n, dp);
+//     }
+    
+//     private int maxProfit(int i, int buy, int cap, int[] prices, int n, Integer[][][] dp){
+        
+//         if(i == n || cap == 0) return 0;
+        
+//         if(dp[i][buy][cap] != null) return dp[i][buy][cap];
+//         // if can buy new stack else sell
+//         if(buy == 1){
+//             return dp[i][buy][cap] = Math.max(-prices[i] + maxProfit(i+1, 0, cap, prices, n, dp)
+//                              , 0 + maxProfit(i+1, 1, cap, prices, n, dp));                     
+//         }
+//         else{
+//             return dp[i][buy][cap] = Math.max(prices[i] + maxProfit(i+1, 1, cap-1, prices, n, dp)
+//                              , 0 + maxProfit(i+1, 0, cap, prices, n, dp));
+//         }
+//     }
+// }
+
+
 class Solution {
     public int maxProfit(int[] prices) {
+        if(prices.length == 0)
+            return 0;
         
-        int n = prices.length;
-        int cap = 2; // no of transactions allowed
-        int buy = 1; // can buy or not
-        Integer[][][] dp = new Integer[n][2][3];
-        return maxProfit(0, buy, cap, prices, n, dp);
-    }
-    
-    private int maxProfit(int i, int buy, int cap, int[] prices, int n, Integer[][][] dp){
+        int n = prices.length, max;
+        int[] prevLine = new int[n];
+        int[] dp = new int[n];
         
-        if(i == n || cap == 0) return 0;
-        
-        if(dp[i][buy][cap] != null) return dp[i][buy][cap];
-        // if can buy new stack else sell
-        if(buy == 1){
-            return dp[i][buy][cap] = Math.max(-prices[i] + maxProfit(i+1, 0, cap, prices, n, dp)
-                             , 0 + maxProfit(i+1, 1, cap, prices, n, dp));                     
+        for(int i = 1; i <= 2; i++){
+            dp = new int[n];
+			//max represents my maximum profit so far by making i-1 transactions
+            max = - prices[0];
+            for(int j = 1; j < n; j++){
+                dp[j] = Math.max(dp[j-1], max + prices[j]);
+                max = Math.max(max,  prevLine[j] - prices[j]);
+            }
+            prevLine = dp;
         }
-        else{
-            return dp[i][buy][cap] = Math.max(prices[i] + maxProfit(i+1, 1, cap-1, prices, n, dp)
-                             , 0 + maxProfit(i+1, 0, cap, prices, n, dp));
-        }
+        
+        return dp[n-1];
     }
 }
