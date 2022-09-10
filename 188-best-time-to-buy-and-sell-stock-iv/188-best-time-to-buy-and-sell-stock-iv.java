402b349790eaeb145ef6f194d3d26c2f385b8828
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-11 02:54:35 +0530

    Time: 6 ms (49.25%), Space: 46 MB (9.92%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -7,8 +7,9 @@
 //         findMax(a,0,k,'\0',0);
 //         return max;
 //     }
+
+        //RECURSION
     
-//     // Recursion
 //     private void findMax(int a[], int idx, int k, char t, int sum){
         
 //         if(idx==a.length || k==0){
@@ -37,27 +38,29 @@
 //     }
 // }
 
-
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        if(prices.length == 0)
-            return 0;
         
-        int n = prices.length, max;
-        int[] prevLine = new int[n];
-        int[] dp = new int[n];
+        int n = prices.length;
+        int cap = k; // no of transactions allowed
+        int buy = 1; // can buy or not
+        Integer[][][] dp = new Integer[n][2][k+1];
+        return maxProfit(0, buy, cap, prices, n, dp);
+    }
+    
+    private int maxProfit(int i, int buy, int cap, int[] prices, int n, Integer[][][] dp){
         
-        for(int i = 1; i <= k; i++){
-            dp = new int[n];
-			//max represents my maximum profit so far by making i-1 transactions
-            max = - prices[0];
-            for(int j = 1; j < n; j++){
-                dp[j] = Math.max(dp[j-1], max + prices[j]);
-                max = Math.max(max,  prevLine[j] - prices[j]);
-            }
-            prevLine = dp;
+        if(i == n || cap == 0) return 0;
+        
+        if(dp[i][buy][cap] != null) return dp[i][buy][cap];
+        // if can buy new stack else sell
+        if(buy == 1){
+            return dp[i][buy][cap] = Math.max(-prices[i] + maxProfit(i+1, 0, cap, prices, n, dp)
+                             , 0 + maxProfit(i+1, 1, cap, prices, n, dp));                     
         }
-        
-        return dp[n-1];
+        else{
+            return dp[i][buy][cap] = Math.max(prices[i] + maxProfit(i+1, 1, cap-1, prices, n, dp)
+                             , 0 + maxProfit(i+1, 0, cap, prices, n, dp));
+        }
     }
 }
