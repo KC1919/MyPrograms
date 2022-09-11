21da2b6416321c32903e485025a41e97234f11e1
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-11 17:07:03 +0530

    Time: 96 ms (33.67%), Space: 67.5 MB (79.67%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -10,6 +10,7 @@ class Solution {
             p[i][1]=speed[i];
         }
         
+        //sort the array in decreasing order of efficiency
         Arrays.sort(p,(a,b)->{
             return b[0]-a[0];
         });
@@ -19,17 +20,22 @@ class Solution {
         
         for(int i=0;i<n;i++){
             
+            //keep adding the speed to the PQ
             pq.add(p[i][1]);
-            sum+=p[i][1];
+            sum+=p[i][1];  //summing of the speeds
             
+            //while k members have not been reached we calculateour max performance
+            //since we have to calculate at most k
             if(pq.size()<k)
                 max=Math.max(p[i][0]*sum,max);
             
+            //if k members have been considered
             else if(pq.size()==k){
-                max=Math.max(p[i][0]*sum,max);
-                sum-=pq.remove();
-            }
-        }
+                max=Math.max(p[i][0]*sum,max); //we again calculate with all k members
+                sum-=pq.remove(); //then remove the member with lowest speed
+            }                     //becoz we need to maximize the performance and the
+        }                         //efficiency tends to decrease as we iterate so we
+                                  //tend to include member who has greater speed
         
         return (int) (max % 1000000007);
     }
