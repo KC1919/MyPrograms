7880ab0f3d6cb6efdfac9685848091cfa7207f1a
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-12 14:53:39 +0530

    Time: 4 ms (60.64%), Space: 43.6 MB (30.92%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        
+        Arrays.sort(tokens);
+        
+        int n=tokens.length;
+        int left=0, right=n-1;
+        
+        int score=0;
+        int max=0;
+        
+        while(left<=right){
+            
+            if(tokens[left]<=power){
+                power-=tokens[left];
+                score++;
+                left++;
+                max=Math.max(score,max);
+            }
+            
+            else if(score>0){
+                power+=tokens[right];
+                score--;
+                right--;
+            }
+            
+            else{
+                break;
+            }
+        }
+        
+        return max;
+    }
+}
