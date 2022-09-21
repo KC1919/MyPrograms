8c16d315a5df0751fe6bda09b8213be2208e54b6
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-22 00:00:49 +0530

    Time: 22 ms (5.47%), Space: 75.9 MB (6.11%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -16,18 +16,25 @@ class Solution {
             int val=queries[i][0];
             int idx=queries[i][1];
             
-            int newVal=nums[idx]+val;
-            
+            // if the value at ith index is already even, means it is included
+            // in the evenSum, hence we remove it from the evenSum because its
+            // value is going to be updated and we dont know whether it will
+            // become even or odd after updation
             if(nums[idx]%2==0){
                 evenSum-=nums[idx];
             }
             
+            //updating the value with the query's value
             nums[idx]+=val;
             
+            
+            //then checking if the value became even after updating, so we add
+            //it to our evenSum, else we do not add
             if(nums[idx]%2==0){
                 evenSum+=nums[idx];
             }
             
+            // wee store the evenSum after each query
             ans[i]=evenSum;
         }
         
