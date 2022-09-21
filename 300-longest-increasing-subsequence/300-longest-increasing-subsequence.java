e2f2bdc383765cc7bcf22893fa2c0bd2be458ac5
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-21 14:47:02 +0530

    Time: 11 ms (71.77%), Space: 45.2 MB (17.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,44 +1,46 @@
 class Solution {
     public int lengthOfLIS(int[] a) {
         
-        int n=a.length;
-        int dp[]=new int[n];
+        int dp[]=new int[a.length];
         
         dp[0]=a[0];
-        
+        int max=1;
         int len=1;
         
-        for(int i=1;i<n;i++){
+        for(int i=1;i<a.length;i++){
             
-            int pos=findPos(dp,0,len-1,a[i]);
+            int pos=findPos(dp,len-1,a[i]);
             
             if(pos==len){
                 len++;
             }
+            
+            max=Math.max(len,max);
             dp[pos]=a[i];
         }
-        return len;
+        
+        return max;
     }
     
-    private int findPos(int a[], int l, int h, int target){
+    private int findPos(int a[], int high, int target){
         
-        int mid=-1;
+        int l=0, h=high;
         
         while(l<=h){
-            mid=(l+h)/2;
+            int mid=(l+h)/2;
             
-            if(a[mid]==target)
-                return mid;
-            
-            else if(a[mid]<target){
+            if(a[mid]<target){
                 l=mid+1;
             }
             
-            else{
+            else if(a[mid]>target){
                 h=mid-1;
             }
+            
+            else{
+                return mid;
+            }
         }
-        
         return l;
     }
 }
