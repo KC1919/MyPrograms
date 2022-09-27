33613ebdb2cb57a85633f430c6c9fe911803d4e9
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-27 15:55:46 +0530

    Time: 22 ms (80.67%), Space: 54.5 MB (37.08%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,30 +1,71 @@
 class Solution {
-    public String pushDominoes(String S) {
-        char[] A = S.toCharArray();
-        int N = A.length;
-        int[] forces = new int[N];
-
-        // Populate forces going from left to right
-        int force = 0;
-        for (int i = 0; i < N; ++i) {
-            if (A[i] == 'R') force = N;
-            else if (A[i] == 'L') force = 0;
-            else force = Math.max(force - 1, 0);
-            forces[i] += force;
+    public String pushDominoes(String dominos) {
+        
+        char a[]=dominos.toCharArray();
+        
+        int left=0, right=0;
+        
+        StringBuilder sb=new StringBuilder();
+        
+        int flag=0;
+        
+        while(left<a.length){
+            
+            if(a[left]=='L'){
+                
+                int temp=left-1;
+                while(temp>=0 && a[temp]=='.'){
+                    a[temp]='L';
+                    temp--;
+                }
+            }
+            
+            else if(a[left]=='R'){
+                right=left+1;
+                while(right<a.length && a[right]=='.'){
+                    right++;
+                }
+                
+                if(right==a.length || a[right]==a[left]){
+                    int temp=left+1;
+                    while(temp<right && a[temp]=='.'){
+                        a[temp]='R';
+                        temp++;
+                    }
+                    left=right;
+                }
+                else{
+                    update(a,left,right);
+                    left=right+1;
+                }
+                right=left;
+                
+                continue;
+            }
+            
+            left++;
+            right=left;
         }
-
-        // Populate forces going from right to left
-        force = 0;
-        for (int i = N-1; i >= 0; --i) {
-            if (A[i] == 'L') force = N;
-            else if (A[i] == 'R') force = 0;
-            else force = Math.max(force - 1, 0);
-            forces[i] -= force;
+        
+        for(int i=0;i<a.length;i++){
+            sb.append(a[i]);
         }
-
-        StringBuilder ans = new StringBuilder();
-        for (int f: forces)
-            ans.append(f > 0 ? 'R' : f < 0 ? 'L' : '.');
-        return ans.toString();
+        
+        return sb.toString();
+    }
+    
+    private void update(char a[], int left, int right){
+        
+        int len=right-left-1;
+        
+        left++;
+        right--;
+        
+        while(left<right){
+            a[left]=a[left-1];
+            a[right]=a[right+1];
+            left++;
+            right--;
+        }
     }
 }
