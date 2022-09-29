3c529313990ea925a9eab8ea134ad6882f3cb4fc
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-09-29 12:24:19 +0530

    Time: 18 ms (38.32%), Space: 62.7 MB (39.57%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -5,58 +5,40 @@ class Solution {
         int idx=findPos(a,x);
         
         int left=idx-1, right=idx;
+        int count=0;
         
-        if(left<0){
-            for(int i=left+1;i<k;i++){
-                list.add(a[i]);
-            }
-        }
-        
-        else if(right==a.length){
-            for(int i=right-1;i>=(right-k);i--){
-                list.add(a[i]);
-            }
-        }
-        
-        else{
-            
-            int count=0;
-            
-            while(left>=0 && right<a.length && count<k){
+        while(left>=0 && right<a.length && count<k){
                 
-                int ldiff=Math.abs(x-a[left]);
-                int rdiff=Math.abs(x-a[right]);
+            int ldiff=Math.abs(x-a[left]);
+            int rdiff=Math.abs(x-a[right]);
                 
-                if(ldiff<=rdiff){
-                    list.add(a[left]);
-                    left--;
-                    count++;
-                }
-                
-                else if(rdiff<ldiff){
-                    list.add(a[right]);
-                    right++;
-                    count++;
-                }
-            }
-            
-            while(left>=0 && count<k){
+            if(ldiff<=rdiff){
                 list.add(a[left]);
                 left--;
-                count++;
             }
-            
-            while(right<a.length && count<k){
+                
+            else if(rdiff<ldiff){
                 list.add(a[right]);
                 right++;
-                count++;
             }
+            count++;
+        }
+            
+        while(left>=0 && count<k){
+            list.add(a[left]);
+            left--;
+            count++;
+        }
+            
+        while(right<a.length && count<k){
+            list.add(a[right]);
+            right++;
+            count++;
         }
         
         Collections.sort(list);
         
         return list;
-        
     }
     
     private int findPos(int a[], int target){
