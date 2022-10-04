9da8f88c283d9650fe6d353bb11c722ddba92586
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-10-04 14:45:31 +0530

    Time: 19 ms (82.28%), Space: 42.7 MB (46.43%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,50 +1,35 @@
 class Solution {
     public int numRollsToTarget(int n, int k, int target) {
-        Long dp[][]=new Long[n][target+1];
-        return (int)countways(k,n,0,target,dp);
+        
+        Integer dp[][]=new Integer[n+1][target+1];
+        return rollDice(n,k,target,dp);
     }
     
-    private static long countways(int m, int n, int idx, int target, Long dp[][]){
+    private int rollDice(int n, int k, int target, Integer dp[][]){
         
-        //if all the dices have been thrown
-        if(idx==n){
-            
-            //and the target has been made, we return 1 to signify success
+        if(n==0){
             if(target==0){
-                return 1;
+                return dp[n][target]=1;
             }
+            
             return 0;
         }
         
-        //if the answer to the current state is already stored so we return that
-        if(dp[idx][target]!=null){
-            return dp[idx][target];
+        if(dp[n][target]!=null){
+            return dp[n][target];
         }
         
-        //take a count variable to store the total number of ways
-        long count=0;
+        int myRes=0;
         
-        //check if the dice number is in range
-        if(idx<n){
+        for(int i=1;i<=k;i++){
             
-            //every dice has 1 to k options to chose from
-            for(int i=1;i<=m;i++){
-                
-                //check if the current chosen number is less that or greater than the target
-                if(i<=target){
-                    
-                    //make call to the next dice with the remaining target sum
-                    long res=countways(m,n,idx+1,target-i,dp);
-                    
-                    //adding to the count the returned number of ways
-                    if(res>0){
-                        count+=res;
-                    }
-                }
+            if(target-i>=0){
+                int rres=rollDice(n-1,k,target-i,dp);
+                myRes+=rres;
+                myRes%=1000000007;
             }
         }
         
-        //storing the answer in the dp table
-        return dp[idx][target]=count%1000000007;
+        return dp[n][target]=myRes%1000000007;
     }
 }
