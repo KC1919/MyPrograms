4b382ceea3d85e6dfae89bf48ae4283053aa0a4a
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-10-08 15:47:36 +0530

    Time: 109 ms (37.03%), Space: 49.4 MB (10.33%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -1,74 +1,64 @@
 class Solution {
-    public int threeSumClosest(int[] a, int target) {
-        Arrays.sort(a);
+    public int threeSumClosest(int[] nums, int target) {
         
-        int max=Integer.MIN_VALUE, close=Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        int sum=0;
+        int n=nums.length;
+        int diff=Integer.MAX_VALUE;
         
-        //have to go till array length-2 because we have to form triplets
-        for(int i=0;i<a.length-2;i++)
-        {
-            if(i>0 && a[i]==a[i-1])
+        for(int i=0;i<nums.length;i++){
+            
+            if(i>0 && nums[i]==nums[i-1])
                 continue;
             
-            int comp=target-a[i];
+            int comp=target-nums[i];
             
-                //getting target or closest to the target two sum
-                int res=twoSum(a,i+1,a.length-1,comp);
-                int fres=res+a[i];  //adding current element to make three sum(triplet)
-                
-                if(fres==target)  //if the triplet is equal to the target we return
-                    return fres;
-                
-                else   //else we store the triplet sum that is closest to the target
-                {
-                    if(Math.abs(target-fres)<close)  //checking closeness by finding the absolute difference between the triplet sum and target
-                    {
-    	                close=Math.abs(target-fres);
-    	                max=fres;   //storing the triplet sum
-	                }
-	                
-	                //if two triplet sum occurrs with equal closeness to the target, then we take the sum which is gretaer in value
-    	            else if(close==Math.abs(target-fres) && fres>max)
-    	                max=fres;
+            if(i+2<n){
+                int res=twoSum(nums,i+1,comp);
+                int d=Math.abs(target-(res+nums[i]));
+
+                if(d<diff){
+                    sum=nums[i]+res;
+                    diff=d;
                 }
+            }
         }
-        return max;
+        
+        return sum;
     }
     
-    public int twoSum(int a[], int start, int end, int target)
-	{
-	    int diff=Integer.MAX_VALUE;
-	    int max=Integer.MIN_VALUE;
-	    
-	    while(start<end)
-	    {
-	        int sum=a[start]+a[end];
-	        
-	        if(sum==target)
-	            return sum;
-	        
-	        //cheking the closeness of the sum formed to the target, if we get sum that is more closer to the target we store that sum and diff
-	        if(Math.abs(target-sum)<=diff)
-	        {
-	            if(Math.abs(target-sum)<diff){
-	                diff=Math.abs(target-sum);
-	                max=sum;
-	            }
-	            
-	            //is more than two sum occurrs with equal closeness to the target we take the one which is greter in value
-	            else if(diff==Math.abs(target-sum) && sum>max)
-	                max=sum;
-	        }
-	        
-	        //normal index moving to get closer to the sum
-	        
-	        if(sum<target)
-	            start++;
-	        
-	        
-	        else if(sum>target)
-	            end--;
-	    }
-	    return max;
+    private int twoSum(int a[], int li, int target){
+        
+        int l=li, r=a.length-1;
+        int diff=Integer.MAX_VALUE;
+        int sum=0;
+        
+        while(l<r){
+            
+            int d=Math.abs(target-(a[l]+a[r]));
+            
+            if(a[l]+a[r]==target){
+                return target;
+            }
+            
+            else if(a[l]+a[r]>target){
+                if(d<diff){
+                    sum=a[l]+a[r];
+                    diff=d;
+                }
+                
+                r--;
+            }
+            
+            else if(a[l]+a[r]<target){
+                if(d<diff){
+                    sum=a[l]+a[r];
+                    diff=d;
+                }
+                
+                l++;
+            }
+        }
+        return sum;
     }
 }
