987bd3cd1320a931472a766ff3f9886d55c88f4f
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-10-19 23:41:08 +0530

    Time: 6 ms (34.05%), Space: 42.7 MB (5.69%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean checkIfPangram(String s) {
         
-        HashMap<Character,Integer>hm=new HashMap<>();
+        HashSet<Character>hm=new HashSet<>();
         
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            if(!hm.contains(ch)){
+                hm.add(ch);
+            }
         }
         
         return hm.size()==26?true:false;
