f83fabcc319e6edf96f4dbaaeba6d778c55385b5
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-10-20 11:05:58 +0530

    Time: 4 ms (99.95%), Space: 41.6 MB (99.16%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,40 +1,33 @@
 class Solution {
     public String intToRoman(int num) {
         
-        // HashMap<Integer,String>hm=new HashMap<>();
-        
         int val[]={1,4,5,9,10,40,50,90,100,400,500,900,1000};
         String roman[]={"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
         
-        // hm.put(1,"I");
-        // hm.put(4,"IV");
-        // hm.put(5,"V");
-        // hm.put(9,"IX");
-        // hm.put(10,"X");
-        // hm.put(40,"XL");
-        // hm.put(50,"L");
-        // hm.put(90,"XC");
-        // hm.put(100,"C");
-        // hm.put(400,"CD");
-        // hm.put(500,"D");
-        // hm.put(900,"CM");
-        // hm.put(1000,"M");
-        
-        
         StringBuilder sb=new StringBuilder();
         
+        //loop till number does not becomes 0
         while(num>0){
             
+            //check if there is any number in the val array smaller or equal than
+            //the current num
             for(int i=12;i>=0;i--){
                 
+                //so we check
                 if(num>=val[i]){
-                    num=num-val[i];
+                    
+                    //append the roman value of the difference value
                     sb.append(roman[i]);
+                    
+                    //update the number to the remaining value
+                    num=num-val[i];
+                    
                     break;
                 }
             }
         }
         
+        //finally return the string
         return sb.toString();
     }
 }
