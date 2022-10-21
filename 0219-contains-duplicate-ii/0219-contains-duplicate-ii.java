6043e5cc9bf2e6e9adbbfd5743a8bdf1f31efb8b
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-10-21 13:30:47 +0530

    Time: 14 ms (99.84%), Space: 51.1 MB (90.43%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -7,9 +7,15 @@ class Solution {
 
         HashSet<Integer> window = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
+            
+            //if window size becomes greater than k we slide the window
             if (i > k) {
                 window.remove(nums[i - k - 1]);
             }
+            
+            //it return true if the the element being added 
+            // is not present in the hashset, so if it returns false, means the
+            // the element was already present, hence we return true
             if (!window.add(nums[i])) {
                 return true;
             }
