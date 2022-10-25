a84f9b5077bc23655620af154b486367bfc632f4
KC1919 <70034796+KC1919@users.noreply.github.com>
2022-10-26 00:33:15 +0530

    Time: 29 ms (66.74%), Space: 51 MB (56.60%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -14,54 +14,38 @@ class Solution {
             return;
         }
         
-        // for(int i=idx;i<list.size();i++){
             
-            String word=list.get(idx);
-            String s=sb.toString();
+        String word=list.get(idx);
+        String s=sb.toString();
             
-            if(unique(word) && compare(s,word)){
-                sb.append(word);
-                findMax(list,idx+1,sb,len+word.length());
-                sb=new StringBuilder(s);
-            }
-            
+        if(unique(word) && compare(s,word)){
+            sb.append(word);
+            findMax(list,idx+1,sb,len+word.length());
+            sb=new StringBuilder(s);
+        }
            
-            findMax(list,idx+1,sb,len);
-            
-        // }
+        findMax(list,idx+1,sb,len);
     }
     
     private boolean unique(String s){
         
-        HashSet<Character>hm=new HashSet<>();
-        
         for(int i=0;i<s.length();i++){
-            
             char ch=s.charAt(i);
-            
-            if(hm.contains(ch))
+            int idx=s.indexOf(ch);
+            if(idx!=-1 && idx!=i){
                 return false;
-            
-            hm.add(ch);
+            }
         }
+        
         return true;
     }
     
     private boolean compare(String s, String t){
         
-        HashSet<Character>hm=new HashSet<>();
-        
-        for(int i=0;i<t.length();i++){
-            
-            char ch=t.charAt(i);
-            
-            hm.add(ch);
-        }
-        
-        
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(hm.contains(ch)){
+            
+            if(t.indexOf(ch)!=-1){
                 return false;
             }
         }
