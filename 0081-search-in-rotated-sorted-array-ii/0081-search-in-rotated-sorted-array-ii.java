3221a5d653992bfe47d28cefa35c485e7a836ec1
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-11 13:02:00 +0530

    Time: 0 ms (100%), Space: 43.3 MB (35.71%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,35 +1,28 @@
 class Solution {
-    public boolean search(int[] a, int target) {
-        
+    public boolean search(int[] a, int target) 
+    {
         int l=0, h=a.length-1;
         
-        while(l<=h){
+        while(l<=h)
+        {
+            while (l < h && a[l] == a[l + 1]) l++;
+            while (l < h && a[h] == a[h - 1]) h--;
+            
             int mid=(l+h)/2;
-
-            if(target==a[mid])return true;
-
-            else if(a[l]==a[mid] && a[mid]==a[h]){
-                l+=1;
-                h-=1;
-                continue;
+            
+            if(a[mid]==target) return true;
+            
+            else if(a[l]<=a[mid])
+            {
+                if(target>=a[l] && target<a[mid]) h=mid-1;
+                else l=mid+1;
             }
-
-            else if(a[mid]>=a[l]){
-                if(target>=a[l] && target<a[mid]){
-                    h=mid-1;
-                }
-                else {
-                    l=mid+1;
-                }
-            }
-            else if(a[mid]<=a[h]){
-                if(target<=a[h] && target>a[mid]){
-                    l=mid+1;
-                }
-                else {
-                    h=mid-1;
-                }
-            }
+            
+            else
+            {
+                if(target>a[mid] && target<=a[h]) l=mid+1;
+                else h=mid-1;
+            } 
         }
         return false;
     }
