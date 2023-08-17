e4750072e814ec0fd135582664c2e784a3f2e7f9
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-17 14:26:45 +0530

    Time: 15 ms (62.25%), Space: 46.1 MB (35.79%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        
+        int n=mat.length;
+        int m=mat[0].length;
+
+        int dp[][]=new int[n][m];
+
+        boolean visited[][]=new boolean[n][m];
+
+        Queue<int[]>que=new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    que.add(new int[]{i,j});
+                }
+            }
+        }
+
+        int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
+
+        int level=0;
+
+        while(que.size()>0){
+            int size=que.size();
+            level++;   
+            while(size-->0){
+                int p[]=que.remove();
+    
+                for(int k=0;k<4;k++){
+                    int row=dir[k][0]+p[0];
+                    int col=dir[k][1]+p[1];
+
+                    if(row>=0 && col>=0 && row<n && col<m && visited[row][col]==false && mat[row][col]==1){
+                        que.add(new int[]{row,col});
+                        dp[row][col]=level;
+                        visited[row][col]=true;
+
+                    }
+                }
+            }
+        }
+
+        return dp;
+    }
+}
