63082f589b2e33a099f33486e6d808ec5c9adc16
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-19 15:16:08 +0530

    Time: 49 ms (88.46%), Space: 70.6 MB (69.99%) - LeetHub

--- 0833-bus-routes/0833-bus-routes.java
+++ 0833-bus-routes/0833-bus-routes.java
@@ -26,27 +26,46 @@ class Solution {
     
 
         int busCount=0;
+
+        //added the source stop
         que.add(source);
 
         while(que.size()>0){
 
             busCount++;
+
+            //number of stops that can be visited
             int size=que.size();
 
+            //exploring all the other stops that can be visited from these stops
             while(size-->0){
+
+                //remove a stop
                 int rem=que.remove();
 
+                //check if this stop has been visited earlier
                 if(stopVisited[rem]==true){
                     continue;
                 }
 
+                //else mark this stop as visited
                 stopVisited[rem]=true;
 
+                //checking which all buses pass from this stop
                 for(int bus:hm.get(rem)){
+
+                    //we check if the bus has been visited earlier
                     if(busVisited[bus]==false){
+                        //mark it as visited
                         busVisited[bus]=true;
+
+                        //we check all the stops to which this bus goes
                         for(int i=0;i<routes[bus].length;i++){
+                            
+                            //we add those stops to the queue
                             que.add(routes[bus][i]);
+
+                            //and if stop == target stop, then we return the current bus count
                             if(routes[bus][i]==target){
                                 return busCount;
                             }
