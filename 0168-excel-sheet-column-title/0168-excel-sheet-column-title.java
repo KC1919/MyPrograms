774864de76db05623d47011fe484881305f24b80
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-22 13:57:39 +0530

    Time: 0 ms (100%), Space: 39.5 MB (77.8%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+
+        return getColumnName(columnNumber);
+
+    }
+
+    public String getColumnName(int colNum){
+
+        int quotient  = colNum / 26;
+        int remainder = colNum % 26;
+        
+        StringBuilder res=new StringBuilder();
+
+        if(remainder==0){
+            quotient-=1;
+            remainder=26;
+        }
+
+        if(quotient>0 && quotient<=26){
+            char prefix=(char)(quotient-1+'A');
+            res.append(prefix);
+        }
+
+        String suffix="";
+
+        if(remainder!=0)
+            suffix = (char)(remainder-1+'A')+"";
+
+
+        if(quotient>26){
+            String ans=getColumnName(quotient);
+            res.append(ans);
+        }
+
+        res.append(suffix);
+
+        return res.toString();
+    }   
+}
