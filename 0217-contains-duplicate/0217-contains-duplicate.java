2c5bfe0e4d5e06d2bd32ea1da26b45f4f99582f2
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-25 12:28:37 +0530

    Time: 11 ms (75.72%), Space: 55.2 MB (63.77%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer>hm=new HashSet<>();
+
+        for(int num:nums){
+            if(hm.contains(num)){
+                return true;
+            }
+            hm.add(num);
+        }
+        return false;
+    }
+}
