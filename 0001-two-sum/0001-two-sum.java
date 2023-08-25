58085204cb107b1f237bf611420689b636403c17
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-25 12:21:56 +0530

    Time: 1 ms (99.42%), Space: 43.5 MB (80.72%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer,Integer>hm=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])){
+                return new int[]{i,hm.get(target-nums[i])};
+            }
+            hm.put(nums[i],i);
+        }
+
+        return new int[0];
+    }
+}
