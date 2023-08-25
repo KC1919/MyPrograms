0fd767a3e30361e64925f16e0dedd48624ab9ea1
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-25 13:17:06 +0530

    Time: 1 ms (100%), Space: 52.4 MB (63.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,24 +3,19 @@ class Solution {
         
         int n=nums.length;
 
-        int lprod[]=new int[n];
-        int rprod[]=new int[n];
-
-        lprod[0]=1;
-        rprod[n-1]=1;
-
-        for(int i=1;i<n;i++){
-            lprod[i]=nums[i-1]*lprod[i-1];
-        }
-
-        for(int i=n-2;i>=0;i--){
-            rprod[i]=nums[i+1]*rprod[i+1];
-        }
-
         int ans[]=new int[n];
 
-        for(int i=0;i<n;i++){
-            ans[i]=lprod[i]*rprod[i];
+        ans[0]=1;
+
+        for(int i=1;i<n;i++){
+            ans[i]=nums[i-1]*ans[i-1];
+        }
+
+        int rprod=nums[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            ans[i]=ans[i]*rprod;
+            rprod*=nums[i];
         }
 
          return ans;
