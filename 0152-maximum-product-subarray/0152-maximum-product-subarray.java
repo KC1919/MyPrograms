7ac26c754222f9f6f7c4c4e40a4e28138449ffd6
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-26 15:28:10 +0530

    Time: 1 ms (85.88%), Space: 42.8 MB (97.95%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -2,49 +2,21 @@ class Solution {
     public int maxProduct(int[] nums) {
         
         int n=nums.length;
-        int prod[][]=new int[n][2];
+        int prefix=1;
+        int suffix=1;
+        int max=Integer.MIN_VALUE;
 
-        prod[0][0]=prod[0][1]=nums[0];
+        for(int i=0;i<n;i++){
+            
+            if(prefix==0) prefix=1;
 
-        int max=nums[0];
+            if(suffix==0) suffix=1;
 
-        for(int i=1;i<n;i++){
-
-            if(prod[i-1][0]==0 || prod[i-1][1]==0){
-                prod[i][0]=prod[i][1]=nums[i];
-                max=Math.max(max,Math.max(prod[i][1],prod[i][0]));
-                continue;
-            }
-
-            if(nums[i]<0){
-
-                if(prod[i-1][0]<0 && prod[i-1][1]<0){
-                    prod[i][0]=nums[i];
-                    prod[i][1]=nums[i]*prod[i-1][0];
-                }
-                else{
-                    prod[i][0]=nums[i]*prod[i-1][0];
-                    prod[i][1]=nums[i]*prod[i-1][1];
-                }
-            }
-            else{
-
-                if(prod[i-1][0]<0 && prod[i-1][1]<0){
-                    prod[i][0]=prod[i-1][0]*nums[i];
-                    prod[i][1]=nums[i];
-                }
-
-                else if(prod[i-1][1]==0){
-                    prod[i][1]=nums[i];
-                }
-
-                else{
-                    prod[i][0]=nums[i]*prod[i-1][0];
-                    prod[i][1]=nums[i]*prod[i-1][1];
-                }
-            }
-            max=Math.max(max,Math.max(prod[i][1],prod[i][0]));
+            prefix=prefix*nums[i];
+            suffix=suffix*nums[n-i-1];
+            max=Math.max(Math.max(prefix,suffix),max);
         }
+        
         return max;
     }
 }
