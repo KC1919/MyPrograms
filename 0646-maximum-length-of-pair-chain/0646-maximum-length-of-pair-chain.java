a6be0481a09881126d9e2371a6c265c2478850a6
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-26 10:05:26 +0530

    Time: 8 ms (99.48%), Space: 43.2 MB (99.25%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,30 +1,58 @@
 class Solution {
     public int findLongestChain(int[][] pairs) {
         
-        Arrays.sort(pairs, (a,b)->{
-            return a[0]-b[0];
-        });
+        Arrays.sort(pairs, (a,b)-> a[1]-b[1] );
 
-        int n=pairs.length;
+        // int dp[][]=new int[pairs.length][2];
 
-        int dp[]=new int[n];
+        // dp[0]=pairs[0];
+        // int max=1;
+        // int len=1;
+        
+        // for(int i=1;i<pairs.length;i++){
+            
+        //     int pos=findPos(dp,len-1,pairs[i][0]);
+            
+        //     if(pos==len){
+        //         len++;
+        //         max=Math.max(len,max);
+        //     }
+            
+        //     dp[pos]=pairs[i];
+        // }
 
-        dp[0]=1;
+        // return max;
 
-        for(int i=1;i<n;i++){
-
-            int max=0;
-
-            for(int j=0;j<i;j++){
-                if(pairs[i][0]>pairs[j][1]){
-                    max=Math.max(dp[j],max);
-                }
+        int end = pairs[0][1];
+        int res = 1;
+        for (int i = 1; i < pairs.length; i++) {
+            if (pairs[i][0] > end) {
+            res++;
+            end = pairs[i][1];
             }
-
-            max+=1;
-            dp[i]=max;
         }
+        return res;
+    }
 
-        return dp[n-1];
+    private int findPos(int a[][], int high, int target){
+        
+        int l=0, h=high;
+        
+        while(l<=h){
+            int mid=(l+h)/2;
+            
+            if(a[mid][1]<target){
+                l=mid+1;
+            }
+            
+            else if(a[mid][1]>target){
+                h=mid-1;
+            }
+            
+            else{
+                return mid;
+            }
+        }
+        return l;
     }
 }
