4ea6ac059a7d2a67bd94e3da14ab20da67a4e96a
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-27 13:40:39 +0530

    Time: 13 ms (93.62%), Space: 56.4 MB (30.63%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.java
@@ -0,0 +1,57 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        
+        if(stones[1]-stones[0]!=1){
+            return false;
+        }
+
+        int n=stones.length;
+
+        HashMap<Integer,Integer>hm=new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            hm.put(stones[i],i);
+        }
+       
+
+        Boolean dp[][]=new Boolean[n][n];
+
+        boolean result=frogJump(1,stones,dp,1,hm);
+
+        return result;
+    }
+
+    public boolean frogJump(int idx, int stones[] , Boolean dp[][], int lastJump, HashMap<Integer,Integer>hm){
+
+        if(idx==stones.length-1){
+            return true;
+        }
+
+        if(dp[idx][lastJump]!=null){
+            return dp[idx][lastJump];
+        }
+
+        if(hm.containsKey(lastJump+stones[idx])){
+            boolean res1=frogJump(hm.get(lastJump+stones[idx]),stones,dp,lastJump,hm);
+            if(res1==true){
+                return dp[idx][lastJump]=true;
+            }
+        }
+
+        if(hm.containsKey(lastJump+stones[idx]+1)){
+            boolean res2=frogJump(hm.get(lastJump+stones[idx]+1),stones,dp,lastJump+1,hm);
+            if(res2==true){
+                return dp[idx][lastJump]=true;
+            }
+        }
+
+        if(lastJump>1 && hm.containsKey(lastJump+stones[idx]-1)){
+            boolean res3=frogJump(hm.get(lastJump+stones[idx]-1),stones,dp,lastJump-1,hm);
+            if(res3==true){
+                return dp[idx][lastJump]=true;
+            }
+        }
+
+        return dp[idx][lastJump]=false;
+    }
+}
