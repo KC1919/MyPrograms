9bc9e51cd9cbfe350ee90b9e51c652a09a30e2c2
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-28 10:14:15 +0530

    Time: 0 ms (100%), Space: 44.1 MB (47.31%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -14,38 +14,22 @@
  * }
  */
 class Solution {
-    int count=0;
-    public TreeNode convertBST(TreeNode node) {
-        List<Integer>list=new ArrayList<>();
-        inorder(node,list);
-        
-        for(int i=list.size()-2;i>=0;i--)
-        {
-            list.set(i,list.get(i+1)+list.get(i));
-        }
-        
-        count=0;
-        rightSum(node,list);
-        
-        return node;
+
+    int sum=0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root==null) return root;
+
+        greaterTree(root);
+        return root;
     }
-    
-    public void inorder(TreeNode node, List<Integer>list)
-    {
-        if(node==null)return;
-        
-        inorder(node.left,list);
-        list.add(node.val);
-        inorder(node.right,list);
-    }
-    
-    public void rightSum(TreeNode node, List<Integer>list)
-    {
-        if(node==null)return;
-        
-        rightSum(node.left,list);
-        node.val=list.get(count);
-        count++;
-        rightSum(node.right,list);
+
+    public void greaterTree(TreeNode node){
+
+        if(node==null) return;
+
+        greaterTree(node.right);
+        sum+=node.val;
+        node.val=sum;
+        greaterTree(node.left); 
     }
 }
