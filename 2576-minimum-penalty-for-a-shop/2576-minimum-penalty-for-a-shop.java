ee057b592f009f97742e05e9fb0c0a74318c5718
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-08-29 12:34:34 +0530

    Time: 8 ms (98.65%), Space: 43.9 MB (94.61%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,52 +1,14 @@
 class Solution {
     public int bestClosingTime(String customers) {
+        int j = -1, score = 0, maxScore = 0;
         
-        int n=customers.length();
-
-        int pair[][]=new int[n+1][2];
-
-        int zcount=0;
-
-        // if(customers.charAt(0)=='N') zcount++;
-
-        for(int i=0;i<n;i++){
-            if(customers.charAt(i)=='N'){
-                pair[i][0]=zcount;
-                zcount++;
-            }
-            else{
-                pair[i][0]=zcount;
+        for (int i = 0; i < customers.length(); i++) {
+            score += customers.charAt(i) == 'Y'? 1 : -1;
+            if (score > maxScore) {
+                j = i;
+                maxScore = score;
             }
         }
-
-        pair[n][0]=zcount;
-
-        int ocount=0;
-
-        for(int i=n-1;i>=0;i--){
-            if(customers.charAt(i)=='Y'){
-                ocount++;
-                pair[i][1]=ocount;
-            }
-            else{
-                pair[i][1]=pair[i+1][1];
-            }
-        }
-        
-        int min=Integer.MAX_VALUE;
-        int idx=-1;
-
-        for(int i=0;i<n;i++){
-            int penality=pair[i][0]+pair[i][1];
-
-            if(penality<min){
-                min=penality;
-                idx=i;
-            }
-        }
-
-        if((pair[n][0]+pair[n][1])<min) return n;
-
-        return idx;
+        return j + 1;
     }
 }
