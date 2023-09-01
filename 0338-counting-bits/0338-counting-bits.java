aba4aec96984e1f121a0dd2912ca18cd92ad0b55
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-01 16:14:12 +0530

    Time: 3 ms (35.7%), Space: 46.6 MB (37.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -8,17 +8,22 @@ class Solution {
             if(dp[i]!=0){
                 continue;
             }
-            dp[i]=calculateBit(i);
+            dp[i]=calculateBit(i,dp);
         }
         return dp;
     }
 
-    public int calculateBit(int n){
+    public int calculateBit(int n, int dp[]){
 
         int rem=0;
         int count=0;
         
         while(n>1){
+
+            if(dp[n]!=0){
+                return dp[n]+count;
+            }
+
             rem=n%2;
             count+=rem==1?1:0;
             n=n/2;
