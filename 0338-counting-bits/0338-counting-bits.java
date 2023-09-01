dc8627378177e707f7295a959dbff5a54f0ad2e5
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-01 16:23:19 +0530

    Time: 2 ms (73.73%), Space: 46.6 MB (25.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,28 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        
-        int dp[]=new int[n+1];
-
-        for(int i=1;i<=n;i++){
-            dp[i]=calculateBit(i,dp);
+        int ans[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            ans[i] = ans[i/2] + (i %2);
         }
-        return dp;
-    }
-
-    public int calculateBit(int n, int dp[]){
-
-        int rem=0;
-        int count=0;
-        
-        while(n>1){
-
-            if(dp[n]>0)
-                return dp[n]+count;
-
-            rem=n%2;
-            count+=rem==1?1:0;
-            n=n/2;
-        }
-        return count+1;
+    return ans;
     }
 }
