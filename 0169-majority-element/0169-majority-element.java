9d5f3e35601e89c1b98c978b6484e9010ac67686
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-06 17:02:17 +0530

    Time: 1 ms (99.85%), Space: 48.9 MB (11.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,23 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        int cand=0;
-        int count=0;
+        int cand=nums[0];
+        int count=1;
 
-        for(int i=0;i<nums.length;i++){
-
-            if(count==0){
-                cand=nums[i];
-            }
+        for(int i=1;i<nums.length;i++){
 
             if(nums[i]==cand){
                 count++;
             }
             else{
                 count--;
+                if(count==0){
+                    cand=nums[i];
+                    count=1;
+                }     
             }
         }
-
         return cand;
     }
 }
