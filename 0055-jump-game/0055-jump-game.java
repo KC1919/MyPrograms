fae1a4f398f391276c37f56cf251d2e7bdc895b9
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-08 22:35:41 +0530

    Time: 1 ms (99.97%), Space: 44 MB (75.89%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,21 +2,13 @@ class Solution {
     public boolean canJump(int[] nums) {
 
         int n=nums.length; 
-        boolean dp[]=new boolean[n];
-
-        dp[n-1]=true;
+        int goal=n-1;
 
         for(int i=n-2;i>=0;i--){
-            for(int j=nums[i];j>0;j--){
-                if(i+j>=n-1){
-                    dp[i]=true;
-                    break;
-                }
-                else if(i+j<n && dp[i+j]==true){
-                    dp[i]=true;
-                }
+            if(i+nums[i]>=goal){
+                goal=i;
             }
         }
-        return dp[0]; 
+        return goal==0?true:false; 
     }
 }
