02a7be2b5d450c34ae013f55f0ae3c68301e0a04
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-08 22:16:58 +0530

    Time: 1 ms (89.25%), Space: 40.6 MB (92.67%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -8,23 +8,22 @@ class Solution {
             if(i==0){
                 list.add(1);
             }
-            else if(i==1){
-                list.add(1);
-                list.add(1);
-            }
+            // else if(i==1){
+            //     list.add(1);
+            //     list.add(1);
+            // }
             else{
                 List<Integer>rem=res.get(i-1);
                 int j=0;
                 while(j<=i){
-                    if(j==0) list.add(1);
-
-                    else if(j==i){
+                    if(j==0) 
                         list.add(1);
-                    }
 
-                    else{
+                    else if(j==i)
+                        list.add(1);
+
+                    else
                         list.add(rem.get(j-1)+rem.get(j));
-                    }
 
                     j++;
                 }
