584dc456b9fad1345289281e48687c6ec698b60a
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-12 15:47:52 +0530

    Time: 52 ms (22.36%), Space: 44.6 MB (49.84%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -9,24 +9,15 @@ class Solution {
             hm.put(ch,hm.getOrDefault(ch,0)+1);
         }
 
-        Integer a[]=new Integer[hm.size()];
-
-        int idx=0;
-        for(char key:hm.keySet()){
-            a[idx++]=hm.get(key);
-        }
-
-        Arrays.sort(a, Collections.reverseOrder());
+        Set<Integer>hs=new HashSet<>();
 
         int count=0;
 
-        for(int i=1;i<a.length;i++){
-            while(a[i]>=a[i-1]){
-                a[i]--;
+        for(char key:hm.keySet()){
+            int freq=hm.get(key);
+            while(freq!=0 && !hs.add(freq)){
+                freq--;
                 count++;
-                if(a[i]==0){
-                    break;
-                }
             }
         }
         return count;
