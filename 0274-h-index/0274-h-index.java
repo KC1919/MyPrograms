51a3187356aa6634c04b899c34d9c4be0d2ac8c9
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-14 08:59:08 +0530

    Time: 2 ms (64.52%), Space: 40.5 MB (36.93%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int hIndex(int[] a) {
+        
+        int n=a.length;
+
+        if(n==1)
+            return a[0]==0?0:1;
+        
+        Arrays.sort(a);
+
+        int hIndex=0;
+
+        for(int i=0;i<n;i++){
+            int val=i+a[i]-1;
+            if(val<n && val>=0){
+                hIndex=a[i];
+            }
+            else{
+                hIndex=Math.max(hIndex,n-i);
+            }
+        }
+        return hIndex;
+    }
+}
