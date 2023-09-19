9c317b732bc620b58251f10da04ed7e2c6e175e6
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-19 11:47:27 +0530

    Time: 44 ms (52.61%), Space: 44.4 MB (31.14%) - LeetHub

---
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    class Pair{
+        int weight;
+        int row;
+        int col;
+
+        Pair(int row, int col, int weight){
+            this.row=row;
+            this.col=col;
+            this.weight=weight;
+        }
+    }
+    public int minimumEffortPath(int[][] height) {
+        
+        int n=height.length;
+        int m=height[0].length;
+
+        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->{
+            return a.weight-b.weight;
+        });
+
+        int dir[][]={{0,1},{0,-1},{-1,0},{1,0}};
+        boolean vis[][]=new boolean[n][m];
+
+        pq.add(new Pair(0,0,0));
+
+        // vis[0][0]=true;
+
+        while(pq.size()>0){
+            Pair rem=pq.remove();
+
+            if(vis[rem.row][rem.col]==true)
+                continue;
+
+            vis[rem.row][rem.col]=true;
+
+            if(rem.row==n-1 && rem.col==m-1)
+                return rem.weight;
+
+            for(int k=0;k<4;k++){
+                int row=dir[k][0]+rem.row;
+                int col=dir[k][1]+rem.col;
+
+                if(row>=0 && col>=0 && row<n && col<m && vis[row][col]==false){
+                    int diff=Math.abs(height[row][col]-height[rem.row][rem.col]);
+
+                    pq.add(new Pair(row,col,Math.max(diff,rem.weight)));
+                }
+            }
+        }
+        return -1;
+    }
+}
