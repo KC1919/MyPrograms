1dd276c6f6774c1a4b8383bff01a9d31d5a2dc31
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-09-22 11:52:58 +0530

    Time: 15 ms (5%), Space: 44 MB (5.41%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,19 +3,43 @@ class Solution {
         
         if(s.length()==0)
             return true;
-            
+
         if(s.length()>t.length())
             return false;
 
-        int idx=0;
+        HashMap<Character, TreeSet<Integer>>hm=new HashMap<>();
+
         for(int i=0;i<t.length();i++){
-            if(s.charAt(idx)==t.charAt(i)){
+            char ch=t.charAt(i);
+
+            if(!hm.containsKey(ch)){
+                hm.put(ch,new TreeSet<>());
+            }
+            hm.get(ch).add(i);
+        }
+
+        int prev=-1, j=0, idx=0;
+
+        System.out.println(hm);
+
+        while(idx<s.length() && j<t.length()){
+            char ch=s.charAt(idx);
+
+            if(!hm.containsKey(ch))
+                return false;
+
+            int ind=hm.get(ch).pollFirst();
+
+            if(hm.get(ch).size()==0)
+                hm.remove(ch);
+
+            if(ind>prev){
+                prev=ind;
                 idx++;
             }
-
-            if(idx==s.length())
-                return true;
+            j++;
         }
-        return false;
+        
+        return idx==s.length()?true:false;
     }
 }
