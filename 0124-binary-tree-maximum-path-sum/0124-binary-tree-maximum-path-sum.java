ce4b7e5bd44e2d752eb1fae4dd98395bdd0c058b
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-10-01 21:12:44 +0530

    Time: 0 ms (100%), Space: 44.1 MB (64.68%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxSum=-1001;
+    public int maxPathSum(TreeNode root) {
+        maxSum=-1001;
+        maxPath(root);
+        return maxSum;
+    }
+
+    private int maxPath(TreeNode node){
+        if(node==null) return Integer.MIN_VALUE;
+
+        int mySum=0;
+        int lres=maxPath(node.left);
+        int rres=maxPath(node.right);
+
+        if(lres!=Integer.MIN_VALUE){
+            mySum=lres;
+            maxSum=Math.max(maxSum,lres);
+            maxSum=Math.max(maxSum,lres+node.val);
+        }
+
+        if(rres!=Integer.MIN_VALUE){
+            mySum=Math.max(mySum,rres);
+            maxSum=Math.max(maxSum,rres);
+            maxSum=Math.max(maxSum,rres+node.val);
+        }
+
+        if(lres!=Integer.MIN_VALUE && rres!=Integer.MIN_VALUE){
+            maxSum=Math.max(maxSum,lres+rres+node.val);
+        }
+
+        maxSum=Math.max(maxSum,node.val);
+        
+        return Math.max(mySum+node.val,node.val);
+    }
+}
