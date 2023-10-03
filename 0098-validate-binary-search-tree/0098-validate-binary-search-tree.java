728b61f873cfd3f515e195964d72da978d5234f8
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-10-03 14:45:27 +0530

    Time: 0 ms (100%), Space: 43.6 MB (71.65%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,24 +14,17 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        if(root==null) return true;
-
-        List<Integer>list=new ArrayList<>();
-        inorder(root,list);
-
-        for(int i=1;i<list.size();i++){
-            if(list.get(i)<=list.get(i-1)) return false;
-        }
-
-        return true;
-    }
-
-    private void inorder(TreeNode node, List<Integer>list){
-        if(node==null) return;
-
-        inorder(node.left,list);
-        list.add(node.val);
-        inorder(node.right,list);
+    public long prev = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) 
+    {
+       if(root==null) 
+           return true;
+        boolean l = isValidBST(root.left);
+        if(prev>=root.val) 
+            return false;
+        prev = root.val;
+        boolean r = isValidBST(root.right);
+        return l && r;
+        
     }
 }
