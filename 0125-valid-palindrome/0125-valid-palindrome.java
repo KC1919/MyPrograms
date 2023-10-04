78a78b5bc013d641aa1e1ecb56a35d4f181d4974
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-10-04 16:44:22 +0530

    Time: 3 ms (73.38%), Space: 43.6 MB (47.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if((ch>=65 && ch<=90) || (ch>=97 && ch<=122) || (ch>='0' && ch<='9')){
+                sb.append(ch);
+            }
+        }
+
+        String str=sb.toString().toLowerCase();
+        
+        int start=0, end=str.length()-1;
+
+        while(start<=end){
+            if(str.charAt(start)!=str.charAt(end)) return false;
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
