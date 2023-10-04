c35b93ca74850f187727dd3f9b79faad298fe39f
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-10-04 16:45:45 +0530

    Time: 3 ms (73.38%), Space: 42.2 MB (64.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -7,16 +7,16 @@ class Solution {
             char ch=s.charAt(i);
 
             if((ch>=65 && ch<=90) || (ch>=97 && ch<=122) || (ch>='0' && ch<='9')){
-                sb.append(ch);
+                sb.append(Character.toLowerCase(ch));
             }
         }
 
-        String str=sb.toString().toLowerCase();
+        // String str=sb.toString().toLowerCase();
         
-        int start=0, end=str.length()-1;
+        int start=0, end=sb.length()-1;
 
         while(start<=end){
-            if(str.charAt(start)!=str.charAt(end)) return false;
+            if(sb.charAt(start)!=sb.charAt(end)) return false;
 
             start++;
             end--;
