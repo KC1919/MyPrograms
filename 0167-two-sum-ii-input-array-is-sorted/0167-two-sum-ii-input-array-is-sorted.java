aa65abec523fb280b3c3b110d8892d230d937004
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-10-04 17:36:32 +0530

    Time: 4 ms (20.5%), Space: 45.2 MB (75.28%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,19 +3,36 @@ class Solution {
         
         int n=nums.length;
 
-        int li=0;
-        int ri=n-1;
+        int ans[]=new int[2];
 
-        while(li<ri){
+        for(int i=0;i<n;i++){
+            int remSum=target-nums[i];
 
-            int sum=nums[li]+nums[ri];
-            if(sum==target){
-                return new int[]{li+1,ri+1};
+            if(remSum>=nums[i]){
+                int idx=bsearch(nums,i+1,remSum);
+
+                if(idx!=-1){
+                    ans[0]=i+1;
+                    ans[1]=idx+1;
+                    break;
+                }
             }
-            else if(sum<target) li++;
-
-            else ri--;
         }
-        return new int[]{li+1,ri+1};
+        return ans;
+    }
+
+    private int bsearch(int nums[], int l, int target){
+
+        int h=nums.length-1;
+
+        while(l<=h){
+            int mid=(l+h)/2;
+
+            if(nums[mid]==target) return mid;
+
+            else if(nums[mid]<target) l=mid+1;
+            else h=mid-1;
+        }
+        return -1;
     }
 }
