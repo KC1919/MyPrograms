0017d8c3ed55cefe0cc086a463d4f6153bcce217
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-10-04 14:48:18 +0530

    Time: 5 ms (51.92%), Space: 43.3 MB (89.11%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,9 +1,9 @@
 class Solution {
     public int romanToInt(String s) {
         
-        int n=s.length();
         HashMap<Character,Integer>hm=new HashMap<>();
-
+        int n=s.length();
+        
         hm.put('I',1);
         hm.put('V',5);
         hm.put('X',10);
@@ -11,54 +11,31 @@ class Solution {
         hm.put('C',100);
         hm.put('D',500);
         hm.put('M',1000);
-
+        
+        int a[]=new int[n];
+        
+        for(int i=0;i<n;i++){
+            a[i]=hm.get(s.charAt(i));
+        }
+        
+        int i=0;
         int sum=0;
-
-        for(int i=0;i<s.length();){
-            char ch=s.charAt(i);
-
-            if(ch=='I'){
-                if(i+1<n){
-                    char nch=s.charAt(i+1);
-                    if(nch=='V' || nch=='X'){
-                        sum=sum+hm.get(nch)-hm.get(ch);
-                        i=i+2;
-                        continue;
-                    }
-                }
-                sum+=hm.get(ch);
-                i++;
+        for(i=0;i<n-1;){
+            
+            if(a[i]>=a[i+1]){
+                sum+=a[i];
+                i+=1;
             }
-            else if(ch=='X'){
-                if(i+1<n){
-                    char nch=s.charAt(i+1);
-                    if(nch=='L' || nch=='C'){
-                        sum=sum+hm.get(nch)-hm.get(ch);
-                        i=i+2;
-                        continue;
-                    }
-                }
-                sum+=hm.get(ch);
-                i++;
-            }
-            else if(ch=='C'){
-                if(i+1<n){
-                    char nch=s.charAt(i+1);
-                    if(nch=='D' || nch=='M'){
-                        sum=sum+hm.get(nch)-hm.get(ch);
-                        i=i+2;
-                        continue;
-                    }
-                }
-                sum+=hm.get(ch);
-                i++;
-                
-            }
+            
             else{
-                sum+=hm.get(ch);
-                i++;
+                sum=sum+(a[i+1]-a[i]);
+                i+=2;
             }
         }
+        
+        if(i==n-1)
+            sum+=a[n-1];
+        
         return sum;
     }
 }
