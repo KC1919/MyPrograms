d68325581f4c907c72633d17f837d507e123d41a
KC1919 <70034796+KC1919@users.noreply.github.com>
2023-10-04 17:00:43 +0530

    Time: 1 ms (90.85%), Space: 40.2 MB (72.63%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
+        if(s.length()>t.length()) return false;
+        
         int i=0;
         int j=0;
         int prev=-1;
